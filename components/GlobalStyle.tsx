2865803b168d9b8d6704942b3032d53d31930965
Scott Byrne <scott.byrne@forestry.io>
2020-01-08 11:22:36 -0400

    add global style, pull theme into component

---
+++ components/GlobalStyle.tsx
@@ -0,0 +1,109 @@
+import React from "react";
+import { createGlobalStyle, css } from "styled-components";
+
+const CssReset = css`
+  html,
+  body,
+  p,
+  ol,
+  ul,
+  li,
+  dl,
+  dt,
+  dd,
+  blockquote,
+  figure,
+  fieldset,
+  legend,
+  textarea,
+  pre,
+  iframe,
+  hr,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    margin: 0;
+    padding: 0;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  button,
+  input,
+  select,
+  textarea {
+    margin: 0;
+  }
+
+  html {
+    box-sizing: border-box;
+  }
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+
+  img,
+  video {
+    height: auto;
+    max-width: 100%;
+  }
+
+  iframe {
+    border: 0;
+  }
+
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+
+  td,
+  th {
+    padding: 0;
+    text-align: left;
+  }
+`;
+
+export const GlobalStyle = createGlobalStyle`
+  ${CssReset}
+
+  html {
+    font-size: 100%;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
+      "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+      "Helvetica Neue", sans-serif;
+    line-height: 1.5;
+    height: 100%;
+    min-height: 100%;
+    box-sizing: border-box;
+
+    * {
+      box-sizing: inherit;
+      font-variant-numeric: inherit;
+      font-family: inherit;
+      line-height: inherit;
+      font-size: 100%;
+      font-weight: normal;
+    }
+
+    @media (min-width: 800px) {
+      font-size: 112.5%;
+    }
+
+    @media (min-width: 1200px) {
+      font-size: 125%;
+    }
+
+    @media (min-width: 1600px) {
+      font-size: 137.5%;
+    }
+  }
+`;
--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,15 @@
 import App from "next/app";
 import React from "react";
 import { ThemeProvider } from "styled-components";
-
-const theme = {
-  colors: {
-    primary: "#0070f3"
-  }
-};
+import { GlobalStyle } from "../components/GlobalStyle";
+import { theme } from "../components/Theme";
 
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     );
