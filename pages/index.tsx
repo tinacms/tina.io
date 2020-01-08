c7100d4c0e3bf67701bb4d73b5c1720bd42b267d
Scott Byrne <scott.byrne@forestry.io>
2020-01-08 10:56:49 -0400

    add styled components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
 function Home() {
-  return <div>Welcome to TinaCMS!</div>;
+  return <Title>Welcome to TinaCMS!</Title>;
 }
+
+const Title = styled.h1`
+  font-size: 50px;
+  color: ${({ theme }) => theme.colors.primary};
+`;
 
 export default Home;
