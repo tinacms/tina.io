f7bec8a4add811ac968fa52daab8dea5805506af
Kendall Strautman <kendallstrautman@gmail.com>
2020-01-29 14:13:17 -0800

    feat: adds blog page routing

--- next.config.js
+++ next.config.js
@@ -1,19 +1,44 @@
-const withSvgr = require("next-svgr");
+const withSvgr = require('next-svgr')
+const glob = require('glob')
 
 module.exports = withSvgr({
   exportTrailingSlash: true,
-  exportPathMap: function() {
-    return {
-      "/": { page: "/" },
-      "/about": { page: "/about" }
-    };
+  exportPathMap: async function() {
+    // TODO: test if the docs and blog routes grab the index file
+    const routes = {
+      '/': { page: '/' },
+      '/community': { page: '/community' },
+      '/teams': { page: '/teams' },
+      '/blog': { page: '/blog/' },
+      '/docs': { page: '/docs/' },
+    }
+    //get all .md files in the blogs dir
+    const blogs = glob.sync('content/blog/**/*.md')
+
+    //remove path and extension to leave filename only
+    const blogSlugs = blogs.map(file =>
+      file
+        .split('/')[2]
+        .replace(/ /g, '-')
+        .slice(0, -3)
+        .trim()
+    )
+
+    //add each blog to the routes obj
+    blogSlugs.forEach(blog => {
+      routes[`/blog/${blog}`] = { page: '/blog/[slug]', query: { slug: blog } }
+    })
+
+    // TODO: Add docs routes
+
+    return routes
   },
   webpack(config) {
     config.module.rules.push({
       test: /\.md$/,
-      use: "raw-loader"
-    });
+      use: 'raw-loader',
+    })
 
-    return config;
-  }
-});
+    return config
+  },
+})
