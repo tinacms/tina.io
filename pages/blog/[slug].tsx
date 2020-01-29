b64d3cd7373ca22b1f6a3f764dd53825c687f9c8
Kendall Strautman <kendallstrautman@gmail.com>
2020-01-29 11:19:16 -0800

    other: prettier formatting

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,28 +1,28 @@
-import matter from "gray-matter";
-import ReactMarkdown from "react-markdown";
-import Layout from "../../components/Layout";
-import Header from "../../components/Header";
+import matter from 'gray-matter'
+import ReactMarkdown from 'react-markdown'
+import Layout from '../../components/layout/Layout'
+import Header from '../../components/layout/Header'
 
 export default function BlogTemplate(props) {
-  return (
-    <Layout pathname="/">
-      <Header />
-      <h1>{props.post.data.title}</h1>
-      <ReactMarkdown source={props.post.content} />
-    </Layout>
-  );
+	return (
+		<Layout pathname='/'>
+			<Header />
+			<h1>{props.post.data.title}</h1>
+			<ReactMarkdown source={props.post.content} />
+		</Layout>
+	)
 }
 
 BlogTemplate.getInitialProps = async function(ctx) {
-  const { slug } = ctx.query;
-  const content = await import(`../../content/blog/${slug}.md`);
-  const post = matter(content.default);
+	const { slug } = ctx.query
+	const content = await import(`../../content/blog/${slug}.md`)
+	const post = matter(content.default)
 
-  return {
-    // fileRelativePath: `src/posts/${slug}.md`,
-    post: {
-      data: { ...post.data, slug },
-      content: post.content
-    }
-  };
-};
+	return {
+		// fileRelativePath: `src/posts/${slug}.md`,
+		post: {
+			data: { ...post.data, slug },
+			content: post.content
+		}
+	}
+}
