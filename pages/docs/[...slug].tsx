11661c8a47c9f002d4ba28b31c9c425d1c78239d
James O'Halloran <james.p.ohalloran@gmail.com>
2020-01-30 11:06:56 -0400

    docs menu

--- pages/docs/[...slug].tsx
+++ pages/docs/[...slug].tsx
@@ -3,12 +3,31 @@ import ReactMarkdown from 'react-markdown'
 import Layout from '../../components/layout/Layout'
 import Header from '../../components/layout/Header'
 
+interface DocSection {
+  id: string
+  slug: string
+  title: string
+  items: DocSection[]
+}
+
+const DocSection = (section: DocSection) => {
+  return (
+    <div>
+      <div>{section.slug}</div>
+      {(section.items || []).map(item => (
+        <DocSection {...item} />
+      ))}
+    </div>
+  )
+}
+
 export default function DocTemplate(props) {
   return (
     <Layout pathname="/">
       <Header />
-      <h1>{props.post.data.title}</h1>
-      <ReactMarkdown source={props.post.content} />
+      <h1>{props.doc.data.title}</h1>
+      {props.docsNav.map(DocSection)}
+      <ReactMarkdown source={props.doc.content} />
     </Layout>
   )
 }
@@ -19,10 +38,20 @@ DocTemplate.getInitialProps = async function(ctx) {
   const content = await import(`../../content/docs/${fullSlug}.md`)
   const doc = matter(content.default)
 
+  const docsNavData = await import('../../content/pages/toc-doc.json')
+
+  //workaround for json data imported as indexed Obj
+  const docsNav = Object.keys(docsNavData).map(function(key) {
+    return docsNavData[key]
+  })
+
+  console.log(JSON.stringify(docsNav))
+
   return {
-    post: {
+    doc: {
       data: { ...doc.data, slug: fullSlug },
       content: doc.content,
     },
+    docsNav,
   }
 }
