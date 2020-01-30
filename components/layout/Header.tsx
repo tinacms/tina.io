3c34e1a0742a3ff3beb13287bed8a97215b7e304
Scott Byrne <scott.byrne@forestry.io>
2020-01-30 09:53:50 -0400

    improve header on mobile, add burger

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,27 +1,74 @@
 import React from 'react'
 import styled from 'styled-components'
+
 import { TinaIcon } from '../logo/TinaIcon'
-import Nav from './Nav'
+import Nav, { NavToggle } from './Nav'
 
 const Header = styled(({ ...styleProps }) => {
   return (
     <header {...styleProps}>
       <TinaIcon />
       <Nav />
-      <Github />
+      <iframe
+        src="https://ghbtns.com/github-btn.html?user=tinacms&repo=tinacms&type=star&count=true&size=large"
+        frameBorder="0"
+        scrolling="0"
+        width="160px"
+        height="30px"
+      ></iframe>
     </header>
   )
 })`
   position: absolute;
   width: 100%;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
   z-index: 100;
-  padding: 1.25rem 2rem;
-`
+  height: 5rem;
 
-const Github = styled.div``
+  ${TinaIcon} {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate3d(-50%, -50%, 0);
+
+    @media (min-width: 500px) {
+      left: 2rem;
+      transform: translate3d(0, -50%, 0);
+    }
+  }
+
+  ${Nav} {
+    @media (max-width: 499px) {
+      display: none;
+    }
+
+    @media (min-width: 500px) {
+      position: absolute;
+      right: 1rem;
+      top: 50%;
+      transform: translate3d(0, -50%, 0);
+    }
+
+    @media (min-width: 800px) {
+      right: auto;
+      left: 50%;
+      transform: translate3d(-50%, -50%, 0);
+    }
+  }
+
+  iframe {
+    display: none;
+
+    @media (min-width: 800px) {
+      display: block;
+      position: absolute;
+      top: 50%;
+      right: 1rem;
+      transform: translate3d(0, -50%, 0);
+    }
+  }
+
+  @media (min-width: 800px) {
+  }
+`
 
 export default Header
--- components/layout/Nav.tsx
+++ components/layout/Nav.tsx
@@ -3,6 +3,18 @@ import styled from 'styled-components'
 import Button from '../ui/Button'
 import Link from 'next/link'
 
+import BurgerSvg from '../../public/svg/burger-menu.svg'
+
+export const NavToggle = styled(({ ...styleProps }) => {
+  return (
+    <button {...styleProps}>
+      <BurgerSvg />
+    </button>
+  )
+})`
+  fill: var(--color-primary);
+`
+
 const Nav = styled(({ ...styleProps }) => {
   return (
     <ul {...styleProps}>
@@ -41,9 +53,33 @@ const Nav = styled(({ ...styleProps }) => {
   margin: 0;
   list-style-type: none;
   display: flex;
+  justify-content: center;
 
-  li {
-    margin: 0 0.3725rem;
+  @media (max-width: 799px) {
+    li {
+      ${Button} {
+        font-size: 0.8rem;
+      }
+      &:not(:first-child) {
+        ${Button} {
+          border-top-left-radius: 0;
+          border-bottom-left-radius: 0;
+        }
+      }
+      &:not(:last-child) {
+        ${Button} {
+          border-right: 1px solid var(--color-seafoam);
+          border-top-right-radius: 0;
+          border-bottom-right-radius: 0;
+        }
+      }
+    }
+  }
+
+  @media (min-width: 800px) {
+    li {
+      margin: 0 0.5rem;
+    }
   }
 `
 export default Nav
