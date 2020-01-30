1dc3e22cb19d47d0910ee654c88f10b8c84b1153
Scott Byrne <scott.byrne@forestry.io>
2020-01-30 11:26:48 -0400

    create section component

---
+++ components/layout/Section.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled, { css } from 'styled-components'
+
+const Section = styled.section`
+  padding: 3rem 0;
+
+  @media (min-width: 800px) {
+    padding: 5rem 0;
+  }
+
+  ${props =>
+    props.seafoam &&
+    css`
+      background-color: var(--color-seafoam);
+    `};
+`
+
+export default Section
