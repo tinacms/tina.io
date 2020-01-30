ffaee6541e5667a8e3dbcc42cbffc75ee6cf9772
Scott Byrne <scott.byrne@forestry.io>
2020-01-30 15:34:09 -0400

    styling teams page

--- pages/teams.tsx
+++ pages/teams.tsx
@@ -4,6 +4,7 @@ import { inlineJsonForm } from 'next-tinacms-json'
 
 import Layout from '../components/layout/Layout'
 import Wrapper from '../components/layout/Wrapper'
+import Section from '../components/layout/Section'
 import { TeamsForm } from '../components/forms'
 import ArrowList from '../components/layout/ArrowList'
 import Head from 'next/head'
@@ -11,24 +12,50 @@ import Head from 'next/head'
 function TeamsPage(props) {
   const data = props.jsonFile
   return (
-    <Layout page="teams">
+    <TeamsLayout page="teams" darkHeader>
       <Head>
         <meta property="og:title" content="TinaCMS - Teams" />
         <meta name="twitter:title" content="TinaCMS - Teams" />
       </Head>
-      <Wrapper>
-        <h2>{data.headline}</h2>
-        <hr />
-        <ArrowList>
-          {data.supporting_points.map(item => (
-            <li key={item.point.trim()}>{item.point}</li>
-          ))}
-        </ArrowList>
-        <TeamsForm hubspotFormID={process.env.GATSBY_HUBSPOT_FORM_ID} />
-      </Wrapper>
-    </Layout>
+      <TeamsSection>
+        <Wrapper>
+          <h2>{data.headline}</h2>
+          <hr />
+          <ArrowList>
+            {data.supporting_points.map(item => (
+              <li key={item.point.trim()}>{item.point}</li>
+            ))}
+          </ArrowList>
+          <TeamsForm hubspotFormID={process.env.GATSBY_HUBSPOT_FORM_ID} />
+        </Wrapper>
+      </TeamsSection>
+    </TeamsLayout>
   )
 }
+
+const TeamsLayout = styled(Layout)`
+  min-height: 100%;
+  background-color: var(--color-secondary-dark);
+  color: white;
+`
+const TeamsSection = styled(Section)`
+  flex: 1 0 auto;
+  padding: 5rem 0 3rem 0;
+
+  li {
+    color: white;
+  }
+
+  h1,
+  h2,
+  h3 {
+    color: var(--color-seafoam-dark);
+  }
+
+  hr {
+    border-color: var(--color-seafoam-dark);
+  }
+`
 
 const TeamsPageOptions = {
   fields: [
