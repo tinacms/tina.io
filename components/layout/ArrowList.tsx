8836a4098478c1f5914cac160cf9d3fe61025cd0
Scott Byrne <scott.byrne@forestry.io>
2020-01-31 10:58:31 -0400

    fix arrowList component styles

--- components/layout/ArrowList.tsx
+++ components/layout/ArrowList.tsx
@@ -6,12 +6,12 @@ export const ArrowList = styled(({ children, ...styleProps }) => {
 })`
   margin: 0;
   list-style: none;
-  padding-left: 0;
-  padding-bottom: 0.5rem;
+  padding-left: 0 !important;
+  padding-bottom: 0 !important;
   color: var(--color-secondary-dark, #241748);
   li {
     position: relative;
-    margin-bottom: 1rem;
+    margin-bottom: 1rem !important;
     padding-left: 2.5em;
     font-size: 1.125rem;
     &:before {
