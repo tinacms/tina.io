e6e7114d8bbca414160f07024c35c77aa8e28b98
DJ <dj@walker.dev>
2020-02-06 11:35:44 -0500

    add index route for /docs
    docs/index.md is the ONLY index.md that has a page route. To preserve filepath-url parity,
    if you wish to add an index for a subsection
    add it as docs/getting-started.md
    instead of docs/getting-started/index.md

---
+++ pages/docs/index.tsx
@@ -0,0 +1,34 @@
+import DocTemplate from './[...slug]'
+import matter from 'gray-matter'
+import { readFile } from '../../utils/readFile'
+
+export async function unstable_getStaticProps(ctx) {
+  const content = await readFile(`content/docs/index.md`)
+  const doc = matter(content)
+
+  const docsNavData = await import('../../content/toc-doc.json')
+  const nextDocPage =
+    doc.data.next && matter(await readFile(`content${doc.data.next}.md`))
+  const prevDocPage =
+    doc.data.prev && matter(await readFile(`content${doc.data.prev}.md`))
+
+  return {
+    props: {
+      doc: {
+        data: { ...doc.data, slug: '/docs' },
+        content: doc.content,
+      },
+      docsNav: docsNavData.default,
+      nextPage: {
+        slug: doc.data.next,
+        title: nextDocPage && nextDocPage.data.title,
+      },
+      prevPage: {
+        slug: doc.data.prev,
+        title: prevDocPage && prevDocPage.data.title,
+      },
+    },
+  }
+}
+
+export default DocTemplate
