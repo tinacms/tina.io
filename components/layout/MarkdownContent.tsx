8d37a9276863a99c2d5e8d04feb9a5ba24776661
Scott Byrne <scott.byrne@forestry.io>
2020-02-06 15:45:26 -0400

    styling

--- components/layout/MarkdownContent.tsx
+++ components/layout/MarkdownContent.tsx
@@ -3,6 +3,10 @@ import ReactMarkdown from 'react-markdown/with-html'
 // import ReactMarkdown from 'react-markdown'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import CodeStyle from '../styles/Code'
+
+import LinkSvg from '../../public/svg/link.svg'
+import styled from 'styled-components'
+
 var GithubSlugger = require('github-slugger')
 
 interface MarkdownContentProps {
@@ -26,25 +30,37 @@ function WithHeadings({ children, level }) {
   const slug = slugger.slug(value)
   return (
     <HeadingTag id={slug}>
-      <a href={`#${slug}`} aria-label={value} className="anchor">
-        <svg
-          aria-hidden="true"
-          focusable="false"
-          height="16"
-          version="1.1"
-          viewBox="0 0 16 16"
-          width="16"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"
-          ></path>
-        </svg>
-      </a>
+      <HeadingLink href={`#${slug}`} aria-label={value} className="anchor">
+        <LinkSvg />
+      </HeadingLink>
       {value}
     </HeadingTag>
   )
 }
+
+const HeadingLink = styled.a`
+  fill: var(--color-secondary);
+  opacity: 0.15;
+  display: flex;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  left: -0.25rem;
+  height: 1.3em;
+  transform: translate3d(-100%, 0, 0);
+  transition: all 180ms ease-out;
+
+  svg {
+    width: 1em;
+    height: 1em;
+  }
+
+  &:hover,
+  &:focus,
+  *:hover > & {
+    opacity: 1;
+  }
+`
 
 export function MarkdownContent({
   content,
