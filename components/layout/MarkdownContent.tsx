b88f56b6f1681740833bebb422962493e2d7f970
James O'Halloran <james.p.ohalloran@gmail.com>
2020-02-06 14:55:20 -0400

    add anchors to headings

--- components/layout/MarkdownContent.tsx
+++ components/layout/MarkdownContent.tsx
@@ -3,6 +3,7 @@ import ReactMarkdown from 'react-markdown/with-html'
 // import ReactMarkdown from 'react-markdown'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import CodeStyle from '../styles/Code'
+var GithubSlugger = require('github-slugger')
 
 interface MarkdownContentProps {
   content: string
@@ -18,6 +19,33 @@ function WithCodeStyles({ language, value }) {
   )
 }
 
+function WithHeadings({ children, level }) {
+  const HeadingTag = `h${level}` as any
+  const value = children[0].props.value
+  var slugger = new GithubSlugger()
+  const slug = slugger.slug(value)
+  return (
+    <HeadingTag id={slug}>
+      <a href={`#${slug}`} aria-label={value} className="anchor">
+        <svg
+          aria-hidden="true"
+          focusable="false"
+          height="16"
+          version="1.1"
+          viewBox="0 0 16 16"
+          width="16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"
+          ></path>
+        </svg>
+      </a>
+      {value}
+    </HeadingTag>
+  )
+}
+
 export function MarkdownContent({
   content,
   escapeHtml,
@@ -28,7 +56,7 @@ export function MarkdownContent({
       escapeHtml={escapeHtml ? escapeHtml : true}
       skipHtml={skipHtml ? skipHtml : false}
       source={content}
-      renderers={{ code: WithCodeStyles }}
+      renderers={{ code: WithCodeStyles, heading: WithHeadings }}
     />
   )
 }
