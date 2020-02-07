950d6c4f717b87ce803169d9afa8cfed2e1f993a
Scott Byrne <scott.byrne@forestry.io>
2020-02-07 12:13:29 -0400

    remove date and by

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -32,11 +32,9 @@ export default function BlogTemplate(props) {
               url:
                 'https://res.cloudinary.com/forestry-demo/image/upload/l_text:tuner-regular.ttf_70:' +
                 encodeURI(frontmatter.title) +
-                ',g_north_west,x_270,y_95,w_840,c_fit,co_rgb:EC4815/l_text:tuner-regular.ttf_30:By%20' +
+                ',g_north_west,x_270,y_95,w_840,c_fit,co_rgb:EC4815/l_text:tuner-regular.ttf_30:' +
                 encodeURI(frontmatter.author) +
-                '%20on%20' +
-                encodeURI(formatDate(frontmatter.date).replace(',', '')) +
-                '.,g_north_west,x_270,y_500,w_840,c_fit,co_rgb:241748/v1581087220/TinaCMS/tinacms-social-empty.png',
+                ',g_north_west,x_270,y_500,w_840,c_fit,co_rgb:241748/v1581087220/TinaCMS/tinacms-social-empty.png',
               width: 1200,
               height: 628,
               alt: frontmatter.title + ` | TinaCMS Blog`,
