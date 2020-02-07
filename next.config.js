63de3f75f85c3c5a45819a0a4b2a29bd7fea13f0
James O'Halloran <james.p.ohalloran@gmail.com>
2020-02-07 14:17:25 -0400

    use node_env for prod state

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 const withSvgr = require('next-svgr')
 require('dotenv').config()
 
-const isProd = process.env.environment == 'production'
 const dummyMailchimpEndpoint =
   'https://theDomainHere.us18.list-manage.com/subscribe/post?u=1512315231251&amp;id=0asd21t12e1'
 
 module.exports = withSvgr({
   env: {
-    MAILCHIMP_ENDPOINT: isProd
-      ? process.env.MAILCHIMP_ENDPOINT
-      : dummyMailchimpEndpoint,
+    MAILCHIMP_ENDPOINT:
+      process.env.NODE_ENV === 'production'
+        ? process.env.MAILCHIMP_ENDPOINT
+        : dummyMailchimpEndpoint,
     HUBSPOT_TEAMS_FORM_ID: process.env.HUBSPOT_TEAMS_FORM_ID,
     HUBSPOT_PORTAL_ID: process.env.HUBSPOT_PORTAL_ID,
     GTM_ID: process.env.GTM_ID,
