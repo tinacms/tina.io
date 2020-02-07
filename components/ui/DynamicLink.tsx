cad0434a75c0bdc1c9981759f812614ca9159e5b
James O'Halloran <james.p.ohalloran@gmail.com>
2020-02-07 13:05:25 -0400

    test dynamic links

--- components/ui/DynamicLink.tsx
+++ components/ui/DynamicLink.tsx
@@ -1,5 +1,5 @@
 import Link, { LinkProps } from 'next/link'
-import { string } from 'prop-types'
+import { getDynamicPath } from '../../utils/getDynamicPath'
 
 type ExtraProps = Omit<LinkProps, 'as' | 'href'>
 
@@ -9,29 +9,10 @@ interface DynamicLinkProps extends ExtraProps {
 }
 
 export const DynamicLink = ({ children, href, ...props }: DynamicLinkProps) => {
-  const dynamicHref = getDynamicUrl(href)
+  const dynamicHref = getDynamicPath(href)
   return (
     <Link href={dynamicHref} as={href} {...props}>
       {children}
     </Link>
   )
-}
-
-const getDynamicUrl = (url: string) => {
-  const docsPattern = new RegExp('(.)?/docs/(.)+')
-  if (docsPattern.test(url)) {
-    return '/docs/[...slug]'
-  }
-
-  const blogIndexPattern = new RegExp('(.)?/blog/page/[0-9]+')
-  if (blogIndexPattern.test(url)) {
-    return '/blog/page/[page_index]'
-  }
-
-  const blogPattern = new RegExp('(.)?/blog/(.)+')
-  if (blogPattern.test(url)) {
-    return '/blog/[slug]'
-  }
-
-  return url
 }
