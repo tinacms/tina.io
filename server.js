b278220fcd41efc8b301d7a6bd386440317472d6
James O'Halloran <james.p.ohalloran@gmail.com>
2020-02-11 12:56:39 -0400

    specify host

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.prepare().then(() => {
     return handle(req, res)
   })
 
-  server.listen(port, err => {
+  server.listen(port, '0.0.0.0', err => {
     if (err) throw err
     console.log(`> Ready on http://localhost:${port}`)
   })
