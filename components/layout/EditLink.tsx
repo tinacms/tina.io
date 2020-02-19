6225df5b15190c01bd966414f77622b9af1dda60
Scott Byrne <scott.byrne@forestry.io>
2020-02-19 10:52:37 -0400

    center popup window to open centered

--- components/layout/EditLink.tsx
+++ components/layout/EditLink.tsx
@@ -1,6 +1,23 @@
 import { useEffect } from 'react'
 import Cookies from 'js-cookie'
 
+function popupWindow(url, title, window, w, h) {
+  const y = window.top.outerHeight / 2 + window.top.screenY - h / 2
+  const x = window.top.outerWidth / 2 + window.top.screenX - w / 2
+  return window.open(
+    url,
+    title,
+    'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=' +
+      w +
+      ', height=' +
+      h +
+      ', top=' +
+      y +
+      ', left=' +
+      x
+  )
+}
+
 export const EditLink = () => {
   let authTab: Window
 
@@ -11,10 +28,12 @@ export const EditLink = () => {
   const onClick = async () => {
     localStorage.setItem('fork_full_name', '')
 
-    authTab = window.open(
+    authTab = popupWindow(
       `/github/start-auth?state=${authState}`,
       '_blank',
-      'fullscreen=no, width=1000, height=800'
+      window,
+      700,
+      500
     )
 
     window.addEventListener(
@@ -38,9 +57,9 @@ export const EditLink = () => {
   }, [])
 
   return (
-    <div onClick={onClick}>
-      <a>Edit this page</a>
-    </div>
+    <a href="#" onClick={onClick}>
+      Edit this page
+    </a>
   )
 }
 
