dbf9278928fa375ebc7b3e309c9e742b805eace4
Joel Huggett <joel@forestry.io>
2020-02-26 15:21:50 -0400

    fixed import error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 
-import { BlockTemplate } from 'tinacms'
+
 import { InlineForm, InlineBlocks, BlockText } from 'react-tinacms-inline'
 
 import { DefaultSeo } from 'next-seo'
