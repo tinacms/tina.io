11b842b23d5dcea1fe3ddebb5e76b7887a0e63bf
Scott Byrne <scott.byrne@forestry.io>
2020-03-05 12:45:19 -0400

    save indicator setup

--- components/layout/OpenAuthoringSiteForm.tsx
+++ components/layout/OpenAuthoringSiteForm.tsx
@@ -8,6 +8,7 @@ import { useCMS, useWatchFormValues, Form, TinaCMS, FieldMeta } from 'tinacms'
 import createDecorator from 'final-form-submit-listener'
 import Cookies from 'js-cookie'
 import { PRPlugin } from '../../open-authoring/PRPlugin'
+import { LoadingDots } from '../ui/LoadingDots'
 
 interface Props extends InlineFormProps {
   editMode: boolean
@@ -25,6 +26,8 @@ const OpenAuthoringSiteForm = ({
 }: Props) => {
   const [statefulPreviewError, setStatefulPreviewError] = useState(previewError)
   const cms = useCMS()
+  const [submitting, setSubmitting] = useState(false)
+
   useEffect(() => {
     /*
      ** Random Fix: sidebar state isn't updated properly
@@ -71,8 +74,9 @@ const OpenAuthoringSiteForm = ({
                 >
                   <UndoIconSvg /> Discard
                 </ToolbarButton>
-                <SaveButton primary onClick={form.submit}>
-                  Save Page
+                <SaveButton primary onClick={form.submit} busy={submitting}>
+                  {submitting && <LoadingDots />}
+                  {!submitting && `Save Page`}
                 </SaveButton>
               </>
             ) : (
@@ -199,6 +203,10 @@ export const ToolbarButton = styled(Button)`
   display: flex;
   align-items: center;
   white-space: nowrap;
+
+  &:focus {
+    outline: none;
+  }
 
   svg {
     fill: currentColor;
