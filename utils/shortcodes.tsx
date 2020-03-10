b33e018e91e48df9b431680c4a6b18a62f9f824c
DJ <dj@walker.dev>
2020-03-10 17:28:44 -0400

    alert demo: use styled button

--- utils/shortcodes.tsx
+++ utils/shortcodes.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import { useCMS } from 'tinacms'
+import { Button } from '../components/ui'
 
 export function AlertTest({ type, message, timeout, buttonText }) {
   const cms = useCMS()
   const sendAlert = React.useCallback(() => {
     cms.alerts[type](message, timeout)
   }, [type, message, timeout])
-  return <button onClick={sendAlert}>{buttonText || 'Try it'}</button>
+  return (
+    <Button color="primary" onClick={sendAlert}>
+      {buttonText || 'Try it'}
+    </Button>
+  )
 }
