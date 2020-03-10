da1ea8fe0a89b0386905a0b83f6c5b59d10b2db2
Jeff See <jeffsee.55@gmail.com>
2020-03-10 11:28:01 -0700

    Add https

--- pages/github/start-auth.tsx
+++ pages/github/start-auth.tsx
@@ -8,7 +8,7 @@ export default function StartAuth() {
     const urlParams = new URLSearchParams(window.location.search)
     const authState = urlParams.get('state')
     window.location.assign(
-      `https://github.com/login/oauth/authorize?scope=public_repo&client_id=${process.env.GITHUB_CLIENT_ID}&state=${authState}&redirect_uri=${window.location.hostname}/github/authorizing`
+      `https://github.com/login/oauth/authorize?scope=public_repo&client_id=${process.env.GITHUB_CLIENT_ID}&state=${authState}&redirect_uri=https://${window.location.hostname}/github/authorizing`
     )
   }
   return (
