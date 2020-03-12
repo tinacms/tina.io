1b89bc08326510fe2621b9356c5ca72357dd8bb8
Nolan Phillips <ncphillips.19@gmail.com>
2020-03-12 11:51:59 -0300

    refactor: guard clause instead of ternary

--- utils/github/sourceProviderConnection.ts
+++ utils/github/sourceProviderConnection.ts
@@ -18,17 +18,20 @@ export interface PreviewData {
 export const getGithubDataFromPreviewProps = (
   previewData?: PreviewData
 ): Response => {
-  return previewData
-    ? {
-        sourceProviderConnection: {
-          forkFullName: previewData.fork_full_name,
-          headBranch: previewData.head_branch || 'master',
-          baseRepoFullName: process.env.REPO_FULL_NAME,
-        },
-        accessToken: previewData.github_access_token,
-      }
-    : {
-        sourceProviderConnection: null,
-        accessToken: null,
-      }
+
+  if (!previewData) {
+    return {
+      sourceProviderConnection: null,
+      accessToken: null,
+    }
+  }
+
+  return {
+    accessToken: previewData.github_access_token,
+    sourceProviderConnection: {
+      forkFullName: previewData.fork_full_name,
+      headBranch: previewData.head_branch || 'master',
+      baseRepoFullName: process.env.REPO_FULL_NAME,
+    },
+  }
 }
