6e127b4a1e85b5ffdcd5e01e975b09a485fd7f53
James O'Halloran <james.p.ohalloran@gmail.com>
2020-03-20 11:45:56 -0300

    split out github create-access-token function into own file

--- pages/api/create-github-access-token.ts
+++ pages/api/create-github-access-token.ts
@@ -1,27 +1,3 @@
-const qs = require('qs')
-import { serialize } from 'cookie'
+import createAccessToken from '../../github/api/create-access-token'
 
-const { createAccessToken } = require('../../open-authoring/github/api')
-
-export default (req, res) => {
-  createAccessToken(
-    process.env.GITHUB_CLIENT_ID,
-    process.env.GITHUB_CLIENT_SECRET,
-    req.query.code,
-    req.query.state
-  ).then(tokenResp => {
-    const { access_token, error } = qs.parse(tokenResp.data)
-    if (error) {
-      res.status(400).json({ error })
-    } else {
-      res.setHeader(
-        'Set-Cookie',
-        serialize('github_access_token', access_token, {
-          path: '/',
-          httpOnly: true,
-        })
-      )
-      res.status(200).json({})
-    }
-  })
-}
+export default createAccessToken
