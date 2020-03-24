38605e6f171cbe7908b424dbec5cf0d282978eec
James O'Halloran <james.p.ohalloran@gmail.com>
2020-03-24 10:23:53 -0300

    remove duplicate generic error handling in interpretError

--- open-authoring/error-interpreter/index.ts
+++ open-authoring/error-interpreter/index.ts
@@ -8,33 +8,17 @@ export default async function interpretError(
   error: OpenAuthoringError,
   github: any
 ): Promise<ActionableModalOptions> {
-  if (!error || !error.code) {
-    console.warn('Error Interpreter: called without an error')
-    const message = error?.message || 'An error occured.'
-    return {
-      title: 'Error',
-      message,
-      actions: [
-        {
-          name: 'Continue',
-          action: () => enterAuthFlow(github),
-        },
-        {
-          name: 'Cancel',
-          action: refresh,
-        },
-      ], // Action buttons
+  if (error.code) {
+    switch (parseInt(error.code.toString()[0])) {
+      case 4: {
+        return await interpretClientError(error, github)
+      }
+      case 5: {
+        return interpretServerError(error)
+      }
     }
   }
 
-  switch (parseInt(error.code.toString()[0])) {
-    case 4: {
-      return await interpretClientError(error, github)
-    }
-    case 5: {
-      return interpretServerError(error)
-    }
-  }
   console.warn('Error Interpreter: Could not interpret error ' + error.code)
   return {
     title: `Error  ${error.code}`,
