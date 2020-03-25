4ada47bd0e5269c2d1d5486b608d37b2fbc9ae62
James O'Halloran <james.p.ohalloran@gmail.com>
2020-03-25 11:19:53 -0300

    configurable autoAuth queryParam key

--- open-authoring/AutoAuthModal.tsx
+++ open-authoring/AutoAuthModal.tsx
@@ -3,13 +3,17 @@ import { useState } from 'react'
 import { enterEditMode } from './authFlow'
 import { ActionableModal } from '../components/ui'
 
+interface Props {
+  autoAuthKey?: string // query param key which kicks off auth flow
+}
+
 // This component checks for the autoAuth query parameter, and
 // conditionally renders the modal to kick off the auth flow
-const AutoAuthModal = () => {
+const AutoAuthModal = ({ autoAuthKey = 'autoAuth' }: Props) => {
   const openAuthoring = useOpenAuthoring()
 
   const [showAuthModal, setShowAuthModal] = useState(
-    typeof window !== 'undefined' && window.location.href.includes('autoAuth')
+    typeof window !== 'undefined' && window.location.href.includes(autoAuthKey)
   )
   const cancelAuth = () => {
     window.history.replaceState(
