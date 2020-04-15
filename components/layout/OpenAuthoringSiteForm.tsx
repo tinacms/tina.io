4aa2fd14ddbaa7150e232c4015d7454d6ef4766a
Scott Byrne <scott.byrne@forestry.io>
2020-04-15 13:44:19 -0300

    style: add lock indicator for master

--- components/layout/OpenAuthoringSiteForm.tsx
+++ components/layout/OpenAuthoringSiteForm.tsx
@@ -83,7 +83,7 @@ const BranchSwitcher = ({ forkName }: { forkName: string }) => {
     <SelectWrapper>
       <SelectBox onClick={() => setOpen(!open)} open={open}>
         <SelectLabel>Branch</SelectLabel>
-        <SelectCurrent>master</SelectCurrent>
+        <SelectCurrent>branch-switcher</SelectCurrent>
         <ChevronDownIcon />
       </SelectBox>
       <SelectDropdown open={open}>
@@ -96,8 +96,10 @@ const BranchSwitcher = ({ forkName }: { forkName: string }) => {
           }}
         >
           <SelectList>
-            <SelectOption>master</SelectOption>
-            <SelectOption current>style-fixes</SelectOption>
+            <SelectOption locked>
+              <LockedIcon /> master
+            </SelectOption>
+            <SelectOption current>branch-switcher</SelectOption>
             <SelectOption>new-blog-post</SelectOption>
             <SelectOption>some-branch-name-that-is-out-of-control</SelectOption>
             <SelectOption>refactor/tinacms-github-next-package</SelectOption>
@@ -171,6 +173,7 @@ const DropdownActions = styled.div`
 
 export interface SelectOptionProps {
   current?: boolean
+  locked?: boolean
 }
 
 const SelectOption = styled.button<SelectOptionProps>`
@@ -191,6 +194,14 @@ const SelectOption = styled.button<SelectOptionProps>`
   overflow: hidden;
   flex: 0 0 auto;
 
+  svg {
+    width: 20px;
+    height: auto;
+    margin: -4px -4px -4px -4px;
+    fill: currentColor;
+    opacity: 0.7;
+  }
+
   :first-child {
     padding-top: 8px;
   }
@@ -210,6 +221,13 @@ const SelectOption = styled.button<SelectOptionProps>`
       font-weight: bold;
       color: var(--tina-color-primary);
       background-color: var(--tina-color-grey-1);
+    `};
+
+  ${p =>
+    p.locked &&
+    css`
+      color: var(--tina-color-grey-4);
+      pointer-events: none;
     `};
 `
 
@@ -340,3 +358,18 @@ const SelectCurrent = styled.span`
   text-align: left;
   line-height: 17px;
 `
+
+export const LockedIcon = () => (
+  <svg
+    width="32"
+    height="32"
+    viewBox="0 0 32 32"
+    fill="inherit"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M25 16.5C25 15.3419 23.9909 14.4 22.75 14.4H21.625V10.25C21.625 7.35515 19.1016 5 16 5C12.8984 5 10.375 7.35515 10.375 10.25V14.4H9.25C8.00912 14.4 7 15.3419 7 16.5V23.9C7 25.0581 8.00912 26 9.25 26H22.75C23.9909 26 25 25.0581 25 23.9V16.5ZM12.625 10.25C12.625 8.5133 14.1392 7.1 16 7.1C17.8608 7.1 19.375 8.5133 19.375 10.25V14.4H12.625V10.25Z"
+      fill="inherit"
+    />
+  </svg>
+)
