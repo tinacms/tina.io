125a5b0c649710b8740aa80ab1be85db74fd0089
Nolan Phillips <ncphillips.19@gmail.com>
2020-04-29 14:52:15 -0300

    refactor: extract blog post creator

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -18,54 +18,7 @@ export const Layout = styled(
   ({ children, color, preview, ...styleProps }: LayoutProps) => {
     const router = useRouter()
 
-    const CreateBlogPlugin = useMemo(
-      () =>
-        new MarkdownCreatorPlugin({
-          label: 'New Blog Post',
-          filename: form => {
-            const slug = slugify(form.title)
-            return `content/blog/${slug}.md`
-          },
-          fields: [
-            {
-              name: 'title',
-              component: 'text',
-              label: 'Title',
-              placeholder: 'My New Post',
-              description: 'The title of the new blog post.',
-            },
-            {
-              label: 'Date',
-              name: 'date',
-              component: 'date',
-              description: 'The default will be today',
-            },
-            {
-              label: 'Author',
-              description: 'Who wrote this, yo?',
-              name: 'author',
-              component: 'text',
-            },
-          ],
-          frontmatter: postInfo => ({
-            title: postInfo.title,
-            date: moment(postInfo.date ? postInfo.date : new Date()).format(),
-            author: postInfo.author ? postInfo.author : `Jane Doe`,
-          }),
-          body: postInfo => `New post, who dis?`,
-          afterCreate: response => {
-            let url = fileToUrl(
-              response.content.path.split('content')[1],
-              'blog'
-            )
-
-            window.location.href = `/blog/${url}`
-          },
-        }),
-      [preview]
-    )
-
-    usePlugin(CreateBlogPlugin)
+    usePlugin(BlogPostCreatorPlugin)
     usePlugin(ReleaseNotesCreatorPlugin)
 
     return (
@@ -88,6 +41,46 @@ export const Layout = styled(
   flex: 1 1 auto;
   min-height: 100%;
 `
+
+const BlogPostCreatorPlugin = new MarkdownCreatorPlugin({
+  label: 'New Blog Post',
+  filename: form => {
+    const slug = slugify(form.title)
+    return `content/blog/${slug}.md`
+  },
+  fields: [
+    {
+      name: 'title',
+      component: 'text',
+      label: 'Title',
+      placeholder: 'My New Post',
+      description: 'The title of the new blog post.',
+    },
+    {
+      label: 'Date',
+      name: 'date',
+      component: 'date',
+      description: 'The default will be today',
+    },
+    {
+      label: 'Author',
+      description: 'Who wrote this, yo?',
+      name: 'author',
+      component: 'text',
+    },
+  ],
+  frontmatter: postInfo => ({
+    title: postInfo.title,
+    date: moment(postInfo.date ? postInfo.date : new Date()).format(),
+    author: postInfo.author ? postInfo.author : `Jane Doe`,
+  }),
+  body: postInfo => `New post, who dis?`,
+  afterCreate: response => {
+    let url = fileToUrl(response.content.path.split('content')[1], 'blog')
+
+    window.location.href = `/blog/${url}`
+  },
+})
 
 const ReleaseNotesCreatorPlugin = new MarkdownCreatorPlugin({
   label: 'New Release Notes',
