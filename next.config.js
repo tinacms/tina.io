5ae3b5c230a44d61f23c47d44fae39fec58890c6
Nolan Phillips <ncphillips.19@gmail.com>
2020-06-19 11:39:32 -0300

    add webpack plugins for bundle analyzing and moment locale stripping
    reduces bundle size by 428kb
    gives us a visual into the websites bundle

--- next.config.js
+++ next.config.js
@@ -1,44 +1,54 @@
 const { aliasTinaDev } = require('@tinacms/webpack-helpers')
 
+const MomentLocalesPlugin = require('moment-locales-webpack-plugin')
 const withSvgr = require('next-svgr')
+
 require('dotenv').config()
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 
 const dummyMailchimpEndpoint =
   'https://theDomainHere.us18.list-manage.com/subscribe/post?u=1512315231251&amp;id=0asd21t12e1'
 
-module.exports = withSvgr({
-  env: {
-    MAILCHIMP_ENDPOINT:
-      process.env.NODE_ENV === 'production'
-        ? process.env.MAILCHIMP_ENDPOINT
-        : dummyMailchimpEndpoint,
-    HUBSPOT_TEAMS_FORM_ID: process.env.HUBSPOT_TEAMS_FORM_ID,
-    HUBSPOT_PORTAL_ID: process.env.HUBSPOT_PORTAL_ID,
-    GTM_ID: process.env.GTM_ID,
-    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-    BASE_REPO_FULL_NAME: process.env.BASE_REPO_FULL_NAME,
-    BASE_BRANCH: process.env.BASE_BRANCH,
-  },
-  exportTrailingSlash: true,
-  exportPathMap: async function() {
-    return {}
-  },
-  webpack(config) {
-    if (process.env.TINA) {
-      let watch
-      if (process.env.TINA_WATCH) {
-        watch = process.env.TINA_WATCH.split(',')
+module.exports = withBundleAnalyzer(
+  withSvgr({
+    env: {
+      MAILCHIMP_ENDPOINT:
+        process.env.NODE_ENV === 'production'
+          ? process.env.MAILCHIMP_ENDPOINT
+          : dummyMailchimpEndpoint,
+      HUBSPOT_TEAMS_FORM_ID: process.env.HUBSPOT_TEAMS_FORM_ID,
+      HUBSPOT_PORTAL_ID: process.env.HUBSPOT_PORTAL_ID,
+      GTM_ID: process.env.GTM_ID,
+      GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+      BASE_REPO_FULL_NAME: process.env.BASE_REPO_FULL_NAME,
+      BASE_BRANCH: process.env.BASE_BRANCH,
+    },
+    exportTrailingSlash: true,
+    exportPathMap: async function() {
+      return {}
+    },
+    webpack(config) {
+      if (process.env.TINA) {
+        let watch
+        if (process.env.TINA_WATCH) {
+          watch = process.env.TINA_WATCH.split(',')
+        }
+        aliasTinaDev(config, process.env.TINA, watch)
       }
-      aliasTinaDev(config, process.env.TINA, watch)
-    }
-    config.module.rules.push({
-      test: /\.md$/,
-      use: 'raw-loader',
-    })
-    config.node = {
-      fs: 'empty',
-    }
+      config.module.rules.push({
+        test: /\.md$/,
+        use: 'raw-loader',
+      })
+      config.node = {
+        fs: 'empty',
+      }
 
-    return config
-  },
-})
+      config.plugins.push(new MomentLocalesPlugin())
+
+      return config
+    },
+  })
+)
