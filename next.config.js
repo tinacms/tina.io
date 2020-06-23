9641abe7e7ad4b2da18c0735ddc381a38509e963
Nolan Phillips <ncphillips.19@gmail.com>
2020-06-23 12:32:02 -0300

    extract config to make the exports line cleaner

--- next.config.js
+++ next.config.js
@@ -12,43 +12,45 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const dummyMailchimpEndpoint =
   'https://theDomainHere.us18.list-manage.com/subscribe/post?u=1512315231251&amp;id=0asd21t12e1'
 
-module.exports = withBundleAnalyzer(
-  withSvgr({
-    env: {
-      MAILCHIMP_ENDPOINT:
-        process.env.NODE_ENV === 'production'
-          ? process.env.MAILCHIMP_ENDPOINT
-          : dummyMailchimpEndpoint,
-      HUBSPOT_TEAMS_FORM_ID: process.env.HUBSPOT_TEAMS_FORM_ID,
-      HUBSPOT_PORTAL_ID: process.env.HUBSPOT_PORTAL_ID,
-      GTM_ID: process.env.GTM_ID,
-      GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-      BASE_REPO_FULL_NAME: process.env.BASE_REPO_FULL_NAME,
-      BASE_BRANCH: process.env.BASE_BRANCH,
-    },
-    exportTrailingSlash: true,
-    exportPathMap: async function() {
-      return {}
-    },
-    webpack(config) {
-      if (process.env.TINA) {
-        let watch
-        if (process.env.TINA_WATCH) {
-          watch = process.env.TINA_WATCH.split(',')
-        }
-        aliasTinaDev(config, process.env.TINA, watch)
+const config = {
+  env: {
+    MAILCHIMP_ENDPOINT:
+      process.env.NODE_ENV === 'production'
+        ? process.env.MAILCHIMP_ENDPOINT
+        : dummyMailchimpEndpoint,
+    HUBSPOT_TEAMS_FORM_ID: process.env.HUBSPOT_TEAMS_FORM_ID,
+    HUBSPOT_PORTAL_ID: process.env.HUBSPOT_PORTAL_ID,
+    GTM_ID: process.env.GTM_ID,
+    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    BASE_REPO_FULL_NAME: process.env.BASE_REPO_FULL_NAME,
+    BASE_BRANCH: process.env.BASE_BRANCH,
+  },
+  exportTrailingSlash: true,
+  exportPathMap: async function() {
+    return {}
+  },
+  webpack(config) {
+    if (process.env.TINA) {
+      let watch
+      if (process.env.TINA_WATCH) {
+        watch = process.env.TINA_WATCH.split(',')
       }
-      config.module.rules.push({
-        test: /\.md$/,
-        use: 'raw-loader',
-      })
-      config.node = {
-        fs: 'empty',
-      }
+      aliasTinaDev(config, process.env.TINA, watch)
+    }
 
-      config.plugins.push(new MomentLocalesPlugin())
+    config.module.rules.push({
+      test: /\.md$/,
+      use: 'raw-loader',
+    })
 
-      return config
-    },
-  })
-)
+    config.node = {
+      fs: 'empty',
+    }
+
+    config.plugins.push(new MomentLocalesPlugin())
+
+    return config
+  },
+}
+
+module.exports = withBundleAnalyzer(withSvgr(config))
