3f5f61c33eb02fc9f5bad6a339d0edfe7444ab53
Kendall Strautman <kendallstrautman@gmail.com>
2020-07-24 10:16:04 -0700

    fix: newsletter signup env?

--- next.config.js
+++ next.config.js
@@ -15,9 +15,7 @@ const dummyMailchimpEndpoint =
 const config = {
   env: {
     MAILCHIMP_ENDPOINT:
-      process.env.NODE_ENV === 'production'
-        ? process.env.MAILCHIMP_ENDPOINT
-        : dummyMailchimpEndpoint,
+      process.env.MAILCHIMP_ENDPOINT || dummyMailchimpEndpoint,
     HUBSPOT_TEAMS_FORM_ID: process.env.HUBSPOT_TEAMS_FORM_ID,
     HUBSPOT_PORTAL_ID: process.env.HUBSPOT_PORTAL_ID,
     GTM_ID: process.env.GTM_ID,
