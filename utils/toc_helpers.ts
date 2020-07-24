6b25d4d2bda24ac42766694b4fa0ac42a4a130a8
Kendall Strautman <kendallstrautman@gmail.com>
2020-07-24 16:05:53 -0700

    refactor: some toc helper variables to constants

--- utils/toc_helpers.ts
+++ utils/toc_helpers.ts
@@ -7,7 +7,7 @@ interface Heading {
 function createHeadings(
   contentRef: React.RefObject<HTMLDivElement>
 ): Heading[] {
-  let headings = []
+  const headings = []
   const htmlElements = contentRef.current.querySelectorAll(
     'h1, h2, h3, h4, h5, h6'
   )
@@ -32,29 +32,28 @@ export function createTocListener(
   const headings = createHeadings(contentRef)
 
   const throttledScroll = () => {
-    let scrollPos = window.scrollY
-    let newActiveIds = []
-    let activeHeadingCandidates = headings.filter(heading => {
+    const scrollPos = window.scrollY
+    const newActiveIds = []
+    const activeHeadingCandidates = headings.filter(heading => {
       return heading.offset - scrollPos < BASE_OFFSET
     })
 
-    let activeHeading =
+    const activeHeading =
       activeHeadingCandidates.length > 0
         ? activeHeadingCandidates.reduce((prev, current) =>
             prev.offset > current.offset ? prev : current
           )
         : {}
-
     newActiveIds.push(activeHeading.id)
 
     if (activeHeading.level != 'H2') {
-      let activeHeadingParentCandidates =
+      const activeHeadingParentCandidates =
         activeHeadingCandidates.length > 0
           ? activeHeadingCandidates.filter(heading => {
               return heading.level == 'H2'
             })
           : []
-      let activeHeadingParent =
+      const activeHeadingParent =
         activeHeadingParentCandidates.length > 0
           ? activeHeadingParentCandidates.reduce((prev, current) =>
               prev.offset > current.offset ? prev : current
