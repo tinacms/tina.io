6e54196096a5159bbd25f24f34b11476357768ef
Joel Huggett <joel@forestry.io>
2020-07-30 16:09:33 -0300

    styled last edited

--- utils/useLastEdited.ts
+++ utils/useLastEdited.ts
@@ -1,22 +1,21 @@
 import { useCMS } from "tinacms"
 import { useEffect } from "react"
 import createDecorator from "final-form-calculate"
-import { formatDate } from "./blog_helpers"
 
 
 export function useLastEdited(form) {
-    const cms = useCMS()
+  const cms = useCMS()
 
-    useEffect(() => {
-        if (cms.disabled) { return }
-        const decorator = createDecorator(
-            {
-                field: /.*/,
-                updates: {
-                    'frontmatter.last_edited': () => (new Date()).toISOString()
-                }
-            }
-        )
-        return decorator(form.finalForm)
-    }, [form.id])
+  useEffect(() => {
+    if (cms.disabled) { return }
+    const decorator = createDecorator(
+      {
+        field: /.*/,
+        updates: {
+          'frontmatter.last_edited': () => (new Date()).toISOString()
+        }
+      }
+    )
+    return decorator(form.finalForm)
+  }, [form.id])
 }
