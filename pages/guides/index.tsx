5dfdfba58b73b138b8c651b252f146d5e8d66e7a
Kendall Strautman <kendallstrautman@gmail.com>
2020-08-05 12:01:58 -0700

    feat(guides): adds docs nav to guides index

--- pages/guides/index.tsx
+++ pages/guides/index.tsx
@@ -1,41 +1,13 @@
 import { getGuideNavProps } from 'utils/guide_helpers'
 import { readMarkdownFile } from 'utils/getMarkdownFile'
-import React, { useMemo } from 'react'
-import { useRouter } from 'next/router'
+import { getDocsNav } from 'utils/docs/getDocProps'
+import React from 'react'
 import { DocsLayout, Wrapper, MarkdownContent } from 'components/layout'
 import { NextSeo } from 'next-seo'
 import styled from 'styled-components'
 
-const GuideTemplate = props => {
-  const data = props.markdownFile.data
-  const frontmatter = data.frontmatter
-  const markdownBody = data.markdownBody
-  const excerpt = props.markdownFile.data.excerpt
-
-  let navData = useMemo(() => {
-    if (props.currentGuide) {
-      return [
-        {
-          title: props.currentGuide.title,
-          id: props.currentGuide.title,
-          collapsible: false,
-          items: props.currentGuide.steps,
-          returnLink: {
-            url: '/guides',
-            label: '‹ Back to Guides',
-          },
-        },
-      ]
-    } else {
-      return props.allGuides.sort((a, b) => a.weight > b.weight)
-    }
-  }, [props.currentGuide, props.allGuides])
-
-  const router = useRouter()
-
-  const guideTitle = props.currentGuide
-    ? props.currentGuide.title
-    : 'TinaCMS Guides'
+const GuideTemplate = ({ markdownFile, navItems }) => {
+  const { frontmatter, markdownBody, excerpt } = markdownFile.data
 
   return (
     <>
@@ -50,16 +22,17 @@ const GuideTemplate = props => {
             {
               url:
                 'https://res.cloudinary.com/forestry-demo/image/upload/l_text:tuner-regular.ttf_90_center:' +
-                encodeURIComponent(guideTitle) +
+                encodeURIComponent('TinaCMS Guides') +
                 ',g_center,x_0,y_50,w_850,c_fit,co_rgb:EC4815/v1581087220/TinaCMS/tinacms-social-empty-docs.png',
               width: 1200,
               height: 628,
-              alt: guideTitle,
+              alt: 'TinaCMS Guides',
             },
           ],
         }}
       />
-      <DocsLayout navItems={navData}>
+      {/**TODO: force nav scroll to guides */}
+      <DocsLayout navItems={navItems}>
         <GuideWrapper narrow>
           <h1>{frontmatter.title}</h1>
           <hr />
@@ -70,18 +43,6 @@ const GuideTemplate = props => {
   )
 }
 
-interface NavSection {
-  id: string
-  slug: string
-  title: string
-  items: NavSection[]
-  collapsible?: boolean
-  returnLink?: {
-    url: string
-    label: string
-  }
-}
-
 const GuideWrapper = styled(Wrapper)`
   padding-top: 2rem;
   padding-bottom: 3rem;
@@ -89,7 +50,7 @@ const GuideWrapper = styled(Wrapper)`
 
 export default GuideTemplate
 
-export const getStaticProps = async () => {
+export const getStaticProps = async ({ preview }) => {
   // @ts-ignore
   const path = __non_webpack_require__('path')
   // the following line will cause all content files to be available in a serverless context
@@ -97,11 +58,11 @@ export const getStaticProps = async () => {
   return {
     props: {
       slug: '/guides',
-      currentGuide: null,
       markdownFile: await readMarkdownFile(
         path.resolve(process.cwd(), './content/guides/index.md')
       ),
       allGuides: await getGuideNavProps(),
+      navItems: await getDocsNav(preview, {}),
     },
   }
 }
