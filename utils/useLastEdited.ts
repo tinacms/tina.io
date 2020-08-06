7be899a83be974dfdefa0503f36aa4b5fdf235b5
Nolan Phillips <ncphillips.19@gmail.com>
2020-08-06 10:46:12 -0300

    refactor: extract useFormDecorator from useLastEdited

--- utils/useLastEdited.ts
+++ utils/useLastEdited.ts
@@ -1,21 +1,26 @@
-import { useCMS } from "tinacms"
-import { useEffect } from "react"
-import createDecorator from "final-form-calculate"
+import { useCMS, Form } from 'tinacms'
+import { useEffect } from 'react'
+import createDecorator, { Calculation } from 'final-form-calculate'
 
+export function useLastEdited(form: Form) {
+  useFormDecorator(form, {
+    field: /.*/,
+    updates: {
+      'frontmatter.last_edited': () => new Date().toISOString(),
+    },
+  })
+}
 
-export function useLastEdited(form) {
+function useFormDecorator(form: Form, ...calculations: Calculation[]) {
   const cms = useCMS()
 
   useEffect(() => {
-    if (cms.disabled) { return }
-    const decorator = createDecorator(
-      {
-        field: /.*/,
-        updates: {
-          'frontmatter.last_edited': () => (new Date()).toISOString()
-        }
-      }
-    )
+    if (cms.disabled) {
+      return
+    }
+
+    const decorator = createDecorator(...calculations)
+
     return decorator(form.finalForm)
   }, [form.id])
 }
