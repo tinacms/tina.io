13be9ae493a6bdbba04aaab6b6f18d6b6993d25e
DJ <dj@walker.dev>
2020-08-11 14:39:28 -0400

    update package search index when page updates

--- utils/docs/getPackageProps.ts
+++ utils/docs/getPackageProps.ts
@@ -4,6 +4,10 @@ import toc from 'markdown-toc'
 
 const atob = require('atob')
 import { slugifyTocHeading } from './slugifyToc'
+import algoliasearch from 'algoliasearch'
+import { stripMarkdown } from '../../utils/blog_helpers'
+
+const MAX_BODY_LENGTH = 200
 
 // @ts-ignore
 const path = __non_webpack_require__('path')
@@ -70,6 +74,17 @@ export async function getPackageProps(
   const currentDoc = await axios.get(currentPackage.link)
   const content = b64DecodeUnicode(currentDoc.data.content)
 
+  if (process.env.VERCEL_GITHUB_COMMIT_REF === 'master' && !preview) {
+    // this should only run when performing incremental SSG on prod
+
+    updateSearchIndex({
+      name: currentPackage.name,
+      content: content,
+    }).catch(e => {
+      console.error(e)
+    })
+  }
+
   return {
     revalidate: 24 * HOURS,
     props: {
@@ -94,3 +109,47 @@ export async function getPackageProps(
 
 const MINUTES = 60 // seconds
 const HOURS = 60 * MINUTES
+
+const updateSearchIndex = async ({ name, content }) => {
+  const client = algoliasearch(
+    process.env.ALGOLIA_APP_ID,
+    process.env.ALGOLIA_ADMIN_KEY
+  )
+  const index = client.initIndex('Tina-Package-Docs')
+  let oldObject
+  try {
+    const objectResult = await index.findObject(hit => hit.objectID === name)
+    oldObject = objectResult.object
+  } catch (e) {
+    oldObject = null
+  }
+  const strippedContent = stripH1(content || '')
+
+  const newObject = {
+    objectID: name,
+    lastUpdated: Date.now(),
+    excerpt: (await stripMarkdown(strippedContent)).substring(
+      0,
+      MAX_BODY_LENGTH
+    ),
+    content: await stripMarkdown(strippedContent),
+  }
+
+  if (shouldUpdate(oldObject, newObject)) {
+    await index.saveObject(newObject)
+  }
+}
+
+const stripH1 = content => {
+  const captureH1 = /^#\s+.*\s*$/gm
+  const result = captureH1.exec(content)
+  if (!result) return content
+  return content.substring(result[0].length)
+}
+
+const shouldUpdate = (oldObject, newObject) => {
+  if (!oldObject) return true
+  if (oldObject.content != newObject.content) return true
+  if (oldObject.excerpt != newObject.excerpt) return true
+  if (Date.now() - oldObject.lastUpdated > 72 * HOURS) return true
+}
