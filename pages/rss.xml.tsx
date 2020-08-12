8e3b167b9ac5698802bdc66353fc28d6fba9fde2
DJ <dj@walker.dev>
2020-08-11 21:46:38 -0400

    fix rss feed

--- pages/rss.xml.tsx
+++ pages/rss.xml.tsx
@@ -1,17 +1,18 @@
 import { NextPageContext } from 'next'
-import matter from 'gray-matter'
 
 import { formatExcerpt, orderPosts } from '../utils'
+import fetchBlogs from '../data-api/fetchBlogs'
 
 export default function FeedPage() {
-  // xml is passed to the browser and written via getInitialProps
+  // xml is passed to the browser and written via getServerSideProps
 }
 
-const blogPostsRssXml = blogPosts => {
+const blogPostsRssXml = async blogPosts => {
   let latestPostDate: string = ''
   let rssItemsXml = ''
-  blogPosts.forEach(post => {
+  for (let post of blogPosts) {
     const postDate = Date.parse(post.data.date)
+    const excerpt = await formatExcerpt(post.content, 1000)
     if (!latestPostDate || postDate > Date.parse(latestPostDate)) {
       latestPostDate = post.createdAt
     }
@@ -19,18 +20,18 @@ const blogPostsRssXml = blogPosts => {
       <title>${post.data.title}</title>
       <link>https://tinacms.org/blog/${post.data.slug}</link>
       <pubDate>${post.data.date}</pubDate>
-      <description><![CDATA[${post.content}]]>
+      <description><![CDATA[${excerpt}]]>
       </description>
     </item>`
-  })
+  }
   return {
     rssItemsXml,
     latestPostDate,
   }
 }
 
-const getRssXml = blogPosts => {
-  const { rssItemsXml, latestPostDate } = blogPostsRssXml(blogPosts)
+const getRssXml = async blogPosts => {
+  const { rssItemsXml, latestPostDate } = await blogPostsRssXml(blogPosts)
   return `<?xml version="1.0" encoding="UTF-8" ?>
   <rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom">
     <channel>
@@ -48,29 +49,8 @@ FeedPage.getInitialProps = async function({ res }: NextPageContext) {
   if (!res) {
     return
   }
-  const blogPosts = await (async context => {
-    const keys = context.keys()
-    const values = keys.map(context)
-    const data = await keys.map(async (key: string, index: number) => {
-      // Create slug from filename
-      const slug = key
-        .replace(/^.*[\\\/]/, '')
-        .split('.')
-        .slice(0, -1)
-        .join('.')
-      const value = values[index]
-      // Parse yaml metadata & markdownbody in document
-      const post = matter(value.default)
-      const formattedContent = await formatExcerpt(post.content)
-      return {
-        data: { ...post.data, slug },
-        content: formattedContent,
-      }
-    })
-
-    return orderPosts(data)
-  })((require as any).context('../content/blog', true, /\.md$/))
+  const blogPosts = orderPosts(await fetchBlogs())
   res.setHeader('Content-Type', 'text/xml')
-  res.write(getRssXml(blogPosts))
+  res.write(await getRssXml(blogPosts))
   res.end()
 }
