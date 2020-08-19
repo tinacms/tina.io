46d7113d63e388b647b7511c042d7fdcbf2fe4b9
DJ <dj@walker.dev>
2020-08-19 12:04:09 -0400

    slugifyToc: recursively strip emphasis

--- utils/docs/slugifyToc.ts
+++ utils/docs/slugifyToc.ts
@@ -4,5 +4,9 @@ const captureEmphasis = /(.*)(?<!\\)_(.*)(?<!\\)_(.*)/
 export const slugifyTocHeading = heading => {
   const captured = captureEmphasis.exec(heading)
   const strippedHeading = captured ? captured.slice(1).join('') : heading
+  // do it again if there are more emphasis chars
+  if (captureEmphasis.exec(strippedHeading)) {
+    return slugifyTocHeading(strippedHeading)
+  }
   return toc.slugify(strippedHeading)
 }
