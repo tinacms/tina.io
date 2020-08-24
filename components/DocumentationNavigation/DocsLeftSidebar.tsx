5819b0777e87264eb37b2491e2b42c563234a495
Scott Byrne <scott.byrne@forestry.io>
2020-08-24 14:15:59 -0300

    fix safari transparent color bug

--- components/DocumentationNavigation/DocsLeftSidebar.tsx
+++ components/DocumentationNavigation/DocsLeftSidebar.tsx
@@ -23,7 +23,7 @@ export const DocsLeftSidebar = styled.div<{ open: boolean }>`
   > ul {
     flex: 1 1 auto;
     padding: 1rem 1px 1rem 0;
-    background: linear-gradient(to bottom, white, transparent 1rem),
+    background: linear-gradient(to bottom, white, rgba(255, 255, 255, 0) 1rem),
       linear-gradient(to bottom, var(--tina-color-grey-1), white 1rem);
     background-attachment: local, scroll;
     background-repeat: no-repeat;
