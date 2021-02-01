8bffe1b7209c955bea709f30456d1994f4c14b04
Scott Byrne <scott.byrne@forestry.io>
2021-02-01 10:26:55 -0400

    project setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,191 +1,255 @@
 import React from 'react'
-import styled from 'styled-components'
-
 import { GetStaticProps } from 'next'
-
-import {
-  InlineBlocks,
-  InlineField,
-  InlineTextareaField,
-  BlockTextarea,
-  BlocksControls,
-} from 'react-tinacms-inline'
-import { EditLink } from 'components/layout/EditLink'
-import { DefaultSeo } from 'next-seo'
-import { useCMS, BlockTemplate, usePlugin } from 'tinacms'
-import { DynamicLink } from 'components/ui/DynamicLink'
-import {
-  Layout,
-  Hero,
-  Wrapper,
-  Section,
-  RichTextWrapper,
-} from 'components/layout'
-
-import { Button, Video, ArrowList } from 'components/ui'
-import { InlineGithubForm } from 'components/layout/InlineGithubForm'
-import { useGithubJsonForm } from 'react-tinacms-github'
 import { getJsonPreviewProps } from 'utils/getJsonPreviewProps'
 
-const HomePage = (props: any) => {
-  const cms = useCMS()
-  const [formData, form] = useGithubJsonForm(props.file, {
-    label: 'Home Page',
-    fields: [
-      {
-        label: 'Headline',
-        name: 'headline',
-        description: 'Enter the main headline here',
-        component: 'text',
-      },
-      {
-        label: 'Description',
-        name: 'description',
-        description: 'Enter supporting main description',
-        component: 'textarea',
-      },
-      {
-        label: 'Selling Points',
-        name: 'three_points',
-        description: 'Edit the points here',
-        component: 'group-list',
-        //@ts-ignore
-        itemProps: item => ({
-          key: item.id,
-          label: `${item.main.slice(0, 15)}...`,
-        }),
-        defaultItem: () => ({
-          main: 'New Point',
-          supporting: '',
-          _template: 'selling_point',
-        }),
-        fields: [
-          {
-            label: 'Main',
-            name: 'main',
-            component: 'textarea',
-          },
-          {
-            label: 'Supporting',
-            name: 'supporting',
-            component: 'textarea',
-          },
-        ],
-      },
-      {
-        label: 'Setup Headline',
-        name: 'setup.headline',
-        description: 'Enter the "setup" headline here',
-        component: 'textarea',
-      },
-      {
-        label: 'Setup Steps',
-        name: 'setup.steps',
-        description: 'Edit the steps here',
-        component: 'group-list',
-        //@ts-ignore
-        itemProps: item => ({
-          key: item.id,
-          label: `${item.step.slice(0, 15)}...`,
-        }),
-        defaultItem: () => ({
-          step: 'New Step',
-          _template: 'setup_point',
-        }),
-        fields: [
-          {
-            label: 'Step',
-            name: 'step',
-            component: 'textarea',
-          },
-        ],
-      },
-    ],
-  })
-
-  usePlugin(form)
-
+const IconRight = () => {
   return (
-    <InlineGithubForm form={form}>
-      <Layout>
-        <DefaultSeo titleTemplate={formData.title + ' | %s'} />
-        <Hero overlap narrow>
-          <InlineTextareaField name="headline" />
-        </Hero>
-        <InlineField name="hero_video">
-          {({ input }) => {
-            return <Video src={input.value} autoPlay={cms.disabled} />
-          }}
-        </InlineField>
-
-        <Section>
-          <Wrapper>
-            <RichTextWrapper>
-              <CtaLayout>
-                <h2>
-                  <em>
-                    <InlineTextareaField name="description" />
-                  </em>
-                </h2>
-                <CtaBar>
-                  <EditLink color="primary" />
-                  <DynamicLink href="/docs/" passHref>
-                    <Button as="a">Get Started</Button>
-                  </DynamicLink>
-                </CtaBar>
-              </CtaLayout>
-              <InfoBlocks
-                name="three_points"
-                blocks={SELLING_POINTS_BLOCKS}
-                direction="horizontal"
-              />
-            </RichTextWrapper>
-          </Wrapper>
-        </Section>
-
-        <Section color="seafoam">
-          <Wrapper>
-            <SetupLayout>
-              <RichTextWrapper>
-                <h2 className="h1">
-                  <InlineTextareaField name="setup.headline" />
-                </h2>
-                <hr />
-                <ArrowList>
-                  <InlineBlocks
-                    name="setup.steps"
-                    blocks={SETUP_POINT_BLOCKS}
-                  />
-                </ArrowList>
-                <DynamicLink href="/docs" passHref>
-                  <Button as="a" color="primary">
-                    Get Started
-                  </Button>
-                </DynamicLink>
-              </RichTextWrapper>
-              <CodeWrapper>
-                <CodeExample
-                  dangerouslySetInnerHTML={{
-                    __html: `yarn add <b>tinacms</b>
-
-import { <b>withTina</b> } from 'tinacms'
-
-function MyApp({ Component, pageProps }) {
-  return &lt;Component {...pageProps} /&gt;
+    <svg
+      stroke="currentColor"
+      fill="currentColor"
+      strokeWidth="0"
+      viewBox="0 0 448 512"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z"></path>
+    </svg>
+  )
 }
 
-export default <b>withTina</b>(MyApp, {
-  <b>enabled: true</b>,
-  <b>sidebar: true</b>,
-})`,
-                  }}
-                ></CodeExample>
-              </CodeWrapper>
-            </SetupLayout>
-          </Wrapper>
-        </Section>
-      </Layout>
-    </InlineGithubForm>
+const TinaIcon = () => {
+  return (
+    <svg
+      viewBox="0 0 49 68"
+      fill="inherit"
+      xmlns="http://www.w3.org/2000/svg"
+      role="img"
+      aria-labelledby="title desc"
+    >
+      <title>Tina</title>
+      <desc>A proud llama</desc>
+      <path d="M31.4615 30.1782C34.763 27.4475 36.2259 11.3098 37.6551 5.50906C39.0843 -0.291715 44.995 0.00249541 44.995 0.00249541C44.995 0.00249541 43.4605 2.67299 44.0864 4.66584C44.7123 6.65869 49 8.44005 49 8.44005L48.0752 10.8781C48.0752 10.8781 46.1441 10.631 44.995 12.9297C43.8459 15.2283 45.7336 37.9882 45.7336 37.9882C45.7336 37.9882 38.8271 51.6106 38.8271 57.3621C38.8271 63.1136 41.5495 67.9338 41.5495 67.9338H37.7293C37.7293 67.9338 32.1252 61.2648 30.9759 57.9318C29.8266 54.5988 30.2861 51.2658 30.2861 51.2658C30.2861 51.2658 24.1946 50.921 18.7931 51.2658C13.3915 51.6106 9.78922 56.2539 9.13908 58.8512C8.48894 61.4486 8.21963 67.9338 8.21963 67.9338H5.19906C3.36057 62.2603 1.90043 60.2269 2.69255 57.3621C4.88665 49.4269 4.45567 44.9263 3.94765 42.9217C3.43964 40.9172 0 39.1676 0 39.1676C1.68492 35.7349 3.4048 34.0854 10.8029 33.9133C18.201 33.7413 28.1599 32.9088 31.4615 30.1782Z" />
+      <path d="M12.25 57.03C12.25 57.03 13.0305 64.2533 17.1773 67.9342H20.7309C17.1773 63.9085 16.7897 53.415 16.7897 53.415C14.9822 54.0035 12.4799 56.1106 12.25 57.03Z" />
+    </svg>
+  )
+}
+
+const HomePage = (props: any) => {
+  return (
+    <>
+      <div className="pageWrapper">
+        <div className="banner orange">
+          <div className="container">
+            <a className="bannerLink">
+              <span>
+                Sign up for early access for <em>Tina Cloud</em> ☁️
+              </span>
+              <IconRight />
+            </a>
+          </div>
+        </div>
+        <div className="nav black">
+          <div className="container navContainer">
+            <div className="logomark">
+              <TinaIcon />
+              <h1 className="wordmark">Tina.io</h1>
+            </div>
+            <nav className="navWrapper">
+              <ul className="navUl">
+                <li className="navLi">
+                  <a className="navLink" href="#">
+                    Docs
+                  </a>
+                </li>
+                <li className="navLi">
+                  <a className="navLink" href="#">
+                    Guides
+                  </a>
+                </li>
+                <li className="navLi">
+                  <a className="navLink" href="#">
+                    Blog
+                  </a>
+                </li>
+                <li className="navLi">
+                  <iframe
+                    className="starButton"
+                    src="https://ghbtns.com/github-btn.html?user=tinacms&repo=tinacms&type=star&count=true&size=large"
+                    frameBorder="0"
+                    scrolling="0"
+                    width="150px"
+                    height="30px"
+                  ></iframe>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <div className="feature black">
+          <div className="container"></div>
+        </div>
+        <div className="video">
+          <div className="container"></div>
+        </div>
+        <div className="feature white">
+          <div className="container"></div>
+        </div>
+        <div className="featureGroup white">
+          <div className="container"></div>
+        </div>
+        <div className="feature blue">
+          <div className="container"></div>
+        </div>
+        <div className="demo blue">
+          <div className="container"></div>
+        </div>
+        <div className="valueProps white">
+          <div className="container"></div>
+        </div>
+        <div className="feature lightGray">
+          <div className="container"></div>
+        </div>
+        <div className="featureGroup lightGray">
+          <div className="container"></div>
+        </div>
+        <div className="learnTina">
+          <div className="container"></div>
+        </div>
+      </div>
+      <style global jsx>{`
+        :root {
+          --color-orange: #ec4815;
+          --color-orange-light: #eb6337;
+          --color-orange-dark: #dc4419;
+          --color-yellow: #f2c94c;
+          --color-green: #6fcf97;
+          --color-black: #111920;
+          --color-blue: #00255b;
+          --color-white: #ffffff;
+          --color-gray: #b6b6b6;
+          --color-light-gray: #f8f8f8;
+        }
+      `}</style>
+      <style jsx>{`
+        .pageWrapper {
+        }
+
+        .container {
+          max-width: 1300px;
+          margin: 0 auto;
+          padding: 0 1.5rem;
+        }
+
+        .bannner {
+        }
+
+        .bannerLink {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          padding: 0.75rem 0;
+          font-size: 1.25rem;
+
+          :global(em) {
+            font-style: normal;
+            font-weight: bold;
+            text-decoration: underline;
+          }
+
+          :global(svg) {
+            margin-left: 1rem;
+            height: 1em;
+          }
+        }
+
+        .nav {
+          padding: 2rem 0;
+        }
+
+        .navContainer {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+        }
+
+        .navUl {
+          display: flex;
+          margin: 0 -1.5rem;
+        }
+
+        .navLi {
+          margin: 0 1.5rem;
+        }
+
+        .navLink {
+          color: white;
+          opacity: 0.7;
+          transition: opacity 150ms ease-out;
+          text-decoration: none;
+          font-size: 1.125rem;
+        }
+
+        .logomark {
+          color: var(--color-orange);
+          fill: var(--color-orange);
+          display: flex;
+          align-items: center;
+
+          :global(svg) {
+            margin-top: -0.375rem;
+            height: 2.5rem;
+            width: auto;
+            margin-right: 0.5rem;
+          }
+        }
+
+        .wordmark {
+          font-size: 2rem;
+          font-family: var(--font-tuner);
+          font-weight: bold;
+        }
+
+        .feature {
+        }
+
+        .featureGroup {
+        }
+
+        .video {
+        }
+
+        .demo {
+        }
+
+        .orange {
+          background: linear-gradient(
+            to top right,
+            var(--color-orange),
+            var(--color-orange-light)
+          );
+          color: var(--color-white);
+        }
+
+        .black {
+          background: var(--color-black);
+          color: var(--color-white);
+        }
+
+        .blue {
+          background: var(--color-blue);
+          color: var(--color-white);
+        }
+
+        .lightGray {
+          background: var(--color-light-gray);
+          color: var(--color-black);
+        }
+
+        .white {
+          background: var(--color-white);
+          color: var(--color-black);
+        }
+      `}</style>
+    </>
   )
 }
 
@@ -197,163 +261,3 @@ export const getStaticProps: GetStaticProps = async function({
 }) {
   return getJsonPreviewProps('content/pages/home.json', preview, previewData)
 }
-
-/*
- ** BLOCKS CONFIG ------------------------------------------------------
- */
-/*
- ** TODO: these selling point blocks should be an inline group-list
- */
-
-function SellingPoint({ data, index }) {
-  return (
-    <BlocksControls index={index}>
-      <div key={`selling-point-${index}`}>
-        <h3>
-          {data.color !== 'Dark Blue (Secondary)' && (
-            <em>
-              <BlockTextarea name="main" />
-            </em>
-          )}
-          {data.color === 'Dark Blue (Secondary)' && (
-            <BlockTextarea name="main" />
-          )}
-        </h3>
-        <p>
-          <BlockTextarea name="supporting" />
-        </p>
-      </div>
-    </BlocksControls>
-  )
-}
-
-const selling_point_template: BlockTemplate = {
-  label: 'Selling Point',
-  defaultItem: {
-    main: 'Tina is dope 🤙',
-    supporting:
-      'It’s pretty much my favorite animal. It’s like a lion and a tiger mixed… bred for its skills in magic.',
-    color: 'Orange (Primary)',
-  },
-  fields: [
-    {
-      label: 'Title Color',
-      name: 'color',
-      component: 'select',
-      // @ts-ignore
-      options: ['Orange (Primary)', 'Dark Blue (Secondary)'],
-    },
-  ],
-}
-
-const SELLING_POINTS_BLOCKS = {
-  selling_point: {
-    Component: SellingPoint,
-    template: selling_point_template,
-  },
-}
-
-function SetupPoint({ data, index }) {
-  return (
-    <BlocksControls index={index}>
-      <li key={`setup-point-${index}`}>
-        <BlockTextarea name="step" />
-      </li>
-    </BlocksControls>
-  )
-}
-
-const setup_point_template: BlockTemplate = {
-  label: 'Setup Point',
-  defaultItem: {
-    step: 'Make yourself a dang quesadilla',
-  },
-  fields: [],
-}
-
-const SETUP_POINT_BLOCKS = {
-  setup_point: {
-    Component: SetupPoint,
-    template: setup_point_template,
-  },
-}
-/*
- ** STYLES -------------------------------------------------------
- */
-
-const CodeWrapper = styled.div`
-  border-radius: 50px;
-  background-color: #d4f0ee;
-  display: block;
-  overflow: auto;
-`
-
-const CodeExample = styled.code`
-  display: block;
-  padding: 3rem;
-  color: #241748;
-  font-size: 1.125rem;
-  line-height: 1.5;
-  font-family: Monaco, 'Courier New', Courier, monospace;
-  white-space: pre;
-  filter: drop-shadow(rgba(104, 120, 125, 0.2) 0px 7px 8px);
-  align-self: flex-start;
-  width: 100%;
-  display: block;
-
-  b {
-    color: var(--color-primary);
-  }
-
-  span {
-    opacity: 0.3;
-  }
-`
-
-const InfoBlocks = styled(InlineBlocks)`
-  display: grid;
-  grid-gap: 2rem;
-
-  @media (min-width: 800px) {
-    grid-template-columns: repeat(3, 1fr);
-  }
-`
-
-const SetupLayout = styled.div`
-  display: grid;
-  grid-gap: 2rem;
-  @media (min-width: 800px) {
-    align-items: start;
-    grid-gap: 4rem;
-    grid-template-columns: repeat(2, 1fr);
-  }
-`
-
-const CtaLayout = styled.div`
-  max-width: 35rem;
-  text-align: center;
-  margin: 0 auto;
-  padding: 0 0 3rem 0;
-
-  @media (min-width: 800px) {
-    padding: 0 0 5rem 0;
-  }
-`
-
-const CtaBar = styled.div`
-  margin: 2rem auto 0 auto;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  button {
-    margin: 0.5rem 0.75rem;
-  }
-  iframe {
-    margin-left: 1rem;
-  }
-  @media (min-width: 1030px) {
-    iframe {
-      display: none;
-    }
-  }
-`
