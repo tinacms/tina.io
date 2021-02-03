d71a6d3125eeca3140d66a3e19c2248f8c436797
Scott Byrne <scott.byrne@forestry.io>
2021-02-03 14:09:52 -0400

    moving definitions

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,140 +7,6 @@ import { useCMS, useForm, usePlugin } from 'tinacms'
 import { useGithubJsonForm } from 'react-tinacms-github'
 import { InlineGithubForm } from '../components/layout/InlineGithubForm'
 
-const IconRight = () => {
-  return (
-    <svg
-      stroke="currentColor"
-      fill="currentColor"
-      strokeWidth="0"
-      viewBox="0 0 448 512"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path d="M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z"></path>
-    </svg>
-  )
-}
-
-const TinaIcon = () => {
-  return (
-    <svg
-      viewBox="0 0 49 68"
-      fill="inherit"
-      xmlns="http://www.w3.org/2000/svg"
-      role="img"
-      aria-labelledby="title desc"
-    >
-      <title>Tina</title>
-      <desc>A proud llama</desc>
-      <path d="M31.4615 30.1782C34.763 27.4475 36.2259 11.3098 37.6551 5.50906C39.0843 -0.291715 44.995 0.00249541 44.995 0.00249541C44.995 0.00249541 43.4605 2.67299 44.0864 4.66584C44.7123 6.65869 49 8.44005 49 8.44005L48.0752 10.8781C48.0752 10.8781 46.1441 10.631 44.995 12.9297C43.8459 15.2283 45.7336 37.9882 45.7336 37.9882C45.7336 37.9882 38.8271 51.6106 38.8271 57.3621C38.8271 63.1136 41.5495 67.9338 41.5495 67.9338H37.7293C37.7293 67.9338 32.1252 61.2648 30.9759 57.9318C29.8266 54.5988 30.2861 51.2658 30.2861 51.2658C30.2861 51.2658 24.1946 50.921 18.7931 51.2658C13.3915 51.6106 9.78922 56.2539 9.13908 58.8512C8.48894 61.4486 8.21963 67.9338 8.21963 67.9338H5.19906C3.36057 62.2603 1.90043 60.2269 2.69255 57.3621C4.88665 49.4269 4.45567 44.9263 3.94765 42.9217C3.43964 40.9172 0 39.1676 0 39.1676C1.68492 35.7349 3.4048 34.0854 10.8029 33.9133C18.201 33.7413 28.1599 32.9088 31.4615 30.1782Z" />
-      <path d="M12.25 57.03C12.25 57.03 13.0305 64.2533 17.1773 67.9342H20.7309C17.1773 63.9085 16.7897 53.415 16.7897 53.415C14.9822 54.0035 12.4799 56.1106 12.25 57.03Z" />
-    </svg>
-  )
-}
-
-const TripleDividerSvg = () => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      preserveAspectRatio="none"
-      viewBox="0 0 1200 150"
-      className="dividerSvg"
-    >
-      <g>
-        <path
-          stroke="var(--color-orange)"
-          strokeWidth="4"
-          strokeDasharray="8 14"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          vectorEffect="non-scaling-stroke"
-          d="M591.036 0v70.447m0 0H20c-11.046 0-20 8.955-20 20v51.406m591.036-71.406H1180c11.05 0 20 8.955 20 20V150"
-        ></path>
-      </g>
-      <line
-        x1="49.25%"
-        x2="49.25%"
-        y1="0"
-        y2="100%"
-        stroke="var(--color-orange)"
-        strokeWidth="4"
-        strokeDasharray="8 14"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        vectorEffect="non-scaling-stroke"
-      />
-    </svg>
-  )
-}
-
-const SingleDividerSvg = () => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 600 120"
-      width="100%"
-      preserveAspectRatio="false"
-    >
-      <line
-        x1="50%"
-        x2="50%"
-        y1="0"
-        y2="100%"
-        stroke="var(--color-orange)"
-        strokeWidth="4"
-        strokeDasharray="8 14"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        vectorEffect="non-scaling-stroke"
-      />
-    </svg>
-  )
-}
-
-interface ContainerProps {
-  children?: any
-  width?: 'medium' | 'narrow' | 'wide'
-  center?: boolean
-}
-
-const Container = ({
-  width = 'medium',
-  center = false,
-  children,
-}: ContainerProps) => {
-  return (
-    <>
-      <div className={['container', width, center ? 'center' : ''].join(' ')}>
-        {children}
-      </div>
-      <style jsx>{`
-        .container {
-          margin: 0 auto;
-          padding: 0 1.5rem;
-        }
-
-        .wide {
-          max-width: 1500px;
-        }
-
-        .medium {
-          max-width: 1240px;
-        }
-
-        .narrow {
-          max-width: 740px;
-        }
-
-        .center {
-          text-align: center;
-        }
-      `}</style>
-    </>
-  )
-}
-
 const HomePage = (props: any) => {
   let src = 'v1571425758/tina-hero-demo-v2'
 
@@ -1240,4 +1106,138 @@ export const getStaticProps: GetStaticProps = async function({
   previewData,
 }) {
   return getJsonPreviewProps('content/pages/home.json', preview, previewData)
+}
+
+const IconRight = () => {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="currentColor"
+      strokeWidth="0"
+      viewBox="0 0 448 512"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z"></path>
+    </svg>
+  )
+}
+
+const TinaIcon = () => {
+  return (
+    <svg
+      viewBox="0 0 49 68"
+      fill="inherit"
+      xmlns="http://www.w3.org/2000/svg"
+      role="img"
+      aria-labelledby="title desc"
+    >
+      <title>Tina</title>
+      <desc>A proud llama</desc>
+      <path d="M31.4615 30.1782C34.763 27.4475 36.2259 11.3098 37.6551 5.50906C39.0843 -0.291715 44.995 0.00249541 44.995 0.00249541C44.995 0.00249541 43.4605 2.67299 44.0864 4.66584C44.7123 6.65869 49 8.44005 49 8.44005L48.0752 10.8781C48.0752 10.8781 46.1441 10.631 44.995 12.9297C43.8459 15.2283 45.7336 37.9882 45.7336 37.9882C45.7336 37.9882 38.8271 51.6106 38.8271 57.3621C38.8271 63.1136 41.5495 67.9338 41.5495 67.9338H37.7293C37.7293 67.9338 32.1252 61.2648 30.9759 57.9318C29.8266 54.5988 30.2861 51.2658 30.2861 51.2658C30.2861 51.2658 24.1946 50.921 18.7931 51.2658C13.3915 51.6106 9.78922 56.2539 9.13908 58.8512C8.48894 61.4486 8.21963 67.9338 8.21963 67.9338H5.19906C3.36057 62.2603 1.90043 60.2269 2.69255 57.3621C4.88665 49.4269 4.45567 44.9263 3.94765 42.9217C3.43964 40.9172 0 39.1676 0 39.1676C1.68492 35.7349 3.4048 34.0854 10.8029 33.9133C18.201 33.7413 28.1599 32.9088 31.4615 30.1782Z" />
+      <path d="M12.25 57.03C12.25 57.03 13.0305 64.2533 17.1773 67.9342H20.7309C17.1773 63.9085 16.7897 53.415 16.7897 53.415C14.9822 54.0035 12.4799 56.1106 12.25 57.03Z" />
+    </svg>
+  )
+}
+
+const TripleDividerSvg = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      preserveAspectRatio="none"
+      viewBox="0 0 1200 150"
+      className="dividerSvg"
+    >
+      <g>
+        <path
+          stroke="var(--color-orange)"
+          strokeWidth="4"
+          strokeDasharray="8 14"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          vectorEffect="non-scaling-stroke"
+          d="M591.036 0v70.447m0 0H20c-11.046 0-20 8.955-20 20v51.406m591.036-71.406H1180c11.05 0 20 8.955 20 20V150"
+        ></path>
+      </g>
+      <line
+        x1="49.25%"
+        x2="49.25%"
+        y1="0"
+        y2="100%"
+        stroke="var(--color-orange)"
+        strokeWidth="4"
+        strokeDasharray="8 14"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        vectorEffect="non-scaling-stroke"
+      />
+    </svg>
+  )
+}
+
+const SingleDividerSvg = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 600 120"
+      width="100%"
+      preserveAspectRatio="false"
+    >
+      <line
+        x1="50%"
+        x2="50%"
+        y1="0"
+        y2="100%"
+        stroke="var(--color-orange)"
+        strokeWidth="4"
+        strokeDasharray="8 14"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        vectorEffect="non-scaling-stroke"
+      />
+    </svg>
+  )
+}
+
+interface ContainerProps {
+  children?: any
+  width?: 'medium' | 'narrow' | 'wide'
+  center?: boolean
+}
+
+const Container = ({
+  width = 'medium',
+  center = false,
+  children,
+}: ContainerProps) => {
+  return (
+    <>
+      <div className={['container', width, center ? 'center' : ''].join(' ')}>
+        {children}
+      </div>
+      <style jsx>{`
+        .container {
+          margin: 0 auto;
+          padding: 0 1.5rem;
+        }
+
+        .wide {
+          max-width: 1500px;
+        }
+
+        .medium {
+          max-width: 1240px;
+        }
+
+        .narrow {
+          max-width: 740px;
+        }
+
+        .center {
+          text-align: center;
+        }
+      `}</style>
+    </>
+  )
 }
