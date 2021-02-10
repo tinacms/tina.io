50db4160bf9614a90a26e1089f37875979186102
Scott Byrne <scott.byrne@forestry.io>
2021-02-10 16:50:55 -0400

    global styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,35 +2,25 @@ import React from 'react'
 import { GetStaticProps } from 'next'
 import { getJsonPreviewProps } from 'utils/getJsonPreviewProps'
 import { Footer } from 'components/layout'
-import Link from 'next/link'
-import { BlockTemplate, useCMS, usePlugin } from 'tinacms'
+import { usePlugin } from 'tinacms'
 import { useGithubJsonForm } from 'react-tinacms-github'
 import { InlineGithubForm } from '../components/layout/InlineGithubForm'
-import ReactMarkdown from 'react-markdown'
 import { NextSeo } from 'next-seo'
-import {
-  BlocksControls,
-  InlineBlocks,
-  InlineTextarea,
-} from 'react-tinacms-inline'
-import { InlineWysiwyg } from 'react-tinacms-editor'
+import { InlineBlocks } from 'react-tinacms-inline'
 import {
   BrowserBlock,
   browser_template,
-  Container,
   DemoBlock,
   demo_teamplate,
-  Divider,
   FeaturesBlock,
   features_template,
   FlyingBlock,
   flying_template,
+  GlobalStyles,
   HeroBlock,
   hero_template,
-  IconRight,
   NavbarBlock,
   navbar_template,
-  TinaIcon,
 } from 'components/home'
 
 const HomePage = (props: any) => {
@@ -53,150 +43,9 @@ const HomePage = (props: any) => {
       />
       <InlineBlocks name="blocks" blocks={HOMEPAGE_BLOCKS} />
       <Footer />
-      <style global jsx>{`
-        :root {
-          --color-orange: #ec4815;
-          --color-orange-light: #eb6337;
-          --color-orange-dark: #dc4419;
-          --color-yellow: #f2c94c;
-          --color-green: #6fcf97;
-          --color-black: #1c1b2e;
-          --color-blue: #241748;
-          --color-white: #ffffff;
-          --color-gray: #f3f3f3;
-          --color-light-gray: #fafafa;
-          --color-seafoam: #e6faf8;
-          --color-seafoam-dark: #b4f4e0;
-
-          --color-emphasis: var(--color-orange);
-          --color-card-background: var(--color-light-gray);
-
-          --spacer-size: 4.5rem;
-          --section-padding: calc(var(--spacer-size) * 2);
-          --container-padding: 1.5rem;
-        }
-
-        html {
-          min-width: 400px;
-        }
-
-        .section {
-          padding: var(--section-padding) 0;
-        }
-
-        .headingHuge {
-          font-family: var(--font-tuner);
-          font-weight: bold;
-          font-size: 2.75rem;
-          line-height: 1.4;
-          margin-bottom: 2rem;
-
-          :global(em),
-          :global(strong) {
-            font-weight: inherit;
-            color: var(--color-emphasis);
-            font-style: inherit;
-
-            @media (min-width: 600px) {
-              white-space: nowrap;
-            }
-          }
-        }
-
-        .headingLarge {
-          font-family: var(--font-tuner);
-          margin-bottom: 1rem;
-          font-size: 2.25rem;
-          font-weight: bold;
-        }
-
-        .headingMedium {
-          font-size: 1.675rem;
-          line-height: 1.4;
-          margin-bottom: 1rem;
-        }
-
-        .textHuge {
-          display: block;
-          width: 100%;
-          max-width: 600px;
-          margin-left: auto;
-          margin-right: auto;
-          font-size: 1.375rem;
-
-          &:not(:last-child) {
-            margin-bottom: 2rem;
-          }
-        }
-
-        .textLarge {
-          font-size: 1.125rem;
-          opacity: 0.7;
-
-          &:not(:last-child) {
-            margin-bottom: 1.25rem;
-          }
-        }
-
-        .spacer {
-          display: block;
-          width: 100%;
-          height: var(--spacer-size);
-        }
-
-        .spacerBig {
-          height: calc(var(--spacer-size) * 1.5);
-        }
-
-        .dottedBorder {
-          border-top: none;
-          border-right: none;
-          border-left: none;
-          border-image: initial;
-          border-bottom: 4px dotted var(--color-orange);
-          width: 6rem;
-          max-width: 100%;
-          display: block;
-          height: 0px;
-          margin: 1.5rem 0px;
-        }
-
-        .orange {
-          background: linear-gradient(
-            to top right,
-            var(--color-orange),
-            var(--color-orange-light)
-          );
-          color: var(--color-white);
-        }
-
-        .black {
-          background: var(--color-black);
-          color: var(--color-white);
-        }
-
-        .blue {
-          background: var(--color-blue);
-          background: linear-gradient(
-            to bottom,
-            var(--color-blue) 30%,
-            var(--color-black) 100%
-          );
-          color: var(--color-white);
-          --color-emphasis: var(--color-orange-light);
-        }
-
-        .lightGray {
-          background: var(--color-light-gray);
-          color: var(--color-black);
-          --color-card-background: var(--color-white);
-        }
-
-        .white {
-          background: var(--color-white);
-          color: var(--color-black);
-        }
-      `}</style>
+      <style global jsx>
+        {GlobalStyles}
+      </style>
     </InlineGithubForm>
   )
 }
