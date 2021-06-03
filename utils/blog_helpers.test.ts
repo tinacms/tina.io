ef919496c0f423dc45a415835069ef98f24042e3
DJ <dj@forestry.io>
2021-06-03 13:27:07 -0400

    fix: strip shortcodes from markdown when generating plaintext

---
+++ utils/blog_helpers.test.ts
@@ -0,0 +1,15 @@
+import { stripMarkdown } from './blog_helpers'
+
+describe('stripMarkdown', () => {
+  describe('with shortcodes', () => {
+    const testDocument =
+      'Hello, {{ WarningCallout text="Tina Cloud is too cool for you" }}World!\n'
+    const expected = 'Hello, World!\n'
+
+    it('should remove shortcode markup', async () => {
+      const stripped = await stripMarkdown(testDocument)
+      console.log(expected, stripped)
+      expect(stripped).toEqual(expected)
+    })
+  })
+})
