a63a8bdb6009995a7e34b991f11e928dd7df8c31
Scott Byrne <scott.byrne@forestry.io>
2021-07-28 15:41:33 -0300

    improve svg type

--- custom.d.ts
+++ custom.d.ts
@@ -1,4 +1,6 @@
-declare module "*.svg" {
-  const content: string;
-  export default content;
+declare module '*.svg' {
+  import React = require('react')
+  export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>
+  const src: string
+  export default src
 }
