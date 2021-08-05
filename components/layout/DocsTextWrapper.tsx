ab14d06433960fe68acaa7195fa8c1b8713b0884
Scott Byrne <scott.byrne@forestry.io>
2021-08-05 12:03:24 -0300

    auto wrap videos with fluid container

--- components/layout/DocsTextWrapper.tsx
+++ components/layout/DocsTextWrapper.tsx
@@ -9,6 +9,40 @@ import { FallbackPlaceholder } from 'components/fallback-placeholder'
 
 export const DocsTextWrapper = ({ children }) => {
   const router = useRouter()
+
+  React.useEffect(() => {
+    /* https://codepen.io/chriscoyier/pen/YzXeXjK */
+
+    var players = ['iframe[src*="youtube.com"]', 'iframe[src*="vimeo.com"]']
+    var fitVids = document.querySelectorAll(players.join(','))
+
+    if (fitVids.length) {
+      // Loop through videos
+      for (var i = 0; i < fitVids.length; i++) {
+        // Get Video Information
+        var fitVid = fitVids[i]
+        var width = fitVid.getAttribute('width')
+        var height = fitVid.getAttribute('height')
+        console.log(width)
+        // @ts-ignore
+        var aspectRatio = height / width
+        var parentDiv = fitVid.parentNode
+
+        // Wrap it in a DIV
+        var div = document.createElement('div')
+        div.className = 'fitVids-wrapper'
+        div.style.paddingBottom = aspectRatio * 100 + '%'
+        parentDiv.insertBefore(div, fitVid)
+        fitVid.remove()
+        div.appendChild(fitVid)
+
+        // Clear height/width from fitVid
+        fitVid.removeAttribute('height')
+        fitVid.removeAttribute('width')
+      }
+    }
+  }, [])
+
   return router.isFallback ? (
     <FallbackPlaceholder
       wrapperStyles={{ paddingLeft: 0, paddingRight: 0 }}
