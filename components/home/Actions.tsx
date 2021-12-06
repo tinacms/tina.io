402cbe1c8444fb98e513f60af1409a44ecc275d0
James O'Halloran <james.p.ohalloran@gmail.com>
2021-12-06 15:38:44 -0400

    Update create-tina-app homepage CTA's (#1130)
    * use latest on create-tina-app
    
    * add command action
    
    * style: cta cli button
    
    * style: feature link styles
    
    * add classname for gtm
    
    Co-authored-by: Scott Byrne <scott.byrne@forestry.io>

--- components/home/Actions.tsx
+++ components/home/Actions.tsx
@@ -1,5 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
+import { BiCopy } from 'react-icons/bi'
 import { IconRight } from './Icons'
+import {
+  copyToClipboard,
+  WithCodeStyles,
+} from 'components/layout/MarkdownContent'
+
+const BashWrapper = styled.div`
+  border: 1px solid black;
+
+  code {
+    ::before {
+      content: '$ ';
+    }
+  }
+`
 
 export const Actions = ({ items, align = 'left' }) => {
   return (
@@ -11,6 +27,10 @@ export const Actions = ({ items, align = 'left' }) => {
         ].join(' ')}
       >
         {items.map(item => {
+          if (item.variant == 'command') {
+            return <CodeButton>{item.label}</CodeButton>
+          }
+
           const { variant, label, icon, url } = item
           const externalUrlPattern = /^((http|https|ftp):\/\/)/
           const external = externalUrlPattern.test(url)
@@ -166,6 +186,134 @@ export const Actions = ({ items, align = 'left' }) => {
 
           :hover {
             color: var(--color-orange-dark);
+          }
+        }
+      `}</style>
+    </>
+  )
+}
+
+export const CodeButton = ({ children }) => {
+  const [copied, setCopied] = React.useState(false)
+
+  const clickEvent = () => {
+    setCopied(true)
+    copyToClipboard(children)
+    setTimeout(() => {
+      setCopied(false)
+    }, 2000)
+  }
+
+  return (
+    <>
+      <button className="button event-cmd-button" onClick={clickEvent}>
+        <span className={`success-message ${copied ? `visible` : ``}`}>
+          Copied to clipboard!
+        </span>
+        <span className="text">
+          <span className="bash">$</span> {children}
+        </span>
+        <span className="icon">
+          <BiCopy />
+        </span>
+      </button>
+      <style jsx>{`
+        .bash {
+          opacity: 0.5;
+          margin-right: 0.25rem;
+        }
+
+        .success-message {
+          position: absolute;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          width: 100%;
+          height: 100%;
+          text-align: center;
+          color: var(--color-orange);
+          font-family: var(--font-tuner);
+          font-weight: regular;
+          font-style: normal;
+          background: white;
+          z-index: 10;
+          transition: opacity 180ms ease-out;
+          opacity: 0;
+        }
+
+        .visible {
+          opacity: 1;
+        }
+
+        .icon {
+          width: 2.75rem;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          background: var(--color-seafoam);
+          border-left: 1px solid #b4f4e0;
+          color: var(--color-orange);
+          align-self: stretch;
+          opacity: 0.5;
+          transition: opacity 180ms ease-out;
+
+          :global(svg) {
+            height: 1.5em;
+            width: auto;
+          }
+        }
+
+        .text {
+          padding: 1rem 1.5rem;
+        }
+
+        .button {
+          display: flex;
+          font-weight: bold;
+          overflow: hidden;
+          font-size: 1rem;
+          border-radius: 0.5rem;
+          cursor: pointer;
+          transition: all 150ms ease-out;
+          width: max-content;
+          transform: translate3d(0px, 0px, 0px);
+          display: flex;
+          align-items: center;
+          background-color: white;
+          color: var(--color-secondary);
+          text-transform: uppercase;
+          font-family: Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace;
+          padding: 0;
+          border: 1px solid #b4f4e0;
+          font-weight: regular;
+          font-style: normal;
+          text-decoration: none !important;
+          white-space: nowrap;
+          opacity: 1;
+          line-height: 1;
+
+          &:hover,
+          &:focus {
+            color: var(--color-orange);
+            text-decoration: none;
+            transform: translate3d(-1px, -2px, 0);
+            transition: transform 180ms ease-out;
+
+            .icon {
+              opacity: 1;
+            }
+          }
+          &:focus {
+            box-shadow: rgba(0, 0, 0, 0.08) 0px 0px 0px 1px inset,
+              rgba(236, 72, 21, 0.7) 0px 0px 0px 3px,
+              rgba(0, 0, 0, 0.12) 0px 2px 3px;
+          }
+          &:focus,
+          &:active {
+            outline: none;
+          }
+          &:active {
+            filter: none;
           }
         }
       `}</style>
