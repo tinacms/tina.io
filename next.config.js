7953b52da699eab2f8f69aabcda23e5f5163a397
Jeff See <jeffsee.55@gmail.com>
2021-12-13 14:40:19 -0800

    Add cors for unrestricted API route

--- next.config.js
+++ next.config.js
@@ -22,6 +22,28 @@ const config = {
     BASE_REPO_FULL_NAME: process.env.BASE_REPO_FULL_NAME,
     BASE_BRANCH: process.env.BASE_BRANCH,
   },
+  //avoiding CORS error, more here: https://vercel.com/support/articles/how-to-enable-cors
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/graphql',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ]
+  },
   trailingSlash: true,
   exportPathMap: async function() {
     return {}
