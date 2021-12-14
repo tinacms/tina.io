ac2551d4d9063430d31e8a016499d9e5edcea175
Jeff See <jeffsee.55@gmail.com>
2021-12-14 15:09:30 -0800

    set content type

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -4,7 +4,7 @@ export default async function feedback(req, res) {
   console.log('received request', req.query)
   // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
   // res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept')
-  // res.setHeader('Content-Type', 'application/json')
+  res.setHeader('Content-Type', 'application/json')
   class InMemoryStore extends gqlPackage.MemoryStore {
     public supportsSeeding() {
       return true
