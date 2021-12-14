3203ba98d79e49c1364b15bb41bd14e38caaae88
Jeff See <jeffsee.55@gmail.com>
2021-12-14 11:34:32 -0800

    Add / to source

--- next.config.js
+++ next.config.js
@@ -27,7 +27,7 @@ const config = {
     return [
       {
         // matching all API routes
-        source: '/api/graphql',
+        source: '/api/graphql/',
         headers: [
           {
             key: 'Access-Control-Allow-Origin',
