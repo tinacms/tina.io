18ea83e314eecd268b210ceb6cad3c61b5aa1bd4
Ashley Doiron <ashley.doiron@forestry.io>
2022-01-26 10:48:46 -0400

    update tina cloud banner to link to quickstart

--- components/cloud-beta-disclaimer.tsx
+++ components/cloud-beta-disclaimer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export const CloudDisclaimer = () => (
   <blockquote>
-    Tina Cloud is in public beta.{' '}
-    <Link href="https://app.tina.io/register">Get Started</Link>
+    Tina Cloud is in public beta. Click to{' '}
+    <Link href="https://app.tina.io/quickstart">Get Started.</Link>
   </blockquote>
 )
