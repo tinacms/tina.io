17246f0b43313b44fada50c2afe36f65bb88ec8c
James O'Halloran <james.p.ohalloran@gmail.com>
2022-08-10 13:04:19 -0300

    Use 0.68.15 Tina release (#1375)
    * bump tina to beta
    
    * switch to new client
    
    * github env
    
    * Upgrade actions to v3
    
    * Change gh action trigger
    
    * bump beta version
    
    * fix scripts
    
    * trigger rebuild
    
    * dont start server on start cmd
    
    * fix build script
    
    * test without noTelemetry
    
    * add missing generated
    
    * Use release versions
    
    * deleted ignored files

--- components/layout/CloudBanner.tsx
+++ components/layout/CloudBanner.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
 import { IconRight } from '../blocks'
-import TinaCloudWordmark from '../../public/svg/tina-cloud-wordmark.svg'
 import { Button, ButtonGroup } from 'components/ui'
 
 export function CloudBanner() {
