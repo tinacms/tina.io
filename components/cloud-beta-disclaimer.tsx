23ccbbcfc79c992ac8c2eb48e0273e93186a6f7e
James O'Halloran <james.p.ohalloran@gmail.com>
2022-11-02 20:04:27 -0300

    Remove beta banner

--- components/cloud-beta-disclaimer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Link from 'next/link'
-
-export const CloudDisclaimer = () => (
-  <blockquote>
-    Tina Cloud is in public beta.{' '}
-    <Link href="https://app.tina.io/register">Get Started</Link>
-  </blockquote>
-)
