aed0b7cd4c5b24762b59d87d0dafc00f71ded487
James O'Halloran <james.p.ohalloran@gmail.com>
2023-01-10 09:35:21 -0400

    cloudinary media (#1464)

---
+++ pages/api/cloudinary/[...media].ts
@@ -0,0 +1,30 @@
+// pages/api/cloudinary/[...media].ts
+
+import {
+  mediaHandlerConfig,
+  createMediaHandler,
+} from 'next-tinacms-cloudinary/dist/handlers'
+
+import { isAuthorized } from '@tinacms/auth'
+
+export const config = mediaHandlerConfig
+
+export default createMediaHandler({
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  authorized: async (req, _res) => {
+    try {
+      if (process.env.NODE_ENV == 'development') {
+        return true
+      }
+
+      const user = await isAuthorized(req)
+
+      return user && user.verified
+    } catch (e) {
+      console.error(e)
+      return false
+    }
+  },
+})
