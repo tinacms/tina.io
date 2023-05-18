5e55616b9a8b96a8a91d21d0597c592e42f55da7
Scott Byrne <scott.byrne@forestry.io>
2023-05-18 14:47:35 -0300

    add icon to button

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -23,6 +23,7 @@ import path from 'path'
 import { TinaMarkdown, Components } from 'tinacms/dist/rich-text'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import atomOneDark from 'react-syntax-highlighter/dist/cjs/styles/prism/atom-dark'
+import { BiRightArrowAlt } from 'react-icons/bi'
 
 const components: Components<{
   Iframe: { iframeSrc: string; height: string }
@@ -199,10 +200,10 @@ const components: Components<{
   Button: ({ link, label }) => (
     <div className="w-full flex justify-start my-6">
       <a
-        className="px-6 pt-[12px] pb-[10px] text-base font-medium transition duration-150 ease-out rounded-full flex items-center font-tuner whitespace-nowrap focus:outline-none focus:shadow-outline hover:-translate-y-px active:translate-y-px hover:-translate-x-px active:translate-x-px leading-tight text-white hover:text-gray-50 border border-orange-600 bg-gradient-to-br from-orange-400 to-orange-600"
+        className="px-6 pt-[12px] pb-[10px] text-base font-medium transition duration-150 ease-out rounded-full flex items-center gap-1 font-tuner whitespace-nowrap focus:outline-none focus:shadow-outline hover:-translate-y-px active:translate-y-px hover:-translate-x-px active:translate-x-px leading-tight text-white hover:text-gray-50 border border-orange-600 bg-gradient-to-br from-orange-400 to-orange-600"
         href={link}
       >
-        {label}
+        {label} <BiRightArrowAlt className="h-5 w-auto -mt-1 opacity-70" />
       </a>
     </div>
   ),
