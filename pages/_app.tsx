74a012151b497bcaf942b1b7407015c0e2c43cdd
James O'Halloran <james.p.ohalloran@gmail.com>
2023-07-06 13:47:36 -0300

    Admin link for signed in users (#1603)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import App from 'next/app'
 import Head from 'next/head'
 import Script from 'next/script'
@@ -9,6 +9,7 @@ import { GlobalStyle } from 'components/styles/GlobalStyle'
 import 'components/styles/fontImports.css'
 import path from 'path'
 import '../styles/tailwind.css'
+import { useEditState } from 'tinacms/dist/react'
 
 // the following line will cause all content files to be available in a serverless context
 path.resolve('./content/')
@@ -61,7 +62,43 @@ const MainLayout = ({ Component, pageProps }) => {
         }}
       />
       <GlobalStyle />
+      <AdminLink />
       <Component {...pageProps} />
+    </>
+  )
+}
+
+const AdminLink = () => {
+  const { edit } = useEditState()
+  const [showAdminLink, setShowAdminLink] = React.useState(false)
+
+  useEffect(() => {
+    setShowAdminLink(
+      !edit &&
+        JSON.parse((window.localStorage.getItem('tinacms-auth') as any) || '{}')
+          ?.access_token
+    )
+  }, [edit])
+
+  const handleDismiss = () => {
+    setShowAdminLink(false)
+  }
+
+  return (
+    <>
+      {showAdminLink && (
+        <div className="fixed top-4 right-4 flex items-center justify-between bg-blue-500 text-white px-3 py-1 rounded-full z-50">
+          <a
+            href={`/admin/index.html#/~${window.location.pathname}`}
+            className="text-xs"
+          >
+            Edit This Page
+          </a>
+          <button onClick={handleDismiss} className="ml-2 text-sm">
+            x
+          </button>
+        </div>
+      )}
     </>
   )
 }
