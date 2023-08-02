2f4073311e6b7b5fe6c20228595679ac0bed64ea
James O'Halloran <james.p.ohalloran@gmail.com>
2023-08-02 16:47:29 -0300

    simplify google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -46,18 +46,15 @@ const MainLayout = ({ Component, pageProps }) => {
         <meta name="theme-color" content="#E6FAF8" />
       </Head>
       <Script
-        async
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
-      <Script
+        id="gtag-base"
+        strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${process.env.GTM_ID}');
           `,
         }}
       />
--- utils/ga/index.js
+++ utils/ga/index.js
@@ -1,11 +1,7 @@
 // log the pageview with their URL
-export const pageview = url => {
-  window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
-    page_path: url,
+export const pageview = (url) => {
+  window.dataLayer.push({
+    event: 'pageview',
+    page: url,
   })
-}
-
-// log specific events happening.
-export const event = ({ action, params }) => {
-  window.gtag('event', action, params)
 }
