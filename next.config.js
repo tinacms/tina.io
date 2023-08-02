4eda2aa5fa0a7fe3ea4c5cf862b5c2267adb6b0d
James O'Halloran <james.p.ohalloran@gmail.com>
2023-08-02 14:43:03 -0300

    img optimization

--- next.config.js
+++ next.config.js
@@ -12,6 +12,16 @@ const dummyMailchimpEndpoint =
   'https://theDomainHere.us18.list-manage.com/subscribe/post?u=1512315231252&amp;id=0asd21t12e1'
 
 const config = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/forestry-demo/**',
+      },
+    ],
+  },
   async rewrites() {
     return [
       {
