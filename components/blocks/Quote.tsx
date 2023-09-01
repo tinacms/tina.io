e7886d18315c53093687418e0de6a9f826e68c84
Scott Gallant <scottgallant@gmail.com>
2023-09-01 11:30:42 -0300

    wip add quote block

--- components/blocks/Quote.tsx
+++ components/blocks/Quote.tsx
@@ -4,8 +4,16 @@ import { tinaField } from 'tinacms/dist/react'
 export const QuoteBlock = ({ data, index }) => {
   return (
     <>
-      <section className="py-12 lg:py-16 text-3xl lg:text-4xl font-tuner lg:leading-tight bg-gradient-to-br from-blue-700/80 via-blue-900/90 to-blue-1000 bg-clip-text text-transparent mb-2">
-        <h1 data-tina-field={tinaField(data, 'title2')}>{data.title2}</h1>
+      <section className="relative z-10 py-20 lg:py-28">
+        <div className="container wide flex flex-col-reverse items-center lg:justify-center gap-12 perspective lg:flex-row">
+          <div className="lg:w-2/5 flex flex-col gap-6 lg:gap-8">
+            <img src={data.logo} />
+          </div>
+          <div className="min-w-0 lg:w-1/2 ">
+            <h2 className="text-xl mb-2" data-tina-field={tinaField(data, 'title2')}>{data.title2}</h2>
+            <p data-tina-field={tinaField(data, 'subtext')}>{data.subtext}</p>
+          </div>
+        </div>
       </section>
     </>
   )
