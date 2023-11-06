0e9961e3227baeb97701c0e4428e21cf2707ae8a
Kelly Davis <kelly.davis@forestry.io>
2023-11-06 10:15:35 -0500

    feat: improve analytics_search exporter

--- scripts/analytics/exportSearch.js
+++ scripts/analytics/exportSearch.js
@@ -5,8 +5,22 @@ const { sql } = require('@vercel/postgres')
 
 // iife to allow async/await
 ;(async () => {
-  const { rows } = await sql`
-    SELECT * FROM analytics_search;
+  // get row count
+  const { rows: rowCount } = await sql`
+    SELECT COUNT(*) FROM analytics_search where vercel_env = 'production';
   `
-  console.log(JSON.stringify(rows, null, 2))
+  // print csv header
+  console.log('timestamp,formatted_timestamp,session_id,search_query')
+
+  // get results in pages of 1000
+  const pages = Math.ceil(rowCount[0].count / 1000)
+  for (let i = 0; i < pages; i++) {
+    const { rows } = await sql`
+      SELECT timestamp, session_id, search_query FROM analytics_search where vercel_env = 'production' LIMIT 1000 OFFSET ${i * 1000};
+    `
+    rows.forEach(row => {
+      const formattedDate = new Date(Number(row.timestamp)).toISOString()
+      console.log(`${row.timestamp},"${formattedDate}","${row.session_id}","${row.search_query.replace(/"/g, '\"')}"`)
+    })
+  }
 })()
