ae243f002d98e8738bf287e3f6bf0810b4920ba5
joshua berman <jber4409@uni.sydney.edu.au>
2024-05-06 16:41:49 +1000

    Change id store to .env

--- components/ui/TinaChatBot.tsx
+++ components/ui/TinaChatBot.tsx
@@ -1,6 +1,5 @@
 import Script from 'next/script'
-
-const chatBaseBotId = 'mJl22IM4pXzWC60JoM5Fj'
+const chatBaseBotId = process.env.NEXT_PUBLIC_CHATBASE_BOT_ID
 
 const ChatBaseBot = () => {
     return (
