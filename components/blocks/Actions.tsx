8372f1ba58760d7af8a3b033f94208885631611e
joshua berman <jber4409@uni.sydney.edu.au>
2024-05-24 09:41:16 +1000

    move id

--- components/blocks/Actions.tsx
+++ components/blocks/Actions.tsx
@@ -21,8 +21,8 @@ export const Actions = ({ items, align = 'left' }) => {
       >
         {items &&
           items.map((item, index) => {
-            const id = sanitizeLabel(item.label);
             if (item.variant == 'command') {
+              const id = sanitizeLabel(item.label);
               return (
                 <React.Fragment key={item.label}>
                   {index === 2 && <span className="or-text">or</span>} {}
