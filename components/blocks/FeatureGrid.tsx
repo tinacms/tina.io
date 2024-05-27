721f22930c86ab5381ee988b5fc2a382efbfd6b4
joshua berman <jber4409@uni.sydney.edu.au>
2024-05-27 15:12:31 +1000

    switch around sides

--- components/blocks/FeatureGrid.tsx
+++ components/blocks/FeatureGrid.tsx
@@ -12,24 +12,7 @@ const Feature = ({ data, index }) => {
   const formattedUrl = url && !url.match(/^https?:\/\//) && !url.startsWith('/') ? `http://${url}` : url;
   const isInternalLink = formattedUrl && formattedUrl.startsWith('/');
 
-  return isInternalLink ? (
-    <Link href={formattedUrl} passHref>
-      <a
-        className="group block py-6 px-8 md:py-9 md:px-11 lg:py-12 lg:px-14 rounded-sm bg-gradient-to-br from-white via-white to-white/50 shadow-[inset_0_0_0_1px_rgba(223,219,252,0.15),_0_0_1px_1px_rgba(223,219,252,0.5)] transition duration-500 hover:scale-105 hover:bg-gradient-to-br hover:from-orange-200 hover:via-orange-400 hover:to-orange-600 hover:z-20"
-        style={{ textDecoration: 'none', overflow: 'visible' }}
-      >
-        <div data-tina-field={tinaField(data, 'headline')} className="flex flex-col gap-4">
-          {headline && (
-            <h3 className="text-2xl md:text-xl lg:text-2xl font-tuner leading-tight text-transparent bg-gradient-to-br from-blue-700/80 via-blue-900/90 to-blue-1000 bg-clip-text mb-2 group-hover:text-white break-words">
-              {headline}
-            </h3>
-          )}
-          {text && <p className="group-hover:text-white">{text}</p>}
-          {actions && <Actions items={actions} />}
-        </div>
-      </a>
-    </Link>
-  ) : (
+  return !isInternalLink ? (
     <a
       href={formattedUrl}
       target="_blank"
@@ -47,6 +30,24 @@ const Feature = ({ data, index }) => {
         {actions && <Actions items={actions} />}
       </div>
     </a>
+  ) : 
+  (
+    <Link href={formattedUrl} passHref>
+      <a
+        className="group block py-6 px-8 md:py-9 md:px-11 lg:py-12 lg:px-14 rounded-sm bg-gradient-to-br from-white via-white to-white/50 shadow-[inset_0_0_0_1px_rgba(223,219,252,0.15),_0_0_1px_1px_rgba(223,219,252,0.5)] transition duration-500 hover:scale-105 hover:bg-gradient-to-br hover:from-orange-200 hover:via-orange-400 hover:to-orange-600 hover:z-20"
+        style={{ textDecoration: 'none', overflow: 'visible' }}
+      >
+        <div data-tina-field={tinaField(data, 'headline')} className="flex flex-col gap-4">
+          {headline && (
+            <h3 className="text-2xl md:text-xl lg:text-2xl font-tuner leading-tight text-transparent bg-gradient-to-br from-blue-700/80 via-blue-900/90 to-blue-1000 bg-clip-text mb-2 group-hover:text-white break-words">
+              {headline}
+            </h3>
+          )}
+          {text && <p className="group-hover:text-white">{text}</p>}
+          {actions && <Actions items={actions} />}
+        </div>
+      </a>
+    </Link>
   );
 };
 
