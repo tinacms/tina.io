ff71d4ecd4cef5ec90b1b8c309edd672d9b1b291
joshua berman <jber4409@uni.sydney.edu.au>
2024-06-03 18:40:41 +1000

    simplify message response

--- components/forms/EmailForm.tsx
+++ components/forms/EmailForm.tsx
@@ -19,30 +19,26 @@ export const EmailForm = (props: EmailFormProps) => {
   const [isEntering, setIsEntering] = useState(false);
   const [isProcessing, setIsProcessing] = useState(false);
 
-  const [successMessage, setSuccessMessage] = useState('');
-  const [warningMessage, setWarningMessage] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [message, setMessage] = useState({ text: '', type: '' });
 
   const { push } = useRouter();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setIsProcessing(true);
-    setSuccessMessage('');
-    setWarningMessage('');
-    setErrorMessage('');
+    setMessage({ text: '', type: '' });
     try {
       const result = await addToMailchimp(email, firstName, lastName);
       if (result.result === 'success') {
-        setSuccessMessage("You've been added to the llama list.");
+        setMessage({ text: "You've been added to the llama list.", type: 'success' });
       } else if (result.message === 'Bad Request') {
-        setWarningMessage("You're already in our herd!");
+        setMessage({ text: "You're already in our herd!", type: 'warning' });
       } else {
-        setErrorMessage('There was an error. Please try again.');
+        setMessage({ text: 'There was an error. Please try again.', type: 'error' });
       }
     } catch (error) {
       console.error('Error submitting email:', error);
-      setErrorMessage('There was an error. Please try again.');
+      setMessage({ text: 'There was an error. Please try again.', type: 'error' });
     }
     setIsProcessing(false);
   };
@@ -77,67 +73,55 @@ export const EmailForm = (props: EmailFormProps) => {
           <p className="text-left w-full mt-2 mb-4">
             Become part of our coding community and stay updated with the latest tips and news. Learn more about the community and get llama-zing content delivered to your inbox!
           </p>
-          {successMessage ? (
-            <p className="text-success font-tuner text-sm mb-4 flex items-center gap-2">
-              <TiTick />
-              <span>{successMessage}</span>
+          {message.text && (
+            <p className={`font-tuner text-sm mb-4 flex items-center gap-2 ${message.type === 'success' ? 'text-green-500' : message.type === 'warning' ? 'text-orange-500' : 'text-red-500'}`}>
+              {message.type === 'success' && <TiTick />}
+              {message.type === 'warning' && <IoIosWarning />}
+              {message.type === 'error' && <ImCross />}
+              <span>{message.text}</span>
             </p>
-          ) : warningMessage ? (
-            <p className="text-warning font-tuner text-sm mb-4 flex items-center gap-2">
-              <IoIosWarning />
-              <span>{warningMessage}</span>
-            </p>
-          ) : (
-            <>
-              {errorMessage && (
-                <p className="text-error font-tuner text-sm mb-4 flex items-center gap-2">
-                  <ImCross />
-                  <span>{errorMessage}</span>
-                </p>
-              )}
-              <div className="flex flex-col gap-1.5 md:flex-row md:gap-2 w-full md:mb-1">
-                <Input
-                  placeholder="First name"
-                  name="firstName"
-                  type="text"
-                  onChange={handleInputChange(setFirstName)}
-                  onFocus={handleInputChange(setFirstName)}
-                  disabled={isProcessing}
-                  className="w-full"
-                />
-                <Input
-                  placeholder="Last name"
-                  name="lastName"
-                  type="text"
-                  onChange={handleInputChange(setLastName)}
-                  onFocus={handleInputChange(setLastName)}
-                  disabled={isProcessing}
-                  className="w-full"
-                />
-              </div>
-              <div className="flex flex-col gap-1.5 mt-2 mb-1 w-full">
-                <Input
-                  placeholder="Email"
-                  name="email"
-                  type="email"
-                  onChange={handleInputChange(setEmail)}
-                  onFocus={handleInputChange(setEmail)}
-                  disabled={isProcessing}
-                  className="w-full"
-                />
-              </div>
-              <div className="w-full flex justify-end">
-                <Button
-                  type="submit"
-                  color="orange"
-                  disabled={isProcessing}
-                  className="mt-4"
-                >
-                  {isProcessing ? 'Processing...' : 'Subscribe'}
-                </Button>
-              </div>
-            </>
           )}
+          <div className="flex flex-col gap-1.5 md:flex-row md:gap-2 w-full md:mb-1">
+            <Input
+              placeholder="First name"
+              name="firstName"
+              type="text"
+              onChange={handleInputChange(setFirstName)}
+              onFocus={handleInputChange(setFirstName)}
+              disabled={isProcessing}
+              className="w-full"
+            />
+            <Input
+              placeholder="Last name"
+              name="lastName"
+              type="text"
+              onChange={handleInputChange(setLastName)}
+              onFocus={handleInputChange(setLastName)}
+              disabled={isProcessing}
+              className="w-full"
+            />
+          </div>
+          <div className="flex flex-col gap-1.5 mt-2 mb-1 w-full">
+            <Input
+              placeholder="Email"
+              name="email"
+              type="email"
+              onChange={handleInputChange(setEmail)}
+              onFocus={handleInputChange(setEmail)}
+              disabled={isProcessing}
+              className="w-full"
+            />
+          </div>
+          <div className="w-full flex justify-end">
+            <Button
+              type="submit"
+              color="orange"
+              disabled={isProcessing}
+              className="mt-4"
+            >
+              {isProcessing ? 'Processing...' : 'Subscribe'}
+            </Button>
+          </div>
         </form>
       </div>
     </div>
