92b98ddd3372c03caecafcd7edd7a0d16c8764c5
joshua berman <jber4409@uni.sydney.edu.au>
2024-06-26 15:25:27 +1000

    ui changes

--- components/blocks/VerticalCards.template.ts
+++ components/blocks/VerticalCards.template.ts
@@ -21,10 +21,8 @@ export const verticalCardsTemplate: TinaTemplate = {
       },
       fields: [
         { name: 'headline', label: 'Headline', type: 'string' },
-        { name: 'description', label: 'Description', type: 'string' },
         { name: 'timeDate', label: 'Time and Date', type: 'string' },
         { name: 'location', label: 'Location', type: 'string' },
-        { name: 'price', label: 'Price', type: 'string' },
         { name: 'image', label: 'Image', type: 'string' },
         { name: 'link', label: 'URL', type: 'string'},
         { name: 'globeid', label:'globe-id', type: 'number', description: 'Note this field corresponds to a location on the globe. When adding new events contact a developer to add new locations to the globe.'}
--- components/blocks/VerticalCards.tsx
+++ components/blocks/VerticalCards.tsx
@@ -1,36 +1,43 @@
-import React, { useState } from 'react';
-import Link from 'next/link';
-import css from 'styled-jsx/css';
-import Globe from '../ui/Globe';
-import Image from 'next/image';
+import React, { useState } from 'react'
+import Link from 'next/link'
+import css from 'styled-jsx/css'
+import Globe from '../ui/Globe'
+import Image from 'next/image'
 
 const Card = ({ cardItem, onHover }) => (
-  <Link href={cardItem.link || "#"}>
-    <div 
+  <Link href={cardItem.link || '#'}>
+    <div
       className="relative p-4 mb-4 rounded-md group flex flex-col lg:flex-row bg-gradient-to-br from-white/25 via-white/50 to-white/75 break-inside-avoid rounded-xl shadow-md transform transition-transform duration-300 hover:scale-105 transform-origin-center"
       onMouseEnter={() => onHover(cardItem.globeid)}
       onMouseLeave={() => onHover(null)}
     >
-      <div className="w-full lg:w-1/3 mb-4 lg:mb-0 mr-0 lg:mr-4">
-        <Image src={cardItem.image} alt={cardItem.headline} className="object-cover" width={250} height={0}/>
+      <div className="flex flex-col lg:w-1/3">
+        <div className="relative h-36 w-full">
+          
+          <Image
+            src={cardItem.image}
+            alt={cardItem.headline}
+            className="object-cover"
+            layout="fill"
+            
+          />
+        </div>
       </div>
-      <div className="flex-grow flex flex-col">
-        <h2 className="font-bold text-3xl mb-2">{cardItem.headline}</h2>
-        <p className="text-gray-700 mb-2">{cardItem.description}</p>
-        <p className="text-gray-500 text-md mb-1">{cardItem.timeDate}</p>
-        <p className="text-gray-500 text-md mb-1">{cardItem.location}</p>
-        <p className="text-gray-800 font-semibold">{cardItem.price}</p>
+      <div className="flex-grow flex flex-col pl-4">
+        <h2 className="font-bold text-3xl mb-1">{cardItem.headline}</h2>
+        <p className="text-gray-500 text-md">{cardItem.timeDate }</p>
+        <p className="text-gray-500 text-md ">{cardItem.location}</p>
         <p className="text-orange-500 underline pr-4">Read more</p>
       </div>
       <div className="absolute inset-0 rounded-md z-0 opacity-0 group-hover:opacity-100 transition-opacity duration-500 pointer-events-none"></div>
     </div>
   </Link>
-);
+)
 
 const VerticalCardsBlock = ({ data, index }) => {
-  const [activeGlobeId, setActiveGlobeId] = useState(null);
+  const [activeGlobeId, setActiveGlobeId] = useState(null)
 
-  if (!data || !data.cardItems) return null;
+  if (!data || !data.cardItems) return null
 
   return (
     <div className="md:px-18 lg:px-18 px-3 md:w-3/4 lg:w-5/6 w-full mx-auto pb-4 pt-8">
@@ -43,18 +50,18 @@ const VerticalCardsBlock = ({ data, index }) => {
         </div>
         <div className="flex flex-col w-full lg:w-1/2">
           {data.cardItems.map((cardItem, idx) => (
-            <Card 
-              key={`${index}-${idx}`} 
-              cardItem={cardItem} 
-              onHover={setActiveGlobeId} 
+            <Card
+              key={`${index}-${idx}`}
+              cardItem={cardItem}
+              onHover={setActiveGlobeId}
             />
           ))}
         </div>
       </div>
       <style jsx>{styles}</style>
     </div>
-  );
-};
+  )
+}
 
 const styles = css`
   .card {
@@ -69,6 +76,6 @@ const styles = css`
   .card:hover {
     box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
   }
-`;
+`
 
-export { VerticalCardsBlock };
+export { VerticalCardsBlock }
