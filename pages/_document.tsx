0e1473083871ddb567faac893093ee5483354896
Josh Berman [SSW] <137844305+joshbermanssw@users.noreply.github.com>
2024-07-03 16:54:56 +1000

    💽💄 Data Consent Banner (#1868)
    * Consent Banner Setup + Style
    
    * test pass design w betty
    
    * ready for init test
    
    * psh
    
    * updates
    
    * default true
    
    * default false for GDPR
    
    * change var
    
    * change to push
    
    * rm gtm
    
    * default consent
    
    * remove prod environment check
    
    * rm dataLayer push
    
    * rm initializeGTM
    
    * Fix acceptAll
    
    * ui changes
    
    * Regenerate lockfile
    
    To fix the failing build
    
    * Refresh lock files
    
    * Modal UI fixes
    
    * Remove 🐛 found with GlobeScene absolute positioning on top of DOM
    
    ---------
    
    Co-authored-by: Brady Stroud [SSW] <bradystroud@ssw.com.au>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
@@ -43,6 +44,18 @@ export default class MyDocument extends Document {
               `,
             }}
           />
+          <script
+            id="gtag-base"
+            dangerouslySetInnerHTML={{
+              __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${process.env.GTM_ID}');
+          `,
+            }}
+          />
         </Head>
         <body>
           <noscript>
@@ -54,7 +67,7 @@ export default class MyDocument extends Document {
             />
           </noscript>
           <noscript>
-            <iframe 
+            <iframe
               src={`https://www.googletagmanager.com/ns.html?id=${process.env.GTM_ID}`}
               height="0"
               width="0"
