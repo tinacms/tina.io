08e86d20786e9c6a55c0990ed316e0986a53846c
joshbermanssw <joshberman@ssw.com.au>
2024-07-03 11:04:06 +1000

    rm dataLayer push

--- utils/gtm.ts
+++ utils/gtm.ts
@@ -10,14 +10,13 @@ export const initializeGTM = (consentState: {
   analytics_storage: boolean;
   ad_user_data: boolean;
 }) => {
-  window.dataLayer = window.dataLayer || [];
-
-  window.dataLayer.push({
-    event: 'consent_update',
-    ad_storage: consentState.ad_storage ? 'granted' : 'denied',
-    ad_personalization: consentState.ad_personalization ? 'granted' : 'denied',
-    analytics_storage: consentState.analytics_storage ? 'granted' : 'denied',
-    ad_user_data: consentState.ad_user_data ? 'granted' : 'denied',
-  });
+  // window.dataLayer = window.dataLayer || [];
+  // window.dataLayer.push({
+  //   event: 'consent_update',
+  //   ad_storage: consentState.ad_storage ? 'granted' : 'denied',
+  //   ad_personalization: consentState.ad_personalization ? 'granted' : 'denied',
+  //   analytics_storage: consentState.analytics_storage ? 'granted' : 'denied',
+  //   ad_user_data: consentState.ad_user_data ? 'granted' : 'denied',
+  // });
 
 };
