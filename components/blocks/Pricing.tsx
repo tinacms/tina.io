d4f4ad3d767193c796ce96e978c1610eb9d5e8f9
joshbermanssw <joshberman@ssw.com.au>
2024-07-08 11:23:31 +1000

    Update Pricing

--- components/blocks/Pricing.tsx
+++ components/blocks/Pricing.tsx
@@ -1,3 +1,4 @@
+import RenderButton from 'utils/renderButtonArrayHelper'
 import { RichTextWrapper } from '../layout/RichTextWrapper'
 import { Wrapper } from '../layout/Wrapper'
 import { Actions } from './ActionsButton'
@@ -30,7 +31,10 @@ const PricingCard = ({ data, single = false }) => {
             {/* @ts-ignore */}
             {data.body && <TinaMarkdown content={data.body} />}
           </div>
-          {data.actions && <Actions items={data.actions} />}
+          {data.buttons &&
+              data.buttons.map((button, index) => (
+                <RenderButton key={index} button={button} index={index} />
+              ))}
         </div>
       </div>
       <style jsx>{`
