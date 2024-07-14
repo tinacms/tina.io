e934f17efa7338441cdf29e7d5247f85adc39a9d
joshbermanssw <joshberman@ssw.com.au>
2024-07-15 09:22:38 +1000

    Remove duplicate layer in Schema

--- tina/schema.tsx
+++ tina/schema.tsx
@@ -489,27 +489,29 @@ export const schema = defineSchema({
       format: 'json',
       fields: [
         {
+          name: 'bookingCard',
+          label: 'Booking Card',
           type: 'object',
-          label: 'Meeting List',
-          name: 'meetingList',
           list: true,
+          ui: {
+            itemProps: (item) => ({
+              label: item.name,
+            }),
+          },
           fields: [
+            { name: 'name', label: 'Name', type: 'string' },
+            { name: 'description', label: 'Description', type: 'string' },
             {
-              name: 'bookingCard',
-              label: 'Booking Card',
-              type: 'object',
-              list: true,
-              ui: {
-                itemProps: (item) => ({
-                  label: item.name,
-                }),
-              },
-              fields: [
-                { name: 'name', label: 'Name', type: 'string' },
-                { name: 'description', label: 'Description', type: 'string' },
-                { name: 'image', label: 'Image', type: 'image', description: 'Image headshot for a meeting card' },
-                { name: 'url', label: 'URL', type: 'string', description: 'URL to a meeting link (i.e HubSpot)' },
-              ],
+              name: 'image',
+              label: 'Image',
+              type: 'image',
+              description: 'Image headshot for a meeting card',
+            },
+            {
+              name: 'url',
+              label: 'URL',
+              type: 'string',
+              description: 'URL to a meeting link (i.e HubSpot)',
             },
           ],
         },
--- utils/getMeetingLinks.ts
+++ utils/getMeetingLinks.ts
@@ -2,7 +2,7 @@ import client from 'tina/__generated__/client';
 
 export const fetchMeetingLinks = async () => {
   const meetingPeopleResponse = await client.queries.meetingLinks({ relativePath: 'meetingList.json' });
-  const meetingPeopleData = meetingPeopleResponse.data.meetingLinks.meetingList[0].bookingCard.map((person) => ({
+  const meetingPeopleData = meetingPeopleResponse.data.meetingLinks.bookingCard.map((person) => ({
     name: person.name || '',
     description: person.description || '',
     image: person.image || '',
