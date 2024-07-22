9e4eb70c0c5d77dfa2a042dcda5c1357bc2e721a
joshbermanssw <joshberman@ssw.com.au>
2024-07-22 16:04:39 +1000

    Added Extra Breakpoints

--- components/blocks/CompareBox.tsx
+++ components/blocks/CompareBox.tsx
@@ -231,9 +231,23 @@ export function CompareBoxBlock({ data, index }: CompareBoxBlockProps) {
     centerPadding: '40px',
     responsive: [
       {
+        breakpoint: 1550,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+      {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 3,
           slidesToScroll: 1,
         },
       },
