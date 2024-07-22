46e7fa3b9da35c077a9752dcceb353a7f7412eb5
Josh Berman [SSW] <137844305+joshbermanssw@users.noreply.github.com>
2024-07-22 16:51:12 +1000

    increase logos >1024px (#1933)
    * increase logos >1024px
    
    * Added Extra Breakpoints
    
    * Update tina versions
    
    ---------
    
    Co-authored-by: Brady Stroud [SSW] <bradystroud@ssw.com.au>

--- components/blocks/CompareBox.tsx
+++ components/blocks/CompareBox.tsx
@@ -225,15 +225,29 @@ export function CompareBoxBlock({ data, index }: CompareBoxBlockProps) {
     dots: false,
     infinite: true,
     speed: 500,
-    slidesToShow: 5,
+    slidesToShow: 6,
     slidesToScroll: 1,
     centerMode: true,
     centerPadding: '40px',
     responsive: [
       {
+        breakpoint: 1550,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+      {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 3,
           slidesToScroll: 1,
         },
       },
