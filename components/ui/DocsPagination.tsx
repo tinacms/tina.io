2162dbcd5da00bce1935502d7d0679d4ff14996b
joshbermanssw <joshberman@ssw.com.au>
2024-08-02 16:50:34 +1000

    re-worked next and prev pagination components

--- components/ui/DocsPagination.tsx
+++ components/ui/DocsPagination.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import styled, { css } from 'styled-components'
 import RightArrowSvg from '../../public/svg/right-arrow.svg'
 import { DynamicLink } from '../ui/DynamicLink'
+
 interface NextPrevPageProps {
   title: string
   slug: string
@@ -14,109 +14,46 @@ interface PaginationProps {
 
 export function DocsPagination({ prevPage, nextPage }: PaginationProps) {
   return (
-    <Wrapper>
+    <div className="mt-8 grid grid-cols-2 gap-4 rounded overflow-hidden p-0.5">
       {prevPage && prevPage.slug && (
-        <DynamicLink href={`${prevPage.slug}`} passHref>
-          <PaginationLink previous>
-            <span>Previous</span>
-            <h5>{prevPage.title}</h5>
-            <RightArrowSvg />
-          </PaginationLink>
-        </DynamicLink>
+        <div className="p-0.5" style={{ backgroundColor: '#E9E9E9' }}>
+          <DynamicLink href={`${prevPage.slug}`} passHref>
+            <a
+              className="block p-4 text-left pl-14 relative transition-all group"
+              style={{ backgroundColor: '#FAFAFA' }}
+            >
+              <span className="text-sm uppercase opacity-50">Previous</span>
+              <h5
+                className="text-xl leading-[1.3] m-0 transition-all ease-out duration-150 text-blue-800 group-hover:text-orange-500"
+              >
+                {prevPage.title}
+              </h5>
+              <RightArrowSvg className="absolute left-3 top-1/2 transform -translate-y-1/2 w-8 fill-gray-400 transition-all ease-out duration-150 rotate-180 group-hover:fill-orange-500" />
+            </a>
+          </DynamicLink>
+        </div>
       )}
       {nextPage && nextPage.slug && (
-        <DynamicLink href={`${nextPage.slug}`} passHref>
-          <PaginationLink>
-            <span>Next</span>
-            <h5>{nextPage.title}</h5>
-            <RightArrowSvg />
-          </PaginationLink>
-        </DynamicLink>
+        <div
+          className="p-0.5 col-start-2"
+          style={{ backgroundColor: '#E9E9E9' }}
+        >
+          <DynamicLink href={`${nextPage.slug}`} passHref>
+            <a
+              className="block p-4 text-right pr-14 relative transition-all group"
+              style={{ backgroundColor: '#FAFAFA' }}
+            >
+              <span className="text-sm uppercase opacity-50">Next</span>
+              <h5 className="text-xl leading-[1.3] m-0 transition-all ease-out duration-150 text-blue-800 group-hover:text-orange-500">
+                {nextPage.title}
+              </h5>
+              <RightArrowSvg className="absolute right-3 top-1/2 transform -translate-y-1/2 w-8 fill-gray-400 transition-all ease-out duration-150 group-hover:fill-orange-500" />
+            </a>
+          </DynamicLink>
+        </div>
       )}
-    </Wrapper>
+    </div>
   )
 }
 
 export default DocsPagination
-
-/*
- ** Styles ------------------------------------------
- */
-
-interface PaginationLinkProps {
-  previous?: boolean
-}
-
-const PaginationLink = styled.a<PaginationLinkProps>`
-  padding: 1rem;
-  display: block;
-  flex: 1 1 auto;
-  font-family: var(--font-tuner);
-  font-weight: regular;
-  font-style: normal;
-  text-decoration: none;
-  background-color: #fafafa;
-  color: var(--color-secondary);
-  position: relative;
-  text-align: right;
-  padding-right: 3.5rem;
-  margin: 0 1px 1px 0;
-
-  span {
-    font-size: 0.9375rem;
-    text-transform: uppercase;
-    opacity: 0.5;
-  }
-
-  h5 {
-    font-size: 1.25rem;
-    line-height: 1.3;
-    margin: 0 !important;
-    transition: all 180ms ease-out;
-  }
-
-  svg {
-    position: absolute;
-    right: 0.75rem;
-    top: 50%;
-    transform: translate3d(0, -50%, 0);
-    width: 2rem;
-    height: auto;
-    fill: var(--color-grey);
-    transition: all 180ms ease-out;
-  }
-
-  &:hover {
-    h5 {
-      color: var(--color-orange);
-    }
-    svg {
-      fill: var(--color-orange);
-    }
-  }
-
-  ${props =>
-    props.previous &&
-    css`
-      padding-right: 1rem;
-      padding-left: 3.5rem;
-      text-align: left;
-
-      svg {
-        right: auto;
-        left: 0.75rem;
-        transform: translate3d(0, -50%, 0) rotate(180deg);
-      }
-    `};
-`
-
-const Wrapper = styled.div`
-  margin-top: 2rem;
-  background-color: var(--color-light-dark);
-  display: flex;
-  border-radius: 5px;
-  overflow: hidden;
-  justify-content: space-between;
-  flex-wrap: wrap;
-  padding: 1px 0 0 1px;
-`
