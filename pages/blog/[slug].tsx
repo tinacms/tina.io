df24ce6835e0eb6079545a1eb4bef37939e0dd38
Isaac Lombard [SSW] <152236421+isaaclombardssw@users.noreply.github.com>
2024-08-07 17:19:54 +1000

    🐞 Headings cut off prematurely, small change so they render properly again (#2010)
    * Headings were getting cut off as they were split into children and only the first was being taken in some cases, changed to include all children
    
    * changing the id to match

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -281,9 +281,9 @@ export const components: Components<{
 
 function FormatHeaders({ children, level }) {
   const HeadingTag = `h${level}` as any
-  const id = getDocId(children.props.content[0].text)
+  const id = getDocId(children.props.content.map(content => content.text).join(''))
 
-  return <HeadingTag id={id}>{children.props.content[0].text}</HeadingTag>
+  return <HeadingTag id={id}>{children}</HeadingTag>
 }
 
 function BlogTemplate({ file, siteConfig, ...props }) {
