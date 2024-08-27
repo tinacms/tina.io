35b26c54e68ca359c865904431eaebf6e43faff5
Isaac Lombard <isaaclombard@ssw.com.au>
2024-08-27 10:41:03 +1000

    updating the spacer description (no longer associated to tailwind classes)

--- components/blocks/Spacer.template.ts
+++ components/blocks/Spacer.template.ts
@@ -7,13 +7,13 @@ export const spacerTemplate: Template = {
         {
             name: 'spacingPx', 
             label: 'Spacing Value', 
-            description: 'This is the spacing in pixels provided by the spacer, only the in-built Tailwind values are supported. Set to 0 if not using. See https://tailwindcss.com/docs/height for possible values.', 
+            description: 'This is the spacing in pixels provided by the spacer.', 
             type: 'number',
         },
         { 
             name: 'spacingPxMobile', 
             label: 'Mobile Spacing Value', 
-            description: 'This is the spacing in pixels provided by the spacer on small screens, only the in-built Tailwind values are supported. Set to 0 if not using. See https://tailwindcss.com/docs/height for possible values.', 
+            description: 'This is the spacing in pixels provided by the spacer on small screens.', 
             type: 'number' ,
         }
     ],
