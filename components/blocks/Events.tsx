6bff3b90bb0bdb84726fd6ca53192d523ada9c15
joshbermanssw <joshberman@ssw.com.au>
2024-08-28 21:13:24 +1000

    ui changes to match mockup

--- components/blocks/Events.tsx
+++ components/blocks/Events.tsx
@@ -2,82 +2,85 @@ import React, { useState, Suspense, useRef, useEffect } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import { format } from 'date-fns'
-import { FaCheck } from 'react-icons/fa'
+import { FaCheck, FaChevronRight } from 'react-icons/fa'
 
 const LazyGlobe = React.lazy(() => import('../ui/Globe'))
 
 const Card = ({ cardItem, onHover }) => {
   const getOrdinalSuffix = (day) => {
-    if (day > 3 && day < 21) return 'th';
+    if (day > 3 && day < 21) return 'th'
     switch (day % 10) {
       case 1:
-        return 'st';
+        return 'st'
       case 2:
-        return 'nd';
+        return 'nd'
       case 3:
-        return 'rd';
+        return 'rd'
       default:
-        return 'th';
+        return 'th'
     }
-  };
+  }
 
   const calculateDaysUntilEvent = (date) => {
-    const eventDate = new Date(date);
-    const currentDate = new Date();
-    eventDate.setHours(0, 0, 0, 0);
-    currentDate.setHours(0, 0, 0, 0);
+    const eventDate = new Date(date)
+    const currentDate = new Date()
+    eventDate.setHours(0, 0, 0, 0)
+    currentDate.setHours(0, 0, 0, 0)
 
-    const timeDifference = eventDate.getTime() - currentDate.getTime();
-    const daysDifference = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
-    return daysDifference;
-  };
+    const timeDifference = eventDate.getTime() - currentDate.getTime()
+    const daysDifference = Math.ceil(timeDifference / (1000 * 60 * 60 * 24))
+    return daysDifference
+  }
 
   const isDateInRange = (startDate, endDate) => {
-    const start = new Date(startDate);
-    const end = new Date(endDate);
-    const current = new Date();
-    start.setHours(0, 0, 0, 0);
-    end.setHours(0, 0, 0, 0);
-    current.setHours(0, 0, 0, 0);
+    const start = new Date(startDate)
+    const end = new Date(endDate)
+    const current = new Date()
+    start.setHours(0, 0, 0, 0)
+    end.setHours(0, 0, 0, 0)
+    current.setHours(0, 0, 0, 0)
 
-    return current >= start && current <= end;
-  };
+    return current >= start && current <= end
+  }
 
   const formatStartDate = (date) => {
-    const d = new Date(date);
-    return `${d.getDate()}${getOrdinalSuffix(d.getDate())} ${format(d, 'MMM')}`;
-  };
+    const d = new Date(date)
+    return `${d.getDate()}${getOrdinalSuffix(d.getDate())} ${format(d, 'MMM')}`
+  }
 
   const formatDateRange = (start, end) => {
-    const startDate = new Date(start);
-    const endDate = new Date(end);
+    const startDate = new Date(start)
+    const endDate = new Date(end)
     if (startDate.getMonth() === endDate.getMonth()) {
       return `${startDate.getDate()}${getOrdinalSuffix(
         startDate.getDate()
       )} - ${endDate.getDate()}${getOrdinalSuffix(endDate.getDate())} ${format(
         endDate,
         'MMM'
-      )}`;
+      )}`
     }
     return `${startDate.getDate()}${getOrdinalSuffix(
       startDate.getDate()
     )} ${format(startDate, 'MMM')} - ${endDate.getDate()}${getOrdinalSuffix(
       endDate.getDate()
-    )} ${format(endDate, 'MMM')}`;
-  };
+    )} ${format(endDate, 'MMM')}`
+  }
 
   const displayDate = () => {
     if (cardItem.startDate && cardItem.endDate) {
-      return formatDateRange(cardItem.startDate, cardItem.endDate);
+      return formatDateRange(cardItem.startDate, cardItem.endDate)
     } else if (cardItem.startDate) {
-      return formatStartDate(cardItem.startDate);
+      return formatStartDate(cardItem.startDate)
     }
-    return '';
-  };
+    return ''
+  }
 
-  const daysUntilEvent = calculateDaysUntilEvent(cardItem.startDate);
-  const isPastEvent = daysUntilEvent < 0; // Check if the event is in the past
-  const isLiveEvent = cardItem.startDate && cardItem.endDate && isDateInRange(cardItem.startDate, cardItem.endDate);
+  const daysUntilEvent = calculateDaysUntilEvent(cardItem.startDate)
+  const isPastEvent = daysUntilEvent < 0
+  const isLiveEvent =
+    cardItem.startDate &&
+    cardItem.endDate &&
+    isDateInRange(cardItem.startDate, cardItem.endDate)
 
   return (
     <div
@@ -97,32 +100,34 @@ const Card = ({ cardItem, onHover }) => {
           </div>
         )}
       </div>
-      <div className="flex-grow flex flex-col pl-4 overflow-hidden">
-        {isLiveEvent ? (
-          <div className="text-sm text-red-500 flex justify-end">LIVE</div>
-        ) : isPastEvent ? (
-          <div className="absolute top-0 right-0 flex justify-center items-center w-24 h-24 transform translate-x-12 -translate-y-12">
-            <div className="w-24 h-24 bg-orange-400 transform rotate-45"></div>
-            <FaCheck className="text-lg text-white absolute -translate-x-5 translate-y-5" />
-          </div>
-        ) : (
-          <div className="text-sm text-orange-500 flex justify-end">
-            {daysUntilEvent} day{daysUntilEvent > 1 ? 's' : ''} left
-          </div>
-        )}
-        <h3 className="font-bold text-2xl mb-1">{cardItem.headline}</h3>
-        <p className="text-gray-500 text-sm">{displayDate()}</p>
-        <p className="text-gray-500 text-sm">{cardItem.location}</p>
+      <div className="flex-grow flex flex-col pl-4 overflow-hidden py-4">
+        <h3 className="font-tuner text-2xl mb-1 bg-gradient-to-br from-orange-400 via-orange-500 to-orange-600 bg-clip-text text-transparent">
+          {cardItem.headline}
+        </h3>
+        <div className="flex items-center text-md text-gray-500">
+          <p className="mr-2">{displayDate()}</p>
+          {isLiveEvent ? (
+            <span className="bg-teal-100 px-2 rounded-lg shadow-lg">LIVE</span>
+          ) : isPastEvent ? (
+            <span className="bg-gray-200 px-2 rounded-lg shadow-lg">DONE</span>
+          ) : (
+            <span className="bg-teal-100 px-2 rounded-lg shadow-lg">
+              {daysUntilEvent} DAY{daysUntilEvent > 1 ? 'S' : ''} TO GO
+            </span>
+          )}
+        </div>
+        <p className="text-gray-500 text-md">{cardItem.location}</p>
         <Link href={cardItem.link || '#'}>
-          <p className="text-orange-500 underline pt-1 pr-4 text-sm">
+          <p className="font-tuner pt-1 pr-4 text-md bg-gradient-to-br from-blue-700 via-blue-850 to-blue-1000 bg-clip-text text-transparent inline-flex items-center">
             Read more
+            <FaChevronRight className="text-md text-blue-700 ml-1" />
           </p>
         </Link>
       </div>
       <div className="absolute inset-0 rounded-md z-0 opacity-0 group-hover:opacity-100 transition-opacity duration-500 pointer-events-none"></div>
     </div>
-  );
-};
+  )
+}
 
 const EventsBlock = ({ data, index }) => {
   const [activeGlobeId, setActiveGlobeId] = useState(null)
@@ -155,7 +160,7 @@ const EventsBlock = ({ data, index }) => {
   }, [])
 
   return (
-    <div className="md:px-18 lg:px-18 px-3 md:w-4/5 lg:w-5/6 w-full mx-auto pb-4 pt-8">
+    <div className="md:px-18 lg:px-10 px-3 md:w-4/5 lg:w-5/6 w-full mx-auto pb-4 pt-8">
       <h1 className="pl-3 font-tuner flex items-center justify-center text-3xl lg:text-5xl lg:leading-tight bg-gradient-to-br from-blue-600/80 via-blue-800/80 to-blue-1000 bg-clip-text text-transparent text-balance text-left mt-10 pb-12">
         {data.title}
       </h1>
@@ -188,7 +193,7 @@ const EventsBlock = ({ data, index }) => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export { EventsBlock as VerticalCardsBlock };
+export { EventsBlock as VerticalCardsBlock }
