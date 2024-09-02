012504e2796746955fd085b2e707f53ba27ad5d0
joshbermanssw <joshberman@ssw.com.au>
2024-09-02 18:00:31 +1000

    refactor IconSelector component

--- components/blocks/IconSelector.tsx
+++ components/blocks/IconSelector.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   FaClock,
   FaUnlock,
@@ -12,12 +12,10 @@ import {
   FaDatabase,
   FaShare,
   FaDesktop,
-  FaLockOpen
-
+  FaLockOpen,
 } from 'react-icons/fa'
 import { AiOutlineUser } from 'react-icons/ai'
-import { BiBadge } from 'react-icons/bi'
-import { BiSupport } from 'react-icons/bi'
+import { BiBadge, BiSupport } from 'react-icons/bi'
 import { AiOutlineUsergroupAdd } from 'react-icons/ai'
 import { CgCrown } from 'react-icons/cg'
 import { HiOutlineSparkles } from 'react-icons/hi2'
@@ -45,23 +43,52 @@ const icons = {
   FaShare,
   FaDesktop,
   FaLockOpen,
-  FaHandPointer
+  FaHandPointer,
 }
 
 const IconSelector = ({ input }) => {
   const iconKeys = Object.keys(icons)
+  const [selectedIcon, setSelectedIcon] = useState(input.value || '')
+  const [isMinimized, setIsMinimized] = useState(true)
+
+  const handleIconChange = (iconKey) => {
+    setSelectedIcon(iconKey)
+    input.onChange(iconKey)
+  }
+
+  const toggleMinimize = () => {
+    setIsMinimized(!isMinimized)
+  }
 
   return (
-    <select
-      value={input.value}
-      onChange={(event) => input.onChange(event.target.value)}
-    >
-      {iconKeys.map((key) => (
-        <option key={key} value={key}>
-          {key}
-        </option>
-      ))}
-    </select>
+    <div>
+      <button onClick={toggleMinimize} className='text-red-600 text-sm mb-2'>
+        {isMinimized ? 'Show Icons' : 'Minimize Icons'}
+      </button>
+      {!isMinimized && (
+        <div className="grid grid-cols-2 gap-2">
+          {iconKeys.map((key) => {
+            const IconComponent = icons[key]
+            const trimmedKey = key.slice(2)
+            return (
+              <div
+                key={key}
+                onClick={() => handleIconChange(key)}
+                className={`flex items-center cursor-pointer p-2 ${
+                  selectedIcon === key ? 'bg-blue-200' : 'bg-white'
+                }`}
+              >
+                <IconComponent
+                  color={selectedIcon === key ? 'blue' : 'black'}
+                  size={16}
+                />
+                <span className="text-xs">{trimmedKey}</span>
+              </div>
+            )
+          })}
+        </div>
+      )}
+    </div>
   )
 }
 
