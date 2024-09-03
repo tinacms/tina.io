7243a7e4b5f96d1da5b374757559449d5deba45f
joshbermanssw <joshberman@ssw.com.au>
2024-09-03 23:21:46 +1000

    added cursor-pointer to <DynamicLink> component

--- components/ui/DynamicLink.tsx
+++ components/ui/DynamicLink.tsx
@@ -1,18 +1,21 @@
-import Link, { LinkProps } from 'next/link'
-import { getDynamicPath } from '../../utils/getDynamicPath'
+import Link, { LinkProps } from 'next/link';
+import { getDynamicPath } from '../../utils/getDynamicPath';
 
-type ExtraProps = Omit<LinkProps, 'as' | 'href'>
+type ExtraProps = Omit<LinkProps, 'as' | 'href'>;
 
 interface DynamicLinkProps extends ExtraProps {
-  href: string
-  children: any
+  href: string;
+  children: any;
 }
 
 export const DynamicLink = ({ children, href, ...props }: DynamicLinkProps) => {
   return (
-    //Work around for using legacyBehaviour so that anchors in HTML in MDX files do not cause legacy errors
-    <Link href={href} {...props} legacyBehavior>
-      {children}
-    </Link>
-  )
-}
+    // Because {children} contains <a> tags we need to wrap it in a div to pass className attributes in. If we inserted into the <Link> it doesnt get passed in. 
+    <div className="cursor-pointer">
+      {/* Work around for using legacyBehavior so that anchors in HTML in MDX files do not cause legacy errors */}
+      <Link href={href} {...props} legacyBehavior>
+        {children}
+      </Link>
+    </div>
+  );
+};
