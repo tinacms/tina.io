05e49466276632e64ff7019fdb558689f7816f48
joshbermanssw <joshberman@ssw.com.au>
2024-09-06 01:55:34 +1000

    add action button and modal button templates

--- tina/collectionsSchema/navigationBar.tsx
+++ tina/collectionsSchema/navigationBar.tsx
@@ -1,3 +1,5 @@
+const modals = ['BookDemo.tsx', 'EmailForm.tsx'];
+
 export const navigationBarCollection = {
   label: 'Navigation Bar',
   name: 'navigationBar',
@@ -20,7 +22,7 @@ export const navigationBarCollection = {
           label: 'String Item',
           ui: {
             itemProps: (item) => {
-              return { label: item?.label };
+              return { label: '📄' + item?.label };
             },
           },
           fields: [
@@ -33,40 +35,6 @@ export const navigationBarCollection = {
               name: 'href',
               label: 'href',
               type: 'string',
-            },
-          ],
-        },
-        {
-          name: 'buttonItem',
-          label: 'Button Item',
-          ui: {
-            itemProps: (item) => {
-              return { label: item?.label };
-            },
-          },
-          fields: [
-            {
-              name: 'label',
-              label: 'Label',
-              type: 'string',
-            },
-            {
-              name: 'href',
-              label: 'href',
-              type: 'string',
-            },
-            {
-              name: 'buttonColour',
-              label: 'Button Colour',
-              type: 'string',
-              options: [
-                { value: 'default', label: 'Seafoam' },
-                { value: 'blue', label: 'Blue' },
-                { value: 'orange', label: 'Orange' },
-                { value: 'white', label: 'White' },
-                { value: 'ghost', label: 'Ghost' },
-                { value: 'command', label: 'Command' },
-              ],
             },
           ],
         },
@@ -77,7 +45,7 @@ export const navigationBarCollection = {
           type: 'object',
           ui: {
             itemProps: (item) => {
-              return { label: item?.label };
+              return { label: '📕' + item?.label };
             },
           },
           fields: [
@@ -107,8 +75,106 @@ export const navigationBarCollection = {
                   label: 'href',
                   type: 'string',
                 },
-              ]
+              ],
             },
+          ],
+        },
+        //Note we are duplicating the ModalButton template from /components/blocks/ModalButton.template.ts but this is
+        //because this file is generated before hand and i still want to keep the ModalButton coupled into the components dir too
+
+        {
+          label: 'Modal Button',
+          name: 'modalButton',
+          type: 'object',
+          list: true,
+          ui: {
+            itemProps: (item) => {
+              return { label: '🚨' + item?.label };
+            },
+            defaultItem: {
+              variant: 'default',
+              label: 'Secondary Action',
+              icon: false,
+              size: 'medium',
+            },
+          },
+          fields: [
+            { name: 'label', label: 'Label', type: 'string' },
+            { name: 'icon', label: 'Icon', type: 'boolean' },
+            {
+              name: 'color',
+              label: 'Color',
+              type: 'string',
+              options: [
+                { value: 'default', label: 'Seafoam' },
+                { value: 'blue', label: 'Blue' },
+                { value: 'orange', label: 'Orange' },
+                { value: 'white', label: 'White' },
+                { value: 'ghost', label: 'Ghost' },
+                { value: 'command', label: 'Command' },
+              ],
+            },
+            {
+              name: 'size',
+              label: 'Size',
+              type: 'string',
+              options: [
+                { value: 'small', label: 'Small' },
+                { value: 'medium', label: 'Medium' },
+                { value: 'large', label: 'Large' },
+              ],
+            },
+            { name: 'modal', label: 'Modal', type: 'string', options: modals },
+          ],
+        },
+
+        //Note we are duplicating the ActionsButton template from /components/blocks/ActionsButton.template.ts but this is
+        //because this file is generated before hand and i still want to keep the ModalButton coupled into the components dir too
+
+        {
+          label: 'Actions',
+          name: 'actions',
+          type: 'object',
+          list: true,
+          ui: {
+            itemProps: (item) => {
+              return { label: '🗡️' + item?.label };
+            },
+            defaultItem: {
+              variant: 'default',
+              label: 'Secondary Action',
+              icon: false,
+              size: 'medium',
+              url: '/',
+            },
+          },
+          fields: [
+            { name: 'label', label: 'Label', type: 'string' },
+            { name: 'icon', label: 'Icon', type: 'boolean' },
+            {
+              name: 'variant',
+              label: 'Variant',
+              type: 'string',
+              options: [
+                { value: 'default', label: 'Seafoam' },
+                { value: 'blue', label: 'Blue' },
+                { value: 'orange', label: 'Orange' },
+                { value: 'white', label: 'White' },
+                { value: 'ghost', label: 'Ghost' },
+                { value: 'orangeWithBorder', label: 'Orange with Border' },
+              ],
+            },
+            {
+              name: 'size',
+              label: 'Size',
+              type: 'string',
+              options: [
+                { value: 'small', label: 'Small' },
+                { value: 'medium', label: 'Medium' },
+                { value: 'large', label: 'Large' },
+              ],
+            },
+            { name: 'url', label: 'URL', type: 'string' },
           ],
         },
       ],
