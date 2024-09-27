9be16d2ccc57d3a1dac3bc5c746e732204948bc7
Isaac Lombard [SSW] <152236421+isaaclombardssw@users.noreply.github.com>
2024-09-27 10:29:44 +1000

    Update graphql.ts

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,4 +1,3 @@
-//TODO – check if this is still needed, the graphiql playgrounds have already been replaced, see https://github.com/tinacms/tina.io/issues/2001
 import * as datalayerPackage from '@tinacms/datalayer-old'
 import * as gqlPackage from '@tinacms/graphql-old'
 
