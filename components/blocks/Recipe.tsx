e475b2519fb06da1c870d280da01b0824c813e56
joshbermanssw <joshberman@ssw.com.au>
2024-11-01 17:00:50 +1100

    change codeblock name

--- components/blocks/Recipe.tsx
+++ components/blocks/Recipe.tsx
@@ -1,3 +1,4 @@
+import { FaChevronCircleDown } from 'react-icons/fa';
 import { TinaMarkdown } from 'tinacms/dist/rich-text';
 import React, { useEffect, useState, useRef } from 'react';
 import Prism from 'prismjs';
@@ -6,6 +7,7 @@ import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
 import 'prismjs/plugins/line-numbers/prism-line-numbers';
 import 'prismjs/plugins/line-highlight/prism-line-highlight.css';
 import 'prismjs/plugins/line-highlight/prism-line-highlight';
+import { MdOutlineContentCopy } from 'react-icons/md';
 
 const customHighlightCSS = `
   :not(pre) > code[class*="language-"],
@@ -22,35 +24,34 @@ const customHighlightCSS = `
     padding: 0.5rem;
   }
 
-.line-numbers-rows > span:before {
+  .line-numbers-rows > span:before {
     content: counter(linenumber);
     color: #9FFCEF; 
     display: block;
     padding-right: 0.8em;
     text-align: right;
-}
+  }
 
-.line-highlight {
-  background: rgba(71, 85, 105, 0.25);
-  border: 1px solid rgba(71, 85, 105, 1);
-}
+  .line-highlight {
+    background: rgba(71, 85, 105, 0.25);
+    border: 1px solid rgba(71, 85, 105, 1);
+  }
 
   .line-numbers .line-numbers-rows {
-  border-right: 1px solid #6B7280;
+    border-right: 1px solid #6B7280;
   }
 
   pre[class*="language-"] {
     padding: 1em;
     margin: 0 0 0.5em 0; 
     overflow: auto;
-}
+  }
 
-pre[class*="language-"] ::selection {
-  background: white; 
-  color: black; 
-}
+  pre[class*="language-"] ::selection {
+    background: white; 
+    color: black; 
+  }
 `;
-
 
 export const RecipeBlock = ({ data }) => {
   const { title, description, codeblock, instruction } = data;
@@ -61,12 +62,12 @@ export const RecipeBlock = ({ data }) => {
   );
   const [LHSheight, setLHSheight] = useState<string | null>(null);
   const [CodeBlockWidth, setCodeBlockWidth] = useState<string | null>(null);
+  const [isBottomOfInstructions, setIsBottomOfInstructions] = useState<boolean>(false);
 
-  const rhsRef = useRef<HTMLDivElement>(null);
+  const codeblockRef = useRef<HTMLDivElement>(null);
   const instructionRefs = useRef<(HTMLDivElement | null)[]>([]);
 
   useEffect(() => {
-    
     const style = document.createElement('style');
     style.textContent = customHighlightCSS;
     document.head.appendChild(style);
@@ -77,9 +78,15 @@ export const RecipeBlock = ({ data }) => {
   }, [highlightLines]);
 
   useEffect(() => {
-    setLHSheight(`${rhsRef.current?.offsetHeight}`);
-    setCodeBlockWidth(`${rhsRef.current?.offsetWidth}`);
+    setLHSheight(`${codeblockRef.current?.offsetHeight}`);
+    setCodeBlockWidth(`${codeblockRef.current?.offsetWidth}`);
   });
+
+  const checkIfBottom = (event: React.UIEvent<HTMLDivElement>) => {
+    const { scrollHeight, scrollTop, clientHeight } = event.currentTarget;
+    //Checking if we are at the bottom <10> is just an arbitrary buffer threshold 
+    setIsBottomOfInstructions(scrollHeight - scrollTop <= clientHeight + 10);
+  };
 
   const handleInstructionClick = (
     index: number,
@@ -90,8 +97,8 @@ export const RecipeBlock = ({ data }) => {
     setClickedInstruction(index === clickedInstruction ? null : index);
 
     // Scroll RHS codeblock view
-    if (rhsRef.current) {
-      rhsRef.current.scrollTo({
+    if (codeblockRef.current) {
+      codeblockRef.current.scrollTo({
         top: 24 * codeLineStart - 20,
         behavior: 'smooth',
       });
@@ -99,7 +106,6 @@ export const RecipeBlock = ({ data }) => {
 
     // Scroll to the clicked instruction in mobile view
     if (window.innerWidth < 1024 && instructionRefs.current[index]) {
-      // 1024px as threshold for md
       instructionRefs.current[index].scrollIntoView({
         behavior: 'smooth',
         block: 'nearest',
@@ -107,10 +113,14 @@ export const RecipeBlock = ({ data }) => {
     }
   };
 
+  const handleDownArrowClick = () => {
+
+  };
+
   const smAndMbHeight = LHSheight ? `${Number(LHSheight) / 2}px` : null;
 
   return (
-    <div className="recipe-block-container mt-20">
+    <div className="recipe-block-container mt-20 relative">
       <div className="title-description px-10">
         <h2 className="font-tuner text-orange-500 text-2xl">
           {title || 'Default Title'}
@@ -119,12 +129,18 @@ export const RecipeBlock = ({ data }) => {
           {description || 'Default Description'}
         </p>
       </div>
+      
+      <FaChevronCircleDown 
+        onClick={handleDownArrowClick} 
+        className={`absolute top-72 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-xl text-white cursor-pointer shadow-md z-10 lg:hidden ${isBottomOfInstructions ? 'hidden' : ''}`} 
+      />
+
       <div className="content-wrapper flex flex-col lg:flex-row px-10 items-stretch">
         <div
           className="instructions bg-gray-800 relative lg:w-1/3 max-h-50vh flex-shrink-0 flex-grow rounded-tl-xl rounded-tr-xl lg:rounded-tr-none lg:rounded-bl-xl overflow-auto flex flex-col"
+          onScroll={checkIfBottom} 
           style={{
-            //I have this inline styling because i couldnt get the height conditions working with tailwind lg: and md: etc
-            height: 
+            height:
               typeof window !== 'undefined' && window.innerWidth >= 1024
                 ? `${LHSheight}px`
                 : `${smAndMbHeight}`,
@@ -163,7 +179,7 @@ export const RecipeBlock = ({ data }) => {
         </div>
 
         <div
-          ref={rhsRef}
+          ref={codeblockRef}
           className="codeblock bg-gray-900 lg:w-2/3 max-h-50vh overflow-auto lg:rounded-tr-xl rounded-bl-xl lg:rounded-bl-none rounded-br-xl "
         >
           {codeblock ? (
@@ -199,8 +215,6 @@ interface CodeBlockProps {
   highlightLines: string;
 }
 
-import { MdOutlineContentCopy } from 'react-icons/md';
-
 export const CodeToolbar = ({
   lang,
   onCopy,
@@ -210,7 +224,7 @@ export const CodeToolbar = ({
   onCopy: () => void;
   tooltipVisible: boolean;
 }) => (
-  <div className={`code-toolbar bg-gray-800 text-white px-4 py-2 lg:rounded-t-xl text-sm font-semibold flex justify-between items-center`}>
+  <div className="code-toolbar bg-gray-800 text-white px-4 py-2 lg:rounded-t-xl text-sm font-semibold flex justify-between items-center">
     <span className="font-tuner">{lang || 'Unknown'}</span>
     <div className="flex items-center ml-4 space-x-4 relative overflow-visible">
       <button
@@ -219,13 +233,12 @@ export const CodeToolbar = ({
       >
         {!tooltipVisible && 
         <MdOutlineContentCopy className="w-4 h-4" />}
-        <span className={``}>{!tooltipVisible ? 'Copy' : 'Copied!'}</span>
+        <span>{!tooltipVisible ? 'Copy' : 'Copied!'}</span>
         
       </button>
     </div>
   </div>
 );
-
 
 const CodeBlockWithHighlightLines = ({
   value,
@@ -266,28 +279,27 @@ const CodeBlockWithHighlightLines = ({
 
   return (
     <div className="codeblock-container">
-  <div className="sticky top-0 z-50">
-    <CodeToolbar
-      lang={lang}
-      onCopy={copyToClipboard}
-      tooltipVisible={tooltipVisible}
-    />
-  </div>
-  <pre
-    className="line-numbers"
-    data-line={highlightLines}
-    style={{
-      overflowX: 'hidden',
-      maxWidth: '100%',
-      whiteSpace: 'pre-wrap',
-      wordBreak: 'break-word',
-    }}
-  >
-    <code className={`language-${lang || 'jsx'}`}>
-      {typeof children === 'string' || children ? children : value}
-    </code>
-  </pre>
-</div>
-
+      <div className="sticky top-0 z-50">
+        <CodeToolbar
+          lang={lang}
+          onCopy={copyToClipboard}
+          tooltipVisible={tooltipVisible}
+        />
+      </div>
+      <pre
+        className="line-numbers"
+        data-line={highlightLines}
+        style={{
+          overflowX: 'hidden',
+          maxWidth: '100%',
+          whiteSpace: 'pre-wrap',
+          wordBreak: 'break-word',
+        }}
+      >
+        <code className={`language-${lang || 'jsx'}`}>
+          {typeof children === 'string' || children ? children : value}
+        </code>
+      </pre>
+    </div>
   );
 };
