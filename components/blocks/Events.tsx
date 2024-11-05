b61d2c6bf2d5a55766bbf5663406f892f258bf82
isaaclombardssw <isaaclombard@ssw.com.au>
2024-11-06 09:24:50 +1100

    fixes

--- components/blocks/Events.tsx
+++ components/blocks/Events.tsx
@@ -2,8 +2,7 @@ import { format } from 'date-fns';
 import Image from 'next/image';
 import Link from 'next/link';
 import React, { Suspense, useEffect, useRef, useState } from 'react';
-import { FaCheck, FaChevronRight } from 'react-icons/fa';
-import { start } from 'repl';
+import { FaChevronRight } from 'react-icons/fa';
 
 const LazyGlobe = React.lazy(() => import('../ui/Globe'));
 
