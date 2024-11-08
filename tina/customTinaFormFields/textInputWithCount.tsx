089f9aefc91b61cd682c0f3a558a9f51660210e0
isaaclombardssw <isaaclombard@ssw.com.au>
2024-11-08 15:45:21 +1100

    removing linting ignore decorators

--- tina/customTinaFormFields/textInputWithCount.tsx
+++ tina/customTinaFormFields/textInputWithCount.tsx
@@ -8,13 +8,11 @@ export const TextInputWithCount = (max: number, isTextArea: boolean = false) =>
     <div className="flex flex-col gap-2">
       {isTextArea ? (
         <textarea
-          // eslint-disable-next-line tailwindcss/no-custom-classname
           className="focus:shadow-outline block min-h-40 w-full resize-y rounded-md border border-gray-200 px-3 py-2 text-base text-gray-600 shadow-inner focus:border-blue-500 focus:text-gray-900"
           {...input}
         />
       ) : (
         <input
-          // eslint-disable-next-line tailwindcss/no-custom-classname
           className="focus:shadow-outline block w-full rounded-md border border-gray-200 bg-white px-3 py-2 text-base text-gray-600 shadow-inner transition-all duration-150 ease-out placeholder:text-gray-300 focus:border-blue-500 focus:text-gray-900 focus:outline-none"
           {...input}
         />
