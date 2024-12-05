c6229509ef4a1a5eb310cc1a5181678a85dfb737
joshbermanssw <joshberman@ssw.com.au>
2024-12-05 14:34:29 +1100

    added hover feedback

--- components/blocks/Events/Events.tsx
+++ components/blocks/Events/Events.tsx
@@ -220,7 +220,7 @@ const EventsBlock = () => {
             href="/events"
             className="pt-10 mb-10 font-bold flex items-center justify-end gap-2"
           >
-            <span className="bg-gradient-to-br text-md from-orange-400 via-orange-500 to-orange-600  bg-clip-text text-transparent">
+            <span className="bg-gradient-to-br text-md from-orange-400 via-orange-500 to-orange-600 hover:from-orange-500 hover:via-orange-700 hover:to-orange-800  bg-clip-text text-transparent">
               SEE ALL EVENTS
             </span>
             <FaArrowRight className="text-orange-500" />
