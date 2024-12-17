0779ef22ac92b5df33885502166039f5f642073c
joshbermanssw <joshberman@ssw.com.au>
2024-12-18 09:12:48 +1100

    move clarity into _app.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -45,18 +45,6 @@ export default class MyDocument extends Document {
               `,
             }}
           />
-          {/* Microsoft Clarity */}
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function(c,l,a,r,i,t,y){
-                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                })(window, document, "clarity", "script", "pepjushhm5");
-              `,
-            }}
-          />
         </Head>
         <body>
           <noscript>
