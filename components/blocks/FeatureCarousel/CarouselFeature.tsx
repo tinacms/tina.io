f4e50d0dba159fdb53f5ae41b424b4aa3c3c6dbf
isaaclombardssw <isaaclombard@ssw.com.au>
2024-12-18 09:52:32 +1100

    images

--- components/blocks/FeatureCarousel/CarouselFeature.tsx
+++ components/blocks/FeatureCarousel/CarouselFeature.tsx
@@ -11,8 +11,6 @@ const checkTouchScreen = () => {
   let hasTouchScreen = false;
   if ('maxTouchPoints' in navigator) {
     hasTouchScreen = navigator.maxTouchPoints > 0;
-  } else if ('msMaxTouchPoints' in navigator) {
-    hasTouchScreen = navigator.msMaxTouchPoints > 0;
   } else {
     const mQ = matchMedia?.('(pointer:coarse)');
     if (mQ?.media === '(pointer:coarse)') {
@@ -21,7 +19,7 @@ const checkTouchScreen = () => {
       hasTouchScreen = true; // deprecated, but good fallback
     } else {
       // Only as a last resort, fall back to user agent sniffing
-      const UA = navigator.userAgent;
+      const UA: string = (navigator as Navigator).userAgent;
       hasTouchScreen =
         /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
         /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA);
