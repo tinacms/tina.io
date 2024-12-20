88f22771024603d4e8214fbee3817eaf91e53243
joshbermanssw <joshberman@ssw.com.au>
2024-12-20 16:38:23 +1100

    extrapolate fetchEvents

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,12 +1,18 @@
-// app/events/page.tsx
 import { client } from 'tina/__generated__/client';
 import EventsClient from './EventsClient';
 
 export default async function EventsPage() {
   const vars = {};
 
-  const res = await client.queries.eventsConnection(vars);
-  const { query, data } = res;
+  const { data, query } = await fetchEvents(vars);
 
   return <EventsClient query={query} data={data} vars={vars} />;
 }
+
+const fetchEvents = async (vars = {}) => {
+  const res = await client.queries.eventsConnection(vars);
+  return {
+    data: res.data,
+    query: res.query,
+  };
+};
