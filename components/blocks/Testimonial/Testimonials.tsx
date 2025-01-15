19db75bc09de88ee186cc5c5d06952cf2bce8886
Tino Liu [SSW] <tinoliu@ssw.com.au>
2025-01-15 14:33:12 +0800

    Implement Marquee into testimonials block

--- components/blocks/Testimonial/Testimonials.tsx
+++ components/blocks/Testimonial/Testimonials.tsx
@@ -1,104 +1,84 @@
+import Marquee from '@/components/ui/marquee';
+import { cn } from '@/lib/utils';
 import Image from 'next/image';
 import { useMemo, useRef, useState } from 'react';
 import { tinaField } from 'tinacms/dist/react';
 import { TinaMarkdown } from 'tinacms/dist/rich-text';
 import { formatDate } from 'utils';
 
-const checkTouchScreen = () => {
-  let hasTouchScreen = false;
-  if ('maxTouchPoints' in navigator) {
-    hasTouchScreen = navigator.maxTouchPoints > 0;
-  } else {
-    const mQ = matchMedia?.('(pointer:coarse)');
-    if (mQ?.media === '(pointer:coarse)') {
-      hasTouchScreen = !!mQ.matches;
-    } else if ('orientation' in window) {
-      hasTouchScreen = true; // deprecated, but good fallback
-    } else {
-      // Only as a last resort, fall back to user agent sniffing
-      const UA: string = (navigator as Navigator).userAgent;
-      hasTouchScreen =
-        /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
-        /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA);
-    }
-  }
-  return hasTouchScreen;
-};
+// const checkTouchScreen = () => {
+//   let hasTouchScreen = false;
+//   if ('maxTouchPoints' in navigator) {
+//     hasTouchScreen = navigator.maxTouchPoints > 0;
+//   } else {
+//     const mQ = matchMedia?.('(pointer:coarse)');
+//     if (mQ?.media === '(pointer:coarse)') {
+//       hasTouchScreen = !!mQ.matches;
+//     } else if ('orientation' in window) {
+//       hasTouchScreen = true; // deprecated, but good fallback
+//     } else {
+//       // Only as a last resort, fall back to user agent sniffing
+//       const UA: string = (navigator as Navigator).userAgent;
+//       hasTouchScreen =
+//         /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
+//         /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA);
+//     }
+//   }
+//   return hasTouchScreen;
+// };
 
-const Testimonial = ({ data, ...props }) => {
-  const Elem = data.link ? 'a' : 'div';
+const TestimonialCard = ({ ...data }) => {
+  const Elem = data?.link ? 'a' : 'div';
 
   return (
-    <Elem
-      className={`group px-7 lg:px-8 py-6 lg:py-7 mb-6 break-inside-avoid rounded-md bg-gradient-to-b from-white to-white/30 shadow-[inset_0_0_0_1px_rgba(223,219,252,0.15),_0_0_1px_1px_rgba(223,219,252,0.5)] flex flex-col gap-5 text-gray-700 ${
-        data.link
-          ? 'cursor-pointer hover:shadow-lg hover:bg-white hover:scale-[1.01] transition-all duration-150 ease-out'
-          : ''
-      }`}
-      href={data.link}
-      {...props}
-      data-tina-field={tinaField(data, 'name')}
-    >
-      {data.testimonial && (
-        <div className="text-base lg:text-md">
-          <TinaMarkdown
-            // components={contentComponents}
-            content={data.testimonial}
-          />
-        </div>
-      )}
-      <div className="flex gap-4 items-center">
-        {data.avatar && (
-          <div
-            className={`relative shrink-0 w-14 h-14 overflow-hidden ${
-              data.imageBorder
-                ? 'rounded-full shadow-[0_1px_3px_1px_rgba(66,_153,_225,0.3)]'
-                : 'rounded-sm'
-            } ${data.rhsImage ? 'order-1 ml-auto' : ''}`}
-          >
-            <Image
-              alt="Testimonial avatar"
-              width={56}
-              height={56}
-              className={`block absolute w-full h-full top-0 left-0 object-fill ${
-                data.link
-                  ? 'group-hover:scale-105 transition-all duration-300 ease-in-out'
-                  : ''
-              }`}
-              src={data.avatar}
-            />
-            <div className="block absolute w-full h-full top-0 left-0 rounded-full"></div>
-          </div>
+    <Elem>
+      <figure
+        className={cn(
+          'relative w-64 cursor-pointer overflow-hidden rounded-xl border p-4',
+          // light styles
+          'border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]',
+          // dark styles
+          'dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]'
         )}
-        <div className="flex flex-col">
-          {data.name && (
-            <h4 className="text-lg lg:text-xl font-tuner text-blue-800 font-medium">
+      >
+        <div className="flex flex-row items-center gap-2">
+          <img
+            className="rounded-full"
+            width="32"
+            height="32"
+            alt=""
+            src={data.avatar}
+          />
+          <div className="flex flex-col">
+            <figcaption className="text-sm font-medium dark:text-white">
               {data.name}
-            </h4>
-          )}
-          {(data.username || data.date) && (
-            <p className="text-base font-medium text-blue-700">
-              {data.username && <>@{data.username}</>}
-              {data.username && data.date && (
-                <span className="mx-1.5 opacity-30">&ndash;</span>
-              )}
-              {data.date && (
-                <span className="opacity-70 text-blue-600">
-                  {formatDate(data.date)}
-                </span>
-              )}
+            </figcaption>
+            <p className="text-xs font-medium dark:text-white/40">
+              {data.username}
             </p>
-          )}
+          </div>
         </div>
-      </div>
+        <blockquote className="mt-2 text-sm">
+          {data.testimonial && (
+            <div className="text-base lg:text-md line-clamp-5">
+              <TinaMarkdown
+                // components={contentComponents}
+                content={data.testimonial}
+              />
+            </div>
+          )}
+        </blockquote>
+      </figure>
     </Elem>
   );
 };
 
 export default function TestimonialsBlock({ data, index }) {
-  const isTouchScreen = useMemo(() => checkTouchScreen(), []);
+  // const isTouchScreen = useMemo(() => checkTouchScreen(), []);
   const [isShowingAll, setIsShowingAll] = useState(false);
   const titleRef = useRef(null);
+  const firstRow = data.testimonials.slice(0, data.testimonials.length / 2);
+  const secondRow = data.testimonials.slice(data.testimonials.length / 2);
 
   return (
     <>
@@ -109,53 +89,25 @@ export default function TestimonialsBlock({ data, index }) {
       >
         {data?.title || 'Loved by Developers'}
       </h1>
-      <section
-        key={index}
-        className={`relative px-8 py-12 lg:py-16 columns-md gap-6`}
-        style={{
-          columnFill: 'balance-all',
-        }}
-      >
-        {data.testimonials &&
-          data.testimonials.map((testimonial, index) => {
-            return (
-              (([0, 1].includes(index) || isShowingAll || !isTouchScreen) && (
-                <Testimonial data={testimonial} key={index} />
-              )) ||
-              ([2].includes(index) && !isShowingAll && (
-                <div
-                  className="relative w-full h-full"
-                  style={{
-                    maskImage: 'linear-gradient(black, transparent 80%)',
-                  }}
-                >
-                  <Testimonial data={testimonial} key={index} />
-                </div>
-              ))
-            );
-          })}
-      </section>
-      {!isShowingAll && isTouchScreen ? (
-        <button
-          className="text-blue-500 text-lg font-tuner cursor-pointer mb-8"
-          onClick={() => setIsShowingAll(true)}
-        >
-          See all
-        </button>
-      ) : null}
-      {isShowingAll && isTouchScreen ? (
-        <button
-          className="text-blue-500 text-lg font-tuner cursor-pointer"
-          onClick={() => {
-            titleRef.current.scrollIntoView({ behavior: 'smooth' });
-            setTimeout(() => {
-              setIsShowingAll(false);
-            }, 700);
-          }}
-        >
-          Hide
-        </button>
-      ) : null}
+
+      <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden ">
+        <Marquee pauseOnHover className="[--duration:20s]">
+          {firstRow.map((review, index) => (
+            <div key={index} className="mr-4">
+              <TestimonialCard key={review.username} {...review} />
+            </div>
+          ))}
+        </Marquee>
+        <Marquee reverse pauseOnHover className="[--duration:20s]">
+          {secondRow.map((review, index) => (
+            <div key={index} className="mr-4">
+              <TestimonialCard key={review.username} {...review} />
+            </div>
+          ))}
+        </Marquee>
+        <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background"></div>
+        <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-white dark:from-background"></div>
+      </div>
     </>
   );
 }
