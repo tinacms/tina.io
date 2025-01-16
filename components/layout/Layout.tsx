89b3861d6ea85096afbfbba927eb4f8a53a48c91
isaaclombardssw <isaaclombard@ssw.com.au>
2025-01-16 11:10:18 +1100

    app router migration tasks – routing

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,32 +1,32 @@
-import { DefaultSeo } from 'next-seo'
-import { useRouter } from 'next/router'
-import React from 'react'
-import { Footer } from './Footer'
-import { Navbar } from './Navbar'
+import { DefaultSeo } from 'next-seo';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+import { Footer } from './Footer';
+import { Navbar } from './Navbar';
 
 interface LayoutProps {
-  children: any | any[]
-  color?: 'white' | 'secondary' | 'seafoam'
-  sticky? : boolean
+  children: any | any[];
+  color?: 'white' | 'secondary' | 'seafoam';
+  sticky?: boolean;
 }
 
 export const Layout = ({ children, color, sticky = true }: LayoutProps) => {
-  const router = useRouter()
+  const pathname = usePathname();
 
   return (
     <>
       <div className="flex flex-col min-h-screen blob-bg font-sans bg-blob-bg bg-[length:100%_100%] bg-top bg-fixed">
         <DefaultSeo
           openGraph={{
-            url: 'https://tina.io' + router.asPath,
+            url: 'https://tina.io' + pathname,
           }}
         />
         <div className="pb-0 lg:pb-20">
-          <Navbar sticky={sticky}/>
+          <Navbar sticky={sticky} />
         </div>
         <div className="flex flex-col flex-1">{children}</div>
         <Footer />
       </div>
     </>
-  )
-}
+  );
+};
