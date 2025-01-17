f7bc197d3cb334f212ab7e61f0f0255d2595a346
isaaclombardssw <isaaclombard@ssw.com.au>
2025-01-17 14:20:58 +1100

    blog pagination prerender

--- app/blog/page/[page_index]/page.tsx
+++ app/blog/page/[page_index]/page.tsx
@@ -1,17 +1,27 @@
 import client from 'tina/__generated__/client';
 
+import { glob } from 'fast-glob';
 import BlogIndexPageClient from './BlogIndexPageClient';
 
 const POSTS_PER_PAGE = 8;
+
+export async function generateStaticParams() {
+  const contentDir = './content/blog/';
+  const files = await glob(`${contentDir}**/*.mdx`);
+  const numFiles = Math.ceil(files.length / POSTS_PER_PAGE);
+  console.log(Array.from(Array(numFiles).keys()).join().split(','));
+  return Array.from(Array(numFiles).keys()).map((page) => ({
+    page_index: (page + 1).toString(),
+  }));
+}
 
 export default async function BlogPaginationPage({
   params,
 }: {
   params: { page_index: string };
 }) {
-  const fg = require('fast-glob');
   const contentDir = './content/blog/';
-  const posts = await fg(`${contentDir}**/*.mdx`);
+  const posts = await glob(`${contentDir}**/*.mdx`);
   const numPages = Math.ceil(posts.length / POSTS_PER_PAGE);
   const pageIndex = parseInt(params.page_index) || 1;
   const startIndex = (pageIndex - 1) * POSTS_PER_PAGE;
