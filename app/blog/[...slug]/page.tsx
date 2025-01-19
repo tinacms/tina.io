1e5b440bad88ef0da3bf57fd4daa5d2e3f22eb76
joshbermanssw <joshberman@ssw.com.au>
2025-01-20 09:00:42 +1100

    add cursor nav

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -3,17 +3,36 @@ import client from 'tina/__generated__/client';
 import BlogPageClient from './BlogPageClient';
 
 export async function generateStaticParams() {
-  const postsResponse = await client.queries.postConnection();
+  let allPosts = [];
+  let hasNextPage = true;
+  let after = null;
 
-  return (
-    postsResponse.data.postConnection.edges?.map((post) => ({
-      slug: [post?.node?._sys.filename], 
-    })) || []
-  );
+  while (hasNextPage) {
+    const postsResponse = await client.queries.postConnection({ after });
+    const edges = postsResponse.data.postConnection.edges || [];
+    const pageInfo = postsResponse.data.postConnection.pageInfo || {};
+    allPosts = allPosts.concat(
+      edges.map((post) => ({
+        slug: [post?.node?._sys.filename],
+      }))
+    );
+    hasNextPage = pageInfo.hasNextPage;
+    after = pageInfo.endCursor;
+  }
+
+  console.log(allPosts);
+  const dataSize = new TextEncoder().encode(JSON.stringify(allPosts)).length;
+  console.log(`Total posts fetched: ${allPosts.length}`);
+  console.log(`Size of allPosts data: ${dataSize} bytes (${(dataSize / 1024).toFixed(2)} KB)`);
+
+  return allPosts;
 }
 
-
-export async function generateMetadata({ params }: { params: { slug: string } }) {
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}) {
   const slug = params.slug;
   const vars = { relativePath: `${slug}.mdx` };
 
