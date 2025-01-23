c5de856627d908c4e2be6af2039e62882ea45fbd
joshbermanssw <joshberman@ssw.com.au>
2025-01-24 09:57:15 +1100

    moved search over to app router

---
+++ app/search/page.tsx
@@ -0,0 +1,56 @@
+import { notFound } from 'next/navigation';
+import client from 'tina/__generated__/client';
+import { formatTableofContentsData } from 'utils/docs/getDocProps';
+import getTableOfContents from 'utils/docs/getTableOfContents';
+import SearchPageClient from './search-client';
+
+async function getSearchPageData() {
+  const slug = 'index';
+  let tableOfContents, formatted;
+
+  try {
+    const results = await client.queries.doc({ relativePath: `${slug}.mdx` });
+    const documentData = results.data.doc;
+    tableOfContents = getTableOfContents(documentData.body.children);
+  } catch (e) {
+    console.error(
+      'Error Fetching Docs Navigation Data: ',
+      e.message
+    );
+    notFound();
+  }
+
+  try {
+    const query = `
+  query {
+      docsTableOfContents(relativePath: "docs-toc.json") {
+        _values
+    }
+  }
+  `;
+    const docTocData = await client.request(
+      {
+        query,
+        variables: { relativePath: 'docs-toc.json' },
+      },
+      {}
+    );
+    formatted = formatTableofContentsData(docTocData, null);
+  } catch (e) {
+    console.error('Error fetching Docs Table of Content Data: ', e.message);
+  }
+
+  return {
+    props: {
+      tableOfContents,
+      formatted,
+    },
+  };
+}
+
+export default async function SearchPage() {
+  const data = await getSearchPageData();
+  return <div>
+    <SearchPageClient {...data} />
+  </div>
+}
