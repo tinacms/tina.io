8b3d8a5276f4c61a91a8b9462a2542d506086573
Tino Liu [SSW] <tinoliu@ssw.com.au>
2025-01-23 19:45:58 +0800

    update timeout time

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  timeout: 120_000,
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
