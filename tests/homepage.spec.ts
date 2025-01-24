28c126de574c3edd95c4eca964f76ab1199b5063
Tino Liu [SSW] <tinoliu@ssw.com.au>
2025-01-24 17:21:06 +0800

    change test to see if pass

--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -3,21 +3,17 @@ import { devices, expect, test } from '@playwright/test';
 test.use(devices['iPhone 12']);
 test('TinaCMS homepage loads and displays successfully', async ({ page }) => {
   await page.goto('/');
-  await expect(page.getByRole('heading', { name: 'Trusted By' })).toBeVisible({
-    timeout: 10000,
-  });
-  await expect(
-    page.getByRole('heading', { name: 'Loved by Developers' })
-  ).toBeVisible({ timeout: 10000 });
+  await expect(page.getByRole('heading', { name: 'Sites, Docs and Llamas. No' })).toBeVisible();
+
+  await expect(page.getByText('TinaCMS is an open-source,')).toBeVisible();
 });
 
 test.use(devices['iPhone 12']);
 test('TinaCMS homepage scrolls without breaking', async ({ page }) => {
   await page.goto('/');
 
-  await expect(page.getByRole('heading', { name: 'Trusted By' })).toBeVisible({
-    timeout: 10000,
-  });
+  await expect(page.getByRole('heading', { name: 'Sites, Docs and Llamas. No' })).toBeVisible();
+
   await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
   await page.waitForTimeout(1000);
 
