8adfe3eec162e712b5acf66ee12b545d98dd50c4
joshbermanssw <joshberman@ssw.com.au>
2025-01-24 13:59:30 +1100

    comment out generateMetaData

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -7,12 +7,12 @@ import { formatTableofContentsData } from 'utils/docs/getDocProps';
 import getTableOfContents from 'utils/docs/getTableOfContents';
 import SearchPageClient from './search-client';
 
-export async function generateMetadata() {
-  return {
-    title: 'Search Page | Tina',
-    description: 'Swiftly Search through the TinaCMS Docs and Blogs',
-  };
-}
+// export async function generateMetadata() {
+//   return {
+//     title: 'Search Page | Tina',
+//     description: 'Swiftly Search through the TinaCMS Docs and Blogs',
+//   };
+// }
 
 async function getSearchPageData() {
   const slug = 'index';
