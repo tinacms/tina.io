dc7aa450b41b4d7ace5a51d8259a5829f5631dda
Isaac Lombard <isaaclombard@ssw.com.au>
2025-02-04 10:59:53 +1100

    re-adding scale

--- components/ui/scrollBasedComponents/scrollCards.tsx
+++ components/ui/scrollBasedComponents/scrollCards.tsx
@@ -124,7 +124,7 @@ export const Card: React.FC<CardProps> = ({
       <motion.div
         style={{
           backgroundColor: 'white',
-          // scale,
+          scale,
           opacity,
           top: `calc(-${5 + i}vh + ${i * 25}px)`,
         }}
