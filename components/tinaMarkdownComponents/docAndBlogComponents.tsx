f12107ae2a1bc02919b9fb1bdf6de63c4320a3e4
isaaclombardssw <isaaclombard@ssw.com.au>
2025-03-12 09:23:30 +1100

    docs content changes - content model

--- components/tinaMarkdownComponents/docAndBlogComponents.tsx
+++ components/tinaMarkdownComponents/docAndBlogComponents.tsx
@@ -206,7 +206,7 @@ export const docAndBlogComponents: Components<{
       style={{ paddingBottom: '56.25%' }}
     >
       <iframe
-        className="absolute top-0 left-0 w-full h-full"
+        className="absolute top-0 left-0 w-full h-full rounded-md"
         src={embedSrc}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
