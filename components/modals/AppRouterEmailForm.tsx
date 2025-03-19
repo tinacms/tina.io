590d9f148e10c1fa3bf69a6001ef6d68b37cdf04
ZenoWang1999 <ZenoWang1999@gmail.com>
2025-03-19 18:04:45 +0800

    Fix the UI issue with email component

--- components/modals/AppRouterEmailForm.tsx
+++ components/modals/AppRouterEmailForm.tsx
@@ -1,12 +1,12 @@
-'use client'
-import React, { useState } from 'react';
-import { addToMailchimp } from '../../utils';
-import { Input, Button } from '../ui';
+'use client';
 import Image from 'next/image';
+import React, { useState } from 'react';
+import { ImCross } from 'react-icons/im';
+import { IoIosWarning } from 'react-icons/io';
+import { TiTick } from 'react-icons/ti';
 import BettyWithLlama from '../../public/img/BettyWithLlama.png';
-import { ImCross } from "react-icons/im";
-import { IoIosWarning } from "react-icons/io";
-import { TiTick } from "react-icons/ti";
+import { addToMailchimp } from '../../utils';
+import { Button, Input } from '../ui';
 
 interface EmailFormProps {
   isFooter: boolean;
@@ -20,7 +20,6 @@ export const EmailForm = (props: EmailFormProps) => {
   const [isProcessing, setIsProcessing] = useState(false);
   const [message, setMessage] = useState({ text: '', type: '' });
 
-  
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setIsProcessing(true);
@@ -28,15 +27,24 @@ export const EmailForm = (props: EmailFormProps) => {
     try {
       const result = await addToMailchimp(email, firstName, lastName);
       if (result.result === 'success') {
-        setMessage({ text: "You've been added to the llama list.", type: 'success' });
+        setMessage({
+          text: "You've been added to the llama list.",
+          type: 'success',
+        });
       } else if (result.message.includes('400')) {
         setMessage({ text: "You're already in our herd!", type: 'warning' });
       } else {
-        setMessage({ text: 'There was an error. Please try again.', type: 'error' });
+        setMessage({
+          text: 'There was an error. Please try again.',
+          type: 'error',
+        });
       }
     } catch (error) {
       console.error('Error submitting email:', error);
-      setMessage({ text: 'There was an error. Please try again.', type: 'error' });
+      setMessage({
+        text: 'There was an error. Please try again.',
+        type: 'error',
+      });
     }
     setIsProcessing(false);
   };
@@ -51,28 +59,42 @@ export const EmailForm = (props: EmailFormProps) => {
   return (
     <div className="grid lg:grid-cols-2 grid-cols-1 gap-1 bg-white max-w-7xl mx-auto">
       <div className="relative lg:h-full md:h-80 h-52">
-        <Image 
-          src={BettyWithLlama} 
-          alt="Betty with a llama" 
+        <Image
+          src={BettyWithLlama}
+          alt="Betty with a llama"
           layout="fill"
           objectFit="cover"
-          className="w-full h-full" 
+          className="w-full h-full"
         />
       </div>
-      <div className={`${message.text ? 'lg:pt-7 lg:pb-3' : 'lg:pt-12 lg:pb-7'}`}>
+      <div
+        className={`${message.text ? 'lg:pt-7 lg:pb-2' : 'lg:pt-12 lg:pb-4'}`}
+      >
         <form
           id="newsletter-signup"
           onSubmit={handleSubmit}
-          className={`flex flex-col justify-center pt-8 pb-9 p-10 md:p-12 lg:p-20 ${props.isFooter ? 'w-auto' : ''}`}
+          className={`flex flex-col justify-center pt-8 pb-6 p-10 md:p-12 lg:px-20 lg:pt-16 lg:pb-10 ${
+            props.isFooter ? 'w-auto' : ''
+          }`}
         >
           <h1 className="inline-block lg:text-left md:text-left sm:text-center m-0 md:text-4xl font-tuner lg:text-3xl text-2xl lg:leading-tight bg-gradient-to-br from-orange-400 via-orange-500 to-orange-600 bg-clip-text text-transparent">
             Join the Herd! 🦙
           </h1>
           <p className="text-left w-full mt-2 mb-4">
-            Become part of our coding community and stay updated with the latest tips and news. Learn more about the community and get llama-zing content delivered to your inbox!
+            Become part of our coding community and stay updated with the latest
+            tips and news. Learn more about the community and get llama-zing
+            content delivered to your inbox!
           </p>
           {message.text && (
-            <p className={`font-tuner text-sm mb-4 flex items-center gap-2 ${message.type === 'success' ? 'text-green-500' : message.type === 'warning' ? 'text-orange-500' : 'text-red-500'}`}>
+            <p
+              className={`font-tuner text-sm mb-4 flex items-center gap-2 ${
+                message.type === 'success'
+                  ? 'text-green-500'
+                  : message.type === 'warning'
+                  ? 'text-orange-500'
+                  : 'text-red-500'
+              }`}
+            >
               {message.type === 'success' && <TiTick />}
               {message.type === 'warning' && <IoIosWarning />}
               {message.type === 'error' && <ImCross />}
@@ -110,12 +132,12 @@ export const EmailForm = (props: EmailFormProps) => {
               className="w-full"
             />
           </div>
-          <div className="w-full flex justify-end">
+          <div className="w-full flex justify-end mt-6">
             <Button
               type="submit"
               color="orange"
               disabled={isProcessing}
-              className="mt-4"
+              className="px-6 py-2.5"
             >
               {isProcessing ? 'Processing...' : 'Subscribe'}
             </Button>
