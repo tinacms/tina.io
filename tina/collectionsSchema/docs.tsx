962121850fa1034970ca9b96658ff6cd47698815
Isaac Lombard [SSW] <152236421+isaaclombardssw@users.noreply.github.com>
2025-03-28 10:00:44 +1100

    Content audit proposal (#2953)
    This is a oversize PR that is the first of a series to change the Docs
    structure.
    
    Old:
    <img width="340" alt="SCR-20250328-ixso"
    src="https://github.com/user-attachments/assets/634025c5-bc3d-4329-a9dd-6f0cdc33286c"
    />
    
    New:
    <img width="362" alt="SCR-20250328-ixvy"
    src="https://github.com/user-attachments/assets/c44ae764-29e1-4650-a8e9-e19a6e5b99f4"
    />
    
    
    As I was working I've made little fixes based on suggestions
    including...
    - giving the text room to breathe (modelled after the Prose tailwind
    class as per suggestion from @JackDevAU).
    - adding some internal right padding to code blocks so the copy button
    isn't overlapping in some cases.
    - adding in Mermaid graph capability
    - adding a description page to the start of the tutorials
    - scrolling showcase embed improvements
    
    ### General Contributing:
    
    - [ ] Have you followed the guidelines in our [Contributing
    document](https://github.com/tinacms/tina.io/blob/master/CONTRIBUTING.md)?
    - [ ] Have you checked to ensure there aren't other open [Pull
    Requests](../../../pulls) for the same update/change?
    
    <!-- For non content related updates, or fixing things like typos, you
    can erase the following section -->
    
    ### All New Content Submissions: (To be confirmed by reviewer)
    
    - [ ] Title is short & specific
    - [ ] Headers are logically ordered & consistent
    - [ ] Purpose of document is explained in the first paragraph
    - [ ] Procedures are tested and work
    - [ ] Any technical concepts are explained or linked to
    - [ ] Document follows structure from templates
    - [ ] All links work
    - [ ] The spelling and grammar checker has been run
    - [ ] Graphics and images are clear and useful
    - [ ] Any prerequisites and next steps are defined.
    
    ---------
    
    Co-authored-by: tina-cloud-app[bot] <58178390+tina-cloud-app[bot]@users.noreply.github.com>
    Co-authored-by: Caleb Williams <CalebWilliams@ssw.com.au>

--- tina/collectionsSchema/docs.tsx
+++ tina/collectionsSchema/docs.tsx
@@ -1,4 +1,4 @@
-import { Template } from 'tinacms';
+import { Template, wrapFieldsWithMeta } from 'tinacms';
 import { CardGridSchema } from '../../components/blocks/CardGrid.schema';
 import { RecipeBlock } from '../../components/blocks/Recipe.template';
 import ScrollBasedShowcase from '../../components/tinaMarkdownComponents/templateComponents/scrollBasedShowcase.schema';
@@ -78,7 +78,23 @@ export const docsCollection = {
               name: 'property',
               label: 'Property',
               list: true,
+              ui: {
+                itemProps: (item) => {
+                  return {
+                    label: item.groupName
+                      ? `📂 ${item.groupName} | ${item.name}`
+                      : item.name,
+                  };
+                },
+              },
               fields: [
+                {
+                  type: 'string',
+                  name: 'groupName',
+                  label: 'Group Name',
+                  description:
+                    'Adjacent properties with the same group name will be grouped together',
+                },
                 {
                   type: 'string',
                   name: 'name',
@@ -103,6 +119,11 @@ export const docsCollection = {
                   type: 'boolean',
                   name: 'required',
                   label: 'Required',
+                },
+                {
+                  type: 'boolean',
+                  name: 'experimental',
+                  label: 'Experimental',
                 },
               ],
             },
@@ -156,6 +177,18 @@ export const docsCollection = {
               name: 'preselectResponse',
               label: 'Select Response by Default',
               description: 'Select the response tab by default',
+            },
+            {
+              type: 'string',
+              name: 'customQueryName',
+              label: 'Custom Query Name',
+              description: "Replaces 'Query' in the tab name",
+            },
+            {
+              type: 'string',
+              name: 'customResponseName',
+              label: 'Custom Response Name',
+              description: "Replaces 'Response' in the tab name",
             },
           ],
         },
@@ -216,12 +249,17 @@ export const docsCollection = {
           label: 'Image and Text',
           fields: [
             {
+              name: 'heading',
+              label: 'Heading',
+              type: 'string',
+              description:
+                'The heading text that will be displayed in the collapsed state',
+            },
+            {
               name: 'docText',
               label: 'docText',
               isBody: true,
               type: 'rich-text',
-              description:
-                'DO NOT USE THIS TEMPLATE WHILST YOU SEE THIS MESSAGE //TODO: #1967',
             },
             {
               name: 'image',
