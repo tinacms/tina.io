685601ebbdf6006aae3e3cce5951528c08aa9b3d
joshbermanssw <joshberman@ssw.com.au>
2025-04-11 15:11:32 +1000

    rm log and btns

--- utils/github-star-helper.tsx
+++ utils/github-star-helper.tsx
@@ -6,13 +6,13 @@ export async function getGitHubStarCount(
   repo: string
 ): Promise<number> {
   try {
-    console.log('Fetching GitHub stars for:', owner, repo);
+    
     const response = await fetch(`/api/githubstar?owner=${owner}&repo=${repo}`);
     if (!response.ok) {
       throw new Error(`API error: ${response.status}`);
     }
     const data = await response.json();
-    console.log('Response data:', data);
+    
     return data.stars;
   } catch (error) {
     console.error('Error fetching GitHub stars:', error);
