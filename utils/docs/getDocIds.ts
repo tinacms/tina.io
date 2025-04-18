18f7f5d0bf13357c9136e7e7dd27fb109ee5a9b3
Zeno Wang [SSW] <zw3743@nyu.edu>
2025-04-18 10:14:07 +0800

    Fix the Highlight Error on Chinese Docs Page (#3299)
    ### General Contributing:
    
    - [ ] Have you followed the guidelines in our [Contributing
    document](https://github.com/tinacms/tina.io/blob/master/CONTRIBUTING.md)?
    - [ ] Have you checked to ensure there aren't other open [Pull
    Requests](../../../pulls) for the same update/change?
    
    <!-- For non content related updates, or fixing things like typos, you
    can erase the following section -->
    
    ### All New Content Submissions: (To be confirmed by reviewer)
    
    - [ ] Title is short & specific
    - [ ] Headers are logically ordered & consistent
    - [ ] Purpose of document is explained in the first paragraph
    - [ ] Procedures are tested and work
    - [ ] Any technical concepts are explained or linked to
    - [ ] Document follows structure from templates
    - [ ] All links work
    - [ ] The spelling and grammar checker has been run
    - [ ] Graphics and images are clear and useful
    - [ ] Any prerequisites and next steps are defined.

--- utils/docs/getDocIds.ts
+++ utils/docs/getDocIds.ts
@@ -1,13 +1,25 @@
-//Gets the Document Ids for blog post formatting headers 
+//Gets the Document Ids for blog post formatting headers
 export const getDocId = (label) => {
-    if (!label) {
-      return;
+  if (!label) {
+    return;
+  }
+
+  const cleanId = label
+    .toLowerCase()
+    .replace(/^\s*"|"\s*$/g, '-')
+    .replace(/^-*|-*$/g, '')
+    .replace(/\s+/g, '-')
+    .replace(/[^a-z0-9\-]/g, '');
+
+  if (cleanId === '' || cleanId === '-') {
+    let hash = 0;
+    for (let i = 0; i < label.length; i++) {
+      const char = label.charCodeAt(i);
+      hash = (hash << 5) - hash + char;
+      hash = hash & hash;
     }
-    return label
-      .toLowerCase()
-      .replace(/^\s*"|"\s*$/g, '-')
-      .replace(/^-*|-*$/g, '')  
-      .replace(/\s+/g, '-')
-      .replace(/[^a-z0-9\-]/g, '');
-  };
-  
+    return 'heading-' + Math.abs(hash).toString(16).substring(0, 8);
+  }
+
+  return cleanId;
+};
