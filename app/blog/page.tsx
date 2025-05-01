badf03e655fe22a40d00c4d4f23d35ef1846e4ff
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2025-05-01 10:22:34 +1000

    fixing seo for /blogs/*

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,21 +1,17 @@
+import settings from '@/content/settings/config.json';
+import { getSeo } from '@/utils/metadata/getSeo';
 import BlogPaginationPage from './page/[page_index]/page';
 
-export async function generateMetadata(){
-
-  const title = 'TinaCMS Blog';
-  const description = 'Stay updated with the TinaCMS blog. Get tips, guides and the latest news on content management and development';
-  return{
-    title: title,
-    description: description,
-    openGraph: {
-      title: title,
-      description: description,
-      url: 'https://tinacms.org/blog',
-    }
-  }
+export async function generateMetadata() {
+  return getSeo({
+    title: 'TinaCMS Blog',
+    description:
+      'Stay updated with the TinaCMS blog. Get tips, guides and the latest news on content management and development',
+    canonicalUrl: `${settings.siteUrl}/blog`,
+  });
 }
 
 export default async function BlogPage() {
-  const params = { page_index: '1' }; 
+  const params = { page_index: '1' };
   return await BlogPaginationPage({ params });
 }
