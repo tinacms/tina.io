dc837c3956c8293a38a538aea53c5d16ea3f0515
joshbermanssw <joshberman@ssw.com.au>
2025-05-02 09:45:48 +1000

    Fix h1 colour

--- components/blocks/Pricing/Pricing.tsx
+++ components/blocks/Pricing/Pricing.tsx
@@ -278,7 +278,7 @@ export function PricingBlock({ data }) {
 
   return (
     <div className="max-w-7xl w-full mx-8">
-      <h1 className="text-center justify-center font-tuner text-4xl lg:leading-tight bg-gradient-to-br from-orange-400 via-orange-600 to-orange-700 group-hover:from-orange-300 group-hover:via-orange-500 group-hover:to-orange-700 bg-clip-text text-transparent">
+      <h1 className="text-center justify-center font-tuner text-4xl lg:leading-tight bg-gradient-to-br from-orange-400 via-orange-500 to-orange-600 group-hover:from-orange-300 group-hover:via-orange-500 group-hover:to-orange-700 bg-clip-text text-transparent">
         {data.headline}
       </h1>
 
