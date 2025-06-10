a5a3e6747950109ab3cc9d576446c6b62f70648b
Tiago Araújo [SSW] <tiagov8@gmail.com>
2025-06-09 18:02:18 -0700

    Update 404 text + improved image look & feel (#3492)

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ import NotFoundClient from './not-found-client';
 
 export const metadata = {
   title: '404 - Page Not Found | TinaCMS',
-  description: "Sorry, we couldn't find what you were looking for.",
+  description: "We couldn't find your baby llama, check pen 404.",
 };
 
 export default function NotFound() {
