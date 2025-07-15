a4501583b565d9fc05f885b57047e05e861e8bb2
Tiago Araújo [SSW] <tiagov8@gmail.com>
2025-07-14 23:03:17 -0700

    Update CardGrid.tsx (#3730)
    cc @adamcogan 
    
    Fixes https://github.com/tinacms/tina.io/issues/3728
    
    Updated cards to have gaps, hover bg in white and hover shadow applied
    
    from
    
    <img width="828" height="480" alt="Screenshot 2025-07-14 at 5 09 27 PM"
    src="https://github.com/user-attachments/assets/8fdb458d-4414-4f85-8a93-cf709aca6f09"
    />
    
    to
    
    <img width="823" height="495" alt="Screenshot 2025-07-14 at 5 10 57 PM"
    src="https://github.com/user-attachments/assets/3b9fdcf5-ae2e-4822-b464-5d75e0969d29"
    />
    
    Co-authored-by: Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>

--- components/blocks/CardGrid.tsx
+++ components/blocks/CardGrid.tsx
@@ -2,12 +2,12 @@ import { Actions } from '../blocks/ActionButton/ActionsButton';
 
 export const CardGrid = ({ props }) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 my-8 border border-seafoam-300 rounded-lg shadow-lg">
+    <div className="grid grid-cols-1 md:grid-cols-2 my-8 gap-4">
       {props.cards?.map((card) => (
         <a
           key={card.id}
           href={card.link}
-          className="flex flex-col justify-between p-6 bg-transparent hover:bg-seafoam-100 transition-all ease-out duration-150 rounded-lg group"
+          className="flex flex-col justify-between p-6 border rounded-lg group bg-transparent hover:shadow-lg hover:bg-white hover:scale-[1.01] transition-all duration-150 ease-out transition-all ease-out duration-150 transition-all ease-out duration-150"
         >
           <h2 className="bg-linear-to-br from-orange-400 via-orange-500 to-orange-600 group-hover:from-blue-700 group-hover:via-blue-600  group-hover:to-blue-500 bg-clip-text text-transparent font-bold text-2xl leading-snug font-ibm-plex transition-all ease-out duration-150">
             {card.title}
