533e8ebbcb8909d6a083882ba054104ded6d5efd
Tino Liu [SSW] <tinoliu@ssw.com.au>
2025-08-20 11:39:01 +0800

    Add a bit more pt

--- components/blocks/Hero/Hero.tsx
+++ components/blocks/Hero/Hero.tsx
@@ -9,7 +9,7 @@ export function HeroBlock({ data, index }) {
     <section
       key={index}
       className={`relative overflow-visible z-10 text-center ${
-        data.margin ? data.margin : 'px-8 pt-8 lg:pt-28'
+        data.margin ? data.margin : 'px-8 pt-8 lg:pt-32'
       }`}
     >
       <Container width="narrow" center>
