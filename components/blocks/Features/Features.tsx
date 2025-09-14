3dae3974255b300ebe146791d96ee40e6073e982
Josh Berman [SSW] <JoshBerman@ssw.com.au>
2025-09-15 09:22:19 +1000

    cur CTA

--- components/blocks/Features/Features.tsx
+++ components/blocks/Features/Features.tsx
@@ -24,7 +24,7 @@ export function FeatureBlock({ data }) {
       >
         {data.headingOne && data.headline && (
           <h1
-            className="font-ibm-plex inline-block text-4xl md:text-5xl py-4 lg:text-6xl lg:leading-tight bg-linear-to-br from-orange-400 via-orange-500 to-orange-600 bg-clip-text text-transparent text-balance text-center lg:text-left"
+            className="font-ibm-plex inline-block text-4xl md:text-5xl py-4 lg:text-6xl lg:leading-tight text-balance text-center lg:text-left"
             data-tina-field={tinaField(data, 'headline')}
           >
             {data.headline}
@@ -32,7 +32,7 @@ export function FeatureBlock({ data }) {
         )}
         {data.headline && !data.headingOne && (
           <h2
-            className={`${BLOCK_HEADINGS} font-ibm-plex inline-block  py-4  lg:leading-tight bg-linear-to-br from-orange-400 via-orange-500 to-orange-600 bg-clip-text text-transparent text-balance text-center lg:text-left`}
+            className={`${BLOCK_HEADINGS} font-ibm-plex inline-block  py-4  lg:leading-tight text-balance text-center lg:text-left`}
             data-tina-field={tinaField(data, 'headline')}
           >
             {data.headline}
@@ -42,7 +42,7 @@ export function FeatureBlock({ data }) {
           <hr className="my-0! w-full block border-none bg-[url('/svg/hr.svg')] bg-[length:auto_100%] bg-no-repeat h-[7px]" />
         </div>
         <p
-          className="text-lg lg:text-xl lg:leading-normal bg-linear-to-br from-blue-700 via-blue-900 to-blue-1000 bg-clip-text text-transparent max-w-60ch text-balance text-center lg:text-left py-4"
+          className="text-lg lg:text-xl lg:leading-normal text-neutral-text-secondary max-w-60ch text-balance text-center lg:text-left py-4"
           data-tina-field={tinaField(data, 'text')}
         >
           {data.text}
