b1b75055434fd7b66a24098489ad293c2e3ca9d6
Josh Berman [SSW] <JoshBerman@ssw.com.au>
2025-09-15 15:01:10 +1000

    fix ghostBlue Outline

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -47,7 +47,7 @@ const colorClasses = {
   ghost: 'text-orange-500 hover:text-orange-400',
   orangeWithBorder:
     'text-orange-500 hover:text-orange-400 border border-orange-500 bg-white',
-  ghostBlue: 'text-blue-800 hover:text-blue-600',
+  ghostBlue: 'text-blue-800 hover:text-blue-600 border-transparent',
   blueOutline:
     'text-blue-600 hover:text-blue-600 border-2 border-blue-600 cursor-pointer',
   ghostOutline: 'text-gray-900 hover:text-black border-black bg-transparent',
