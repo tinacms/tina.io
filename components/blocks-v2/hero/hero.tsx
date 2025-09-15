e93d8c29ed4c46df15d5ffff2caadf1703b9b6db
Josh Berman [SSW] <JoshBerman@ssw.com.au>
2025-09-15 16:35:51 +1000

    Hero section responsiveness

--- components/blocks-v2/hero/hero.tsx
+++ components/blocks-v2/hero/hero.tsx
@@ -9,20 +9,20 @@ export default function HeroV2(data: { data: PageBlocksHeroV2 }) {
   const { title, subtext, buttons, image } = data.data || {};
 
   return (
-    <Container size="medium" className="min-h-[50vh] grid grid-cols-2 gap-4">
-      <div className="flex  flex-col gap-8  border-red-500 py-10">
+    <Container size="medium" className="min-h-[50vh] grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div className="flex items-center flex-col gap-8  border-red-500 py-10">
         {title && (
-          <h2 className={`${H1_HEADINGS} font-ibm-plex`}>
+          <h2 className={`${H1_HEADINGS} max-w-md md:max-w-none font-ibm-plex`}>
             {curlyBracketFormatter(title)}
           </h2>
         )}
         {subtext && (
-          <p className="text-neutral-text-secondary max-w-[62ch] font-light leading-relaxed text-lg">
+          <p className="text-neutral-text-secondary md:max-w-[62ch] font-light leading-relaxed text-lg max-w-md">
             {subtext}
           </p>
         )}
         {buttons && buttons.length > 0 && (
-          <div className="flex flex-row flex-wrap gap-2 max-w-[62ch]">
+          <div className="flex justify-center md:justify-start flex-row flex-wrap gap-2 max-w-[62ch]">
             {buttons.map((button, index) => (
               <RenderButton
                 key={`button-${button?.label || index}`}
@@ -35,12 +35,13 @@ export default function HeroV2(data: { data: PageBlocksHeroV2 }) {
         )}
       </div>
       {image && (
-        <div className="relative">
+        <div className="relative flex items-center justify-center mx-auto max-w-md md:max-w-none">
           <Image
             src={image}
             alt={title || 'Hero image'}
             width={500}
             height={500}
+            className="w-[250px] h-[300px] md:w-[500px] md:h-[500px]"
           />
         </div>
       )}
