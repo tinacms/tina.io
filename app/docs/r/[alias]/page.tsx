04dde45f65d59f7778f7869509545e774c3ec6c3
Josh Berman [SSW] <137844305+joshbermanssw@users.noreply.github.com>
2025-09-19 15:24:37 +1000

    Docs Aliases (#3906)

---
+++ app/docs/r/[alias]/page.tsx
@@ -0,0 +1,29 @@
+import { notFound, redirect } from 'next/navigation';
+import client from 'tina/__generated__/client';
+import type { DocConnectionQuery } from 'tina/__generated__/types';
+
+export default async function AliasRedirect({
+  params,
+}: {
+  params: { alias: string };
+}) {
+  console.log('The Params Are', params);
+
+  let finalDocument: DocConnectionQuery['docConnection']['edges'][number]['node'];
+  try {
+    const res = await client.queries.docConnection({
+      filter: { alias: { eq: params.alias } },
+    });
+
+    const edges = res.data.docConnection.edges;
+    if (edges.length === 0) {
+      return notFound();
+    }
+    finalDocument = edges[0].node;
+  } catch (error) {
+    console.error('Error fetching alias', error);
+    return notFound();
+  }
+  const slug = finalDocument._sys.relativePath.slice(0, -4);
+  return redirect(`/docs/${slug}`);
+}
