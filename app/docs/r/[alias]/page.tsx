d229f72627c1b964780151e26dbb5101bf2391b2
Josh Berman [SSW] <JoshBerman@ssw.com.au>
2025-09-22 09:27:14 +1000

    lint

--- app/docs/r/[alias]/page.tsx
+++ app/docs/r/[alias]/page.tsx
@@ -1,16 +1,15 @@
+import { glob } from 'fast-glob';
+import { readFileSync } from 'fs';
+import matter from 'gray-matter';
 import { notFound, redirect } from 'next/navigation';
+import path from 'path';
 import client from 'tina/__generated__/client';
 import type { DocConnectionQuery } from 'tina/__generated__/types';
-import { glob } from 'fast-glob';
-import matter from 'gray-matter';
-import { readFileSync } from 'fs';
-import path from 'path';
 
 export const dynamicParams = false;
 
 export async function generateStaticParams() {
   try {
-   
     const contentDir = './content/docs/';
     const files = await glob(`${contentDir}**/*.mdx`);
     const aliasParams = [];
@@ -20,7 +19,7 @@ export async function generateStaticParams() {
         const absolutePath = path.resolve(file);
         const fileContent = readFileSync(absolutePath, 'utf8');
         const { data } = matter(fileContent);
-        
+
         if (data.alias && typeof data.alias === 'string') {
           aliasParams.push({ alias: data.alias });
         }
@@ -60,9 +59,9 @@ export default async function AliasRedirect({
     if (edges.length === 0) {
       return notFound();
     }
-    
+
     finalDocument = edges[0].node;
-    
+
     if (!finalDocument?._sys?.relativePath) {
       return notFound();
     }
@@ -70,12 +69,12 @@ export default async function AliasRedirect({
     console.error('Error fetching alias', error);
     return notFound();
   }
-  
+
   const slug = finalDocument._sys.relativePath.slice(0, -4);
-  
+
   if (!slug) {
     return notFound();
   }
-  
+
   return redirect(`/docs/${slug}`);
 }
