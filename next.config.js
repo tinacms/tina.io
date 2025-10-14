8a2ab6322a686c97c87b30d1fda88582dcd38b01
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2025-10-15 09:37:41 +1100

    uncommenting the rewrites for landing page

--- next.config.js
+++ next.config.js
@@ -7,8 +7,8 @@ require('dotenv').config();
 
 const isStatic = process.env.EXPORT_MODE === 'static';
 
-const TINA_DOCS_URL = 'https://tina-docs-red.vercel.app/tinadocs';
-const _TINA_DOCS_LANDING_URL = 'https://tina-docs-landing.vercel.app/tinadocs';
+const TINA_DOCS_URL = 'https://tina-docs-red.vercel.app';
+const TINA_DOCS_LANDING_URL = 'https://tina-docs-landing.vercel.app';
 
 /**
  * @type {import('next').NextConfig}
@@ -62,32 +62,36 @@ const config = {
       // Docs
       {
         source: '/tinadocs/docs',
-        destination: `${TINA_DOCS_URL}/docs`,
+        destination: `${TINA_DOCS_URL}/tinadocs/docs`,
       },
       {
         source: '/tinadocs/docs/:path*',
-        destination: `${TINA_DOCS_URL}/docs/:path*`,
+        destination: `${TINA_DOCS_URL}/tinadocs/docs/:path*`,
       },
-      // {
-      //   source: '/tinadocs/docsassets/:path*',
-      //   destination: `${TINA_DOCS_URL}/docsassets/:path*`,
-      // },
-      // // Docs - Search functionality - Pagefind
-      // {
-      //   source: '/tinadocs/_next/static/pagefind/:path*',
-      //   destination: `${TINA_DOCS_URL}/_next/static/pagefind/:path*`,
-      // },
+      {
+        source: '/tinadocs/docsassets/:path*',
+        destination: `${TINA_DOCS_URL}/tinadocs/docsassets/:path*`,
+      },
+      // Docs - Search functionality - Pagefind
+      {
+        source: '/tinadocs/_next/static/pagefind/:path*',
+        destination: `${TINA_DOCS_URL}/tinadocs/_next/static/pagefind/:path*`,
+      },
 
       // Landing Page - Specific patterns first
-      // {
-      //   source: '/tinadocs/landing/:path*',
-      //   destination: `${TINA_DOCS_LANDING_URL}/landing/:path*`,
-      // },
-      // // Catch-all for remaining tinadocs paths
-      // {
-      //   source: '/tinadocs/:path*',
-      //   destination: `${TINA_DOCS_LANDING_URL}/:path*`,
-      // },
+      {
+        source: '/tinadocs',
+        destination: `${TINA_DOCS_LANDING_URL}/tinadocs`,
+      },
+      {
+        source: '/tinadocs/landing/:path*',
+        destination: `${TINA_DOCS_LANDING_URL}/tinadocs/landing/:path*`,
+      },
+      // Catch-all for remaining tinadocs paths
+      {
+        source: '/tinadocs/:path*',
+        destination: `${TINA_DOCS_LANDING_URL}/tinadocs/:path*`,
+      },
 
       // Admin passthrough (yours)
       { source: '/admin', destination: '/admin/index.html' },
