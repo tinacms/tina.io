63a119e33a6f90f97eb83a50ec7cf7493d15f0ca
Tino Liu [SSW] <tinoliu@ssw.com.au>
2025-10-28 17:42:26 +0800

    test sitemap error fix (#4041)
    ### General Contributing:
    
    - [ ] Have you followed the guidelines in our [Contributing
    document](https://github.com/tinacms/tina.io/blob/master/CONTRIBUTING.md)?
    - [ ] Have you checked to ensure there aren't other open [Pull
    Requests](../../../pulls) for the same update/change?
    
    <!-- For non content related updates, or fixing things like typos, you
    can erase the following section -->
    
    ### All New Content Submissions: (To be confirmed by reviewer)
    
    - [ ] Title is short & specific
    - [ ] Headers are logically ordered & consistent
    - [ ] Purpose of document is explained in the first paragraph
    - [ ] Procedures are tested and work
    - [ ] Any technical concepts are explained or linked to
    - [ ] Document follows structure from templates
    - [ ] All links work
    - [ ] The spelling and grammar checker has been run
    - [ ] Graphics and images are clear and useful
    - [ ] Any prerequisites and next steps are defined.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -9,10 +9,7 @@ module.exports = {
       },
       { userAgent: '*', allow: '/' },
     ],
-    additionalSitemaps: [
-      'https://tina.io/tinadocs/sitemap.xml',
-      'https://tina.io/tinadocs/doc/sitemap.xml',
-    ],
+    additionalSitemaps: ['https://tina.io/tinadocs/sitemap.xml'],
   },
   exclude: ['/api/*', '/github/*', '/rss.xml', '/blog/page/*'],
   sitemapSize: 7000,
