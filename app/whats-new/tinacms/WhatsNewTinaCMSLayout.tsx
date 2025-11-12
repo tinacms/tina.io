115b64e5d6bd34b1b1b8cf72cdf1cdb4e9464ae6
Josh Berman [SSW] <JoshBerman@ssw.com.au>
2025-11-13 08:44:32 +1100

    start refactor

--- app/whats-new/tinacms/WhatsNewTinaCMSLayout.tsx
+++ app/whats-new/tinacms/WhatsNewTinaCMSLayout.tsx
@@ -4,33 +4,49 @@ import { FaGithub, FaNewspaper } from 'react-icons/fa';
 import { TinaMarkdown } from 'tinacms/dist/rich-text';
 import { H1_HEADINGS_SIZE } from '@/component/styles/typography';
 
+type WhatsNewCardProps = {
+  item: {
+    versionNumber: string;
+    dateReleased: string;
+    body: string;
+    key: string;
+  };
+};
+
+export const WhatsNewCard = ({ item }: WhatsNewCardProps) => {
+  return (
+    <div
+      key={item.key}
+      className="mb-6 p-10 shadow-xl rounded-lg transform transition-transform duration-300 hover:scale-105 bg-linear-to-br from-white/25 via-white/50 to-white/75"
+    >
+      <div className="flex justify-between items-baseline">
+        <h2 className="text-2xl bg-linear-to-br from-blue-700 to-blue-1000 bg-clip-text text-transparent font-semibold">
+          Version {item.versionNumber}
+        </h2>
+        <p className="text-sm text-gray-500">
+          Released on {new Date(item.dateReleased).toLocaleDateString()}
+        </p>
+      </div>
+      {item.body && (
+        <TinaMarkdown content={item.body} components={whatsNewMDComponents} />
+      )}
+    </div>
+  );
+};
+
 const WhatsNewTinaCMSPageLayout = ({ data }) => {
   const items = data.WhatsNewTinaCMSConnection.edges.map((edge) => edge.node);
 
   return (
-    <div className="p-6 py-12 lg:py-16 last:pb-20 lg:last:pb-32 max-w-prose md:mx-auto">
+    <div className="p-6 py-12 lg:py-16 last:pb-20 lg:last:pb-32 max-w-5xl md:mx-auto">
       <h1
         className={`${H1_HEADINGS_SIZE} text-center justify-center font-ibm-plex lg:leading-tight`}
       >
-        What's new with TinaCMS
+        What's new with <span className="text-orange-500">TinaCMS</span>
       </h1>
       <div className="mt-8">
         {items.map((item) => (
-          <div
-            key={item.id}
-            className="mb-6 p-10 shadow-xl rounded-lg transform transition-transform duration-300 hover:scale-105 bg-linear-to-br from-white/25 via-white/50 to-white/75"
-          >
-            <h2 className="text-2xl bg-linear-to-br from-blue-700 to-blue-1000 bg-clip-text text-transparent font-semibold">
-              Version {item.versionNumber}
-            </h2>
-            <p className="text-sm text-gray-500">
-              Released on {new Date(item.dateReleased).toLocaleDateString()}
-            </p>
-            <TinaMarkdown
-              content={item.body}
-              components={whatsNewMDComponents}
-            />
-          </div>
+          <WhatsNewCard key={item.id} item={item} />
         ))}
       </div>
       <div className="font-ibm-plex text-lg text-center text-blue-700">
