d500eacc26d7d6a48c6def93855b9715767396d7
Josh Berman [SSW] <JoshBerman@ssw.com.au>
2025-11-14 14:56:43 +1100

    UI

--- app/whats-new/tinacloud/WhatsNewTinaCloudPageLayout.tsx
+++ app/whats-new/tinacloud/WhatsNewTinaCloudPageLayout.tsx
@@ -7,7 +7,7 @@ export const WhatsNewTinaCloudPageLayout = ({ data }) => {
   const items = data.WhatsNewTinaCloudConnection.edges.map((edge) => edge.node);
 
   return (
-    <div className="p-6 py-12 lg:py-16 last:pb-20 lg:last:pb-32 max-w-5xl md:mx-auto">
+    <div className="p-6 py-12 lg:py-16 last:pb-20 lg:last:pb-32 w-5xl md:mx-auto">
       <h1
         className={`${H1_HEADINGS_SIZE} text-center justify-center font-ibm-plex lg:leading-tight`}
       >
