a1eb015aee19f6608fd0d7c08bb87ec28a4b39aa
Josh Berman [SSW] <137844305+joshbermanssw@users.noreply.github.com>
2025-12-08 08:27:01 +1100

    add need help popover (#4183)

--- components/blocks/CodeButton/CodeButton.tsx
+++ components/blocks/CodeButton/CodeButton.tsx
@@ -2,7 +2,8 @@ import { useState } from 'react';
 import { BiCopy } from 'react-icons/bi';
 import 'react-responsive-modal/styles.css';
 import { CheckIcon } from '@heroicons/react/24/outline';
-import type { Components } from 'tinacms/dist/rich-text';
+import { IoMdClose } from 'react-icons/io';
+import { type Components, TinaMarkdown } from 'tinacms/dist/rich-text';
 import { sanitizeLabel } from 'utils/sanitizeLabel';
 import { copyToClipboard } from '../../layout/MarkdownContent';
 
@@ -24,46 +25,70 @@ export const CodeButton = ({
   label,
   id,
   className = '',
+  helpText,
   ...props
 }) => {
   const [_copied, setCopied] = useState(false);
+  const [showHelpText, setShowHelpText] = useState(false);
+
+  const buttonId = id || sanitizeLabel(label);
+
   const clickEvent = () => {
     setCopied(true);
     copyToClipboard(label);
+    setShowHelpText(true);
     setTimeout(() => {
       setCopied(false);
     }, 2000);
   };
 
-  const buttonId = id || sanitizeLabel(label);
-
   return (
-    <button
-      type="button"
-      className={`relative border-2 border-seafoam-150 rounded-md bg-white text-black cursor-pointer flex items-center pl-4 gap-2 hover:text-orange-500 transition-colors overflow-hidden`}
-      onClick={clickEvent}
-      id={buttonId}
-      {...props}
-    >
-      <span className="bash flex items-center text-black text-lg select-none">
-        &gt;
-      </span>
-      <span className="label font-mono pr-2 text-sm flex items-center mt-0.5 select-text">
-        {label}
-      </span>
-      <span className="flex items-center bg-seafoam-50 h-full px-4 py-3 rounded-tr-md rounded-br-md border-l border-seafoam-150 transition-colors duration-200">
-        <BiCopy className="w-5 h-5 opacity-70" />
-      </span>
-
-      {/* overlay */}
-      {_copied && (
-        <div className="absolute inset-0 bg-white rounded-md flex items-center justify-center gap-2">
-          <CheckIcon className="w-5 h-5 text-orange-500" />
-          <span className="font-mono text-sm text-orange-600 font-medium">
-            Copied to Clipboard
+    <div className="relative flex flex-col border-2 border-seafoam-150 rounded-md">
+      <button
+        type="button"
+        className={`relative ${_copied || showHelpText ? 'rounded-t-md' : 'rounded-md'} bg-white text-black cursor-pointer hover:text-orange-500 transition-colors`}
+        onClick={clickEvent}
+        id={buttonId}
+        {...props}
+      >
+        <div className="group flex items-center pl-4 gap-2">
+          <span className="bash flex items-center text-black text-lg select-none group-hover:text-orange-500">
+            &gt;
+          </span>
+          <span className="label font-mono pr-2 text-sm flex items-center mt-0.5 select-text">
+            {label}
+          </span>
+          <span
+            className={`flex items-center bg-seafoam-50 h-full px-4 py-3 rounded-tr-md rounded-br-md border-l border-seafoam-150 transition-colors duration-200`}
+          >
+            <BiCopy className="w-5 h-5 opacity-70 group-hover:text-orange-500" />
           </span>
         </div>
+
+        {_copied && (
+          <div className="absolute inset-0 bg-white rounded-md flex items-center justify-center gap-2">
+            <CheckIcon className="w-5 h-5 text-orange-500" />
+            <span className="font-mono text-sm text-orange-600 font-medium">
+              Copied to Clipboard
+            </span>
+          </div>
+        )}
+      </button>
+      {showHelpText && helpText?.children?.length > 0 && (
+        <div className="bg-white text-xs border-t border-seafoam-150 rounded-b-md flex items-center justify-between px-2 pb-1 pt-2 overflow-hidden animate-slide-down max-h-[500px]">
+          <TinaMarkdown
+            content={helpText}
+            components={CodeButtonMarkdownStyle}
+          />
+          <button
+            onClick={() => setShowHelpText(false)}
+            type="button"
+            className="hover:text-orange-500 hover:cursor-pointer"
+          >
+            <IoMdClose className="w-5 h-5" />
+          </button>
+        </div>
       )}
-    </button>
+    </div>
   );
 };
