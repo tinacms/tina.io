f95d39b32bee5cd1a3e640e7f3f1669fc26b14ba
Josh Berman [SSW] <JoshBerman@ssw.com.au>
2025-12-24 10:23:18 +1100

    code button improvements

--- components/blocks/CodeButton/CodeButton.tsx
+++ components/blocks/CodeButton/CodeButton.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { BiCopy } from 'react-icons/bi';
+import { BiCheck } from 'react-icons/bi';
 import 'react-responsive-modal/styles.css';
 import { CheckIcon } from '@heroicons/react/24/outline';
 import { IoMdClose } from 'react-icons/io';
@@ -46,36 +47,28 @@ export const CodeButton = ({
     <div className="relative flex flex-col border-2 border-seafoam-150 rounded-md">
       <button
         type="button"
-        className={`relative ${_copied || showHelpText ? 'rounded-t-md' : 'rounded-md'} bg-white text-black cursor-pointer hover:text-orange-500 transition-colors`}
+        className={`relative ${_copied || showHelpText ? 'rounded-t-md' : 'rounded-sm'} bg-[#120101] text-seafoam-500 cursor-pointer hover:text-seafoam-300 transition-colors`}
         onClick={clickEvent}
         id={buttonId}
         {...props}
       >
         <div className="group flex items-center pl-4 gap-2">
-          <span className="bash flex items-center text-black text-lg select-none group-hover:text-orange-500">
+          <span className="bash flex items-center text-lg select-none">
             &gt;
           </span>
           <span className="label font-mono pr-2 text-sm flex items-center mt-0.5 select-text">
             {label}
           </span>
           <span
-            className={`flex items-center bg-seafoam-50 h-full px-4 py-3 rounded-tr-md rounded-br-md border-l border-seafoam-150 transition-colors duration-200`}
+            className={`relative flex items-center bg-seafoam-500 h-full px-4 py-3 rounded-tr-sm ${_copied || showHelpText ? '' : 'rounded-br-sm'} border-l border-seafoam-150 ease-out transition-colors duration-200 text-black group-hover:text-black/90`}
           >
-            <BiCopy className="w-5 h-5 opacity-70 group-hover:text-orange-500" />
+            <BiCopy className={`w-5 h-5 duration-200 ${_copied ? 'rotate-180 scale-0 opacity-0' : 'rotate-0 scale-100 opacity-100'}`} />
+            <BiCheck className={`w-5 h-5 absolute inset-0 m-auto duration-200 ${_copied ? 'rotate-0 scale-100 opacity-100' : '-rotate-180 scale-0 opacity-0'}`} />
           </span>
         </div>
-
-        {_copied && (
-          <div className="absolute inset-0 bg-white rounded-md flex items-center justify-center gap-2">
-            <CheckIcon className="w-5 h-5 text-orange-500" />
-            <span className="font-mono text-sm text-orange-600 font-medium">
-              Copied to Clipboard
-            </span>
-          </div>
-        )}
       </button>
       {showHelpText && helpText?.children?.length > 0 && (
-        <div className="bg-white text-xs border-t border-seafoam-150 rounded-b-md flex items-center justify-between px-2 pb-1 pt-2 overflow-hidden animate-slide-down max-h-[500px]">
+        <div className={`bg-[#120101] text-seafoam-500 text-xs border-t border-seafoam-150 rounded-b-md flex items-center justify-between px-2 pb-1 pt-2 overflow-hidden animate-slide-down max-h-[500px]`}>
           <TinaMarkdown
             content={helpText}
             components={CodeButtonMarkdownStyle}
@@ -83,7 +76,7 @@ export const CodeButton = ({
           <button
             onClick={() => setShowHelpText(false)}
             type="button"
-            className="hover:text-orange-500 hover:cursor-pointer"
+            className="hover:text-seafoam-300 hover:cursor-pointer"
           >
             <IoMdClose className="w-5 h-5" />
           </button>
