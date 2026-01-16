e65d472b100a265ce8f1ced384bde9adf3cbe5a4
Calinator444 <calebwilliams@ssw.com.au>
2026-01-16 16:31:36 +1100

    improved visual hierarchy of featured post

--- components/blocks/RecentPosts/FeaturedPost.tsx
+++ components/blocks/RecentPosts/FeaturedPost.tsx
@@ -25,47 +25,49 @@ type FeaturedPostProps = {
 
 export const FeaturedPost = ({ featuredPost }: FeaturedPostProps) => {
   return (
-    <div className="gap-2 flex w-full">
-      <div className='w-[calc(50%-1rem)]'>
-        {featuredPost.imageUrl ? (
-          <img 
-            src={featuredPost.imageUrl} 
-            alt={featuredPost.title || 'Featured post'} 
-            className="w-full h-auto object-cover"
-            data-tina-field={tinaField(featuredPost, 'imageUrl')}
-          />
-        ) : featuredPost.embedUrl ? (
-          <YouTubeEmbed src={featuredPost.embedUrl} />
-        ) : null}
-      </div>
-      <div className='flex flex-col gap-2'>
-        {featuredPost.datePosted && (
-          <p 
-            className='text-sm text-neutral-text-secondary'
-            data-tina-field={tinaField(featuredPost, 'datePosted')}
-          >
-            {formatDate(featuredPost.datePosted)}
-          </p>
-        )}
-        <h3 
-          className='text-2xl' 
-          data-tina-field={tinaField(featuredPost, 'title')}
+    <div className="flex flex-col gap-2 w-full">
+      {featuredPost.datePosted && (
+        <p 
+          className='text-base text-neutral-text-secondary w-fit'
+          data-tina-field={tinaField(featuredPost, 'datePosted')}
         >
-          {featuredPost.title}
-        </h3>
-        
-        {featuredPost.authorName && (
-          <p className='text-neutral-text-secondary text-base'>
-            By{' '}
-            <a 
-              className='hover:text-neutral-text uppercase underline transition-colors' 
-              href={featuredPost.authorUrl || '#'}
-              data-tina-field={tinaField(featuredPost, 'authorName')}
-            >
-              {featuredPost.authorName}
-            </a>
-          </p>
-        )}
+          {formatDate(featuredPost.datePosted)}
+        </p>
+      )}
+      <div className="gap-2 flex w-full">
+        <div className='w-[calc(75%-1rem)]'>
+          {featuredPost.imageUrl ? (
+            <img 
+              src={featuredPost.imageUrl} 
+              alt={featuredPost.title || 'Featured post'} 
+              className="w-full h-auto object-cover"
+              data-tina-field={tinaField(featuredPost, 'imageUrl')}
+            />
+          ) : featuredPost.embedUrl ? (
+            <YouTubeEmbed src={featuredPost.embedUrl} />
+          ) : null}
+        </div>
+        <div className='flex flex-col py-1 gap-2'>
+          <h3 
+            className='text-2xl' 
+            data-tina-field={tinaField(featuredPost, 'title')}
+          >
+            {featuredPost.title}
+          </h3>
+          
+          {featuredPost.authorName && (
+            <p className='text-neutral-text-secondary text-base'>
+              By{' '}
+              <a 
+                className='hover:text-neutral-text uppercase underline transition-colors' 
+                href={featuredPost.authorUrl || '#'}
+                data-tina-field={tinaField(featuredPost, 'authorName')}
+              >
+                {featuredPost.authorName}
+              </a>
+            </p>
+          )}
+        </div>
       </div>
     </div>
   );
