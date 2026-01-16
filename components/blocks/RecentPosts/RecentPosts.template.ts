afe2ce973dc129482cf5dd5b5df72e534b482219
Calinator444 <calebwilliams@ssw.com.au>
2026-01-16 15:54:35 +1100

    - removed redundant disclaimer about time being ignored for date input - Added schema for featured blog field

--- components/blocks/RecentPosts/RecentPosts.template.ts
+++ components/blocks/RecentPosts/RecentPosts.template.ts
@@ -11,6 +11,52 @@ export const recentPostsTemplate = {
       type: 'string',
     },
     {
+      name: 'featuredHeading',
+      label: 'Featured Heading',
+      type: 'string',
+      description: 'Heading for the featured post section',
+    },
+    {
+      name: 'featuredPost',
+      label: 'Featured Post',
+      type: 'object',
+      fields: [
+        {
+          name: 'title',
+          label: 'Title',
+          type: 'string',
+        },
+        {
+          name: 'datePosted',
+          label: 'Date Posted',
+          type: 'datetime',
+        },
+        {
+          name: 'authorName',
+          label: 'Author Name',
+          type: 'string',
+        },
+        {
+          name: 'authorUrl',
+          label: 'Author URL',
+          type: 'string',
+          description: 'The SSW People URL of the author',
+        },
+        {
+          name: 'embedUrl',
+          label: 'YouTube Embed URL',
+          type: 'string',
+          description: 'YouTube Embed URL, formatted like https://www.youtube.com/embed/<id>',
+        },
+        {
+          name: 'imageUrl',
+          label: 'Image',
+          type: 'image',
+          description: 'Takes precedence over video if both are provided',
+        },
+      ],
+    },
+    {
       name: 'youtubeVideos',
       label: 'YouTube Videos',
       type: 'object',
@@ -34,7 +80,6 @@ export const recentPostsTemplate = {
           name: 'dateReleased',
           label: 'Date Released',
           type: 'datetime',
-          description: 'Time doesnt matter we only use day, month and year',
         },
         { name: 'title', label: 'Title', type: 'string' },
         { name: 'authorName', label: 'Author Name', type: 'string' },
