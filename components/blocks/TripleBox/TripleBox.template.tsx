92d1d9d64d6368d7e9bd6e17d97446caa74c781c
Josh Berman [SSW] <137844305+joshbermanssw@users.noreply.github.com>
2026-01-19 10:20:39 +1100

    💄 Hero Section, Code Button & Template Section (#4260)

---
+++ components/blocks/TripleBox/TripleBox.template.tsx
@@ -0,0 +1,77 @@
+import type { Template } from 'tinacms';
+import { actionsButtonTemplate } from '../ActionButton/ActionsButton.template';
+import { codeButtonTemplate } from '../CodeButton/CodeButton.template';
+import { modalButtonTemplate } from '../ModalButton/ModalButton.template';
+
+export const tripleBoxTemplate = {
+  label: 'Triple Box',
+  name: 'tripleBox',
+  fields: [
+    {
+      label: 'Title',
+      name: 'title',
+      type: 'string',
+    },
+    {
+      name: 'tagLine',
+      label: 'tagLine',
+      type: 'string',
+    },
+    {
+      label: 'Buttons',
+      list: true,
+      name: 'buttons',
+      type: 'object',
+      ui: {
+        visualSelector: true,
+        min: 0,
+        max: 1,
+      },
+      templates: [
+        actionsButtonTemplate as Template,
+        modalButtonTemplate as Template,
+        codeButtonTemplate as Template,
+      ],
+    },
+    {
+      name: 'boxes',
+      label: 'Boxes',
+      type: 'object',
+      list: true,
+      ui: {
+        itemProps: (item) => {
+          return { label: item?.title };
+        },
+      },
+      fields: [
+        {
+          name: 'title',
+          label: 'Title',
+          type: 'string',
+        },
+        {
+          name: 'description',
+          label: 'Description',
+          type: 'string',
+          ui: { component: 'textarea' },
+        },
+        {
+          name: 'image',
+          label: 'Image',
+          type: 'image',
+        },
+        {
+          name: 'link',
+          label: 'Link',
+          type: 'string',
+        },
+        {
+          name: 'badges',
+          label: 'Badges',
+          type: 'string',
+          list: true,
+        },
+      ],
+    },
+  ],
+};
