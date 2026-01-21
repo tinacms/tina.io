be2a77fa87ef9faef12ac7f8dae67f8d4f713558
Calinator444 <calebwilliams@ssw.com.au>
2026-01-21 12:08:42 +1100

    Fixed button pill sizing when switching between inline & notification view for "new" badge

--- components/blocks-v2/hero/hero.tsx
+++ components/blocks-v2/hero/hero.tsx
@@ -32,10 +32,15 @@ export default function HeroV2(data: { data: PageBlocksHeroV2 }) {
       <div className={cn("flex items-center flex-col gap-8  border-red-500 py-10",bannerFilled && "pt-0" )}>
         
         <div className='flex flex-col gap-3'>
+          {/* Banner */}
         {bannerFilled && (
           <Badge className={cn(
             'mb-2',
-            (recentNewsBanner.badge?.position === 'top left' || recentNewsBanner.badge?.position === 'top right') && 'relative'
+            (recentNewsBanner.badge?.position === 'top left' || recentNewsBanner.badge?.position === 'top right') && 'relative',
+            recentNewsBanner.badge?.text &&
+              (recentNewsBanner.badge?.position === 'left' || recentNewsBanner.badge?.position === 'right')
+              ? 'px-1 py-1'
+              : 'px-4 py-2'
           )} asChild={true}>
             <Link 
               href={recentNewsBanner.link || '#'}
