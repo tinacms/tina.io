bc2fbc3844fdafef9d75e0718bb8604859433bd1
Calinator444 <calebwilliams@ssw.com.au>
2026-01-22 12:35:09 +1100

    removed unused code for icon component

--- components/forms/IconPicker.tsx
+++ components/forms/IconPicker.tsx
@@ -1,13 +1,10 @@
 import { Popover, PopoverButton, Transition, PopoverPanel } from '@headlessui/react';
 import React from 'react';
 import { BiChevronRight } from 'react-icons/bi';
-import { FaFacebookF } from 'react-icons/fa';
-
 
 import * as MdIcons from 'react-icons/md'
 import { GoCircleSlash } from 'react-icons/go';
 import { Button, wrapFieldsWithMeta } from 'tinacms';
-
 
 const parseIconName = (name: string) => {
   const splitName = name.split(/(?=[A-Z])/);
@@ -22,93 +19,23 @@ export const IconOptions = {
   ...MdIcons,
 };
 
-const iconColorClass: {
-  [name: string]: { regular: string; circle: string };
-} = {
-  blue: {
-    regular: 'text-blue-400',
-    circle: 'bg-blue-400 text-blue-50',
-  },
-  teal: {
-    regular: 'text-teal-400',
-    circle: 'bg-teal-400 text-teal-50',
-  },
-  green: {
-    regular: 'text-green-400',
-    circle: 'bg-green-400 text-green-50',
-  },
-  red: {
-    regular: 'text-red-400',
-    circle: 'bg-red-400 text-red-50',
-  },
-  pink: {
-    regular: 'text-pink-400',
-    circle: 'bg-pink-400 text-pink-50',
-  },
-  purple: {
-    regular: 'text-purple-400',
-    circle: 'bg-purple-400 text-purple-50',
-  },
-  orange: {
-    regular: 'text-orange-400',
-    circle: 'bg-orange-400 text-orange-50',
-  },
-  yellow: {
-    regular: 'text-yellow-400',
-    circle: 'bg-yellow-400 text-yellow-50',
-  },
-  white: {
-    regular: 'text-white opacity-80',
-    circle: 'bg-white-400 text-white-50',
-  },
-};
+export const Icon = ({ data, className = ''}) => {
 
-const iconSizeClass = {
-  xs: 'w-6 h-6 shrink-0',
-  small: 'w-8 h-8 shrink-0',
-  medium: 'w-12 h-12 shrink-0',
-  large: 'w-14 h-14 shrink-0',
-  xl: 'w-16 h-16 shrink-0',
-  custom: '',
-};
-
-export const Icon = ({ data, parentColor = '', className = '', tinaField = '' }) => {
-
-  
   if (IconOptions[data.name] === null || IconOptions[data.name] === undefined) {
     return null;
   }
 
-  const { name, color, size = 'medium', style = 'regular' } = data;
-
+  const { name } = data;
   
   const IconSVG = IconOptions[name];
 
-  const iconSizeClasses = typeof size === 'string' ? iconSizeClass[size] : iconSizeClass[Object.keys(iconSizeClass)[size]];
-
-  const iconColor = color && color !== 'primary' ? color : 'blue';
-
-  if (style == 'circle') {
-    return (
-      <div
-        {...(tinaField ? { 'data-tina-field': tinaField } : {})} // only render data-tina-field if it exists
-        className={`relative z-10 inline-flex items-center justify-center shrink-0 ${iconSizeClasses} rounded-full ${iconColorClass[iconColor].circle} ${className}`}
-      >
-        <IconSVG className='w-2/3 h-2/3' />
-      </div>
-    );
-  } else {
-    const iconColorClasses = iconColorClass[parentColor === 'primary' && iconColor === 'blue' ? 'white' : iconColor!].regular;
-    return (
-      <IconSVG
-        {...(tinaField ? { 'data-tina-field': tinaField } : {})} // only render data-tina-field if it exists
-        className={`${iconSizeClasses} ${iconColorClasses} ${className}`}
-      />
-    );
-  }
+  return (
+    <IconSVG
+      className={`${className}`}
+    />
+  );
+  
 };
-
-
 
 export const IconPickerInput: React.FC<{field: unknown}> = wrapFieldsWithMeta(({ input }) => {
   const [filter, setFilter] = React.useState('');
@@ -192,10 +119,8 @@ export const IconPickerInput: React.FC<{field: unknown}> = wrapFieldsWithMeta(({
                                 <Icon
                                   data={{
                                     name: name,
-                                    size: 'custom',
-                                    color: 'blue',
                                   }}
-                                  className='w-7 h-auto'
+                                  className='w-7 h-auto text-blue-400'
                                 />
                               </button>
                             );
