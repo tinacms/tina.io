83cb6ba80f0e2453911a1933832097acfddc030a
Harley <mathmark825@gmail.com>
2026-08-27 10:52:04 +1000

    Fix formatting

--- components/blocks/CompareBox/CompareBox.tsx
+++ components/blocks/CompareBox/CompareBox.tsx
@@ -143,7 +143,10 @@ const CompanyCard = ({ company, criteria }) => {
                 style={{
                   ...commonHeightStyle,
                   backgroundColor: company.backgroundColor
-                    ? hexToRgba(isTina ? company.backgroundColor : '#808080', 0.2)
+                    ? hexToRgba(
+                        isTina ? company.backgroundColor : '#808080',
+                        0.2,
+                      )
                     : 'transparent',
                 }}
               >
